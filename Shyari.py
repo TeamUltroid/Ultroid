137ad8c563c5fb61ed8eadfe598b252f60bdd6c3
Abhinav Shukla <67401627+maxprogrammer007@users.noreply.github.com>
2021-02-25 08:35:28 +0530

    successfully tested
    this is a shyri plugin which shows random shayri  all credits are with @kraken_the_badass  i have copied , edited, debugged this code and made it compatible with ultroid !! 
    this plugin is successfully tested by me if want proofs u can contact me at tg @maxprogrammer007

---
+++ Shyari.py
@@ -0,0 +1,88 @@
+#Made By @Kraken_The_BadASS. and commands edited by @maxprogrammer007
+# Ported for Ultroid < https://github.com/TeamUltroid/Ultroid > 
+
+
+""" Making A Shayri... 
+    Command .me_shayr
+    By @Kraken_The_BadASS """
+
+from telethon import events
+import asyncio
+import os
+import sys
+import random
+from userbot.utils import admin_cmd
+
+@borg.on(admin_cmd(pattern=r"me_shayr$", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+    await event.edit("Making A Shayri.......")
+    await asyncio.sleep(2)
+    x=(random.randrange(1,33))
+    if x==1:
+        await event.edit("🙂Kitna Khusnuma Hoga,\nWoh Meri Maut Ka Manjar\nJab Mujhe Thukrane Wale\nKhud Mujhe Paane Ke Liye,\nAansu Bahayange!!!☺️\n\n\n✍️@maxprogrammer007")
+    if x==2:
+        await event.edit("Zindagi me baar baar\nKoi sahara nahi milta,\n\nBaar baar koi\nPyaar se pyara nahi milta,\n\nJo paas hai ussy sambhal ke rakhna,\nKyuki koi khoo jaaye toh\n\n**Phir doobara nahi milta...**☺️\n\n\n✍️@maxprogrammer007")
+    if x==3:
+        await event.edit("कभी अपना कहते थे\n आज बेगाना कर गए...\n\nहमसे बात ना करने के लिए\n बहाना कर गए...\n\nशुक्रिया कैसे करूं तुम्हारा \nसमझ नहीं आ रहा...\n\nमेरे इस नियाने से दिल को \n**सयाना कर गए...*\n\n\n✍️@maxprogrammer007 ")
+    if x==4:
+        await event.edit("Teri Khubsurti Ki Tareef \nMain Ab Kya Likhu\n\n\nKuch Khubsurat Lafzon Ki Talaash \nAb Bhi Hai Mujhe🙂\n\n\n✍️@maxprogrammer007")
+    if x==5:
+        await event.edit("Main Uska Ho Nahi Sakta,\nWoh Meri Ho Nahi Sakti\n\nWoh Aaye Lakh Khwaabon Main,\nSapna Sach Ho Nahi Sakta\n\nMere Nazdeek Ho Kar Bhi,\nNahi Hai Woh Sath Mere\n\nUsse Neend Nahi Aati,\nYaha Main So Nahi Sakta\n\nMohabbat Ka Jo Rishta Hai,\nNaa Jaane Kaisa Rishta Hai\n\nMera Ho Ke Bhi Koi,\n**Mera Ho Nahi Sakta!!!**\n\n\n✍️@maxprogrammer007")
+    if x==6:
+        await event.edit("Dukh yeh nhi,\nKe koi Apna nhi....\n\nDukh Yeh Hai Ke\nKisi ne\n\n\n**APNA BANA KAR CHOR DIA**🙂💔\n\n\n✍️@maxprogrammer007")
+    if x==7:
+        await event.edit("एक बार भूल से ही \nकहा होता \nकी हम किसी और के भी है \nखुदा कसम \nहम तेरे सायें से भी दूर रहते...🙂\n\n\n✍️@maxprogrammer007  ")    
+    if x==8:
+        await event.edit("Dosti Nibhate Nibhate \nUs Se Mohabbat Si Ho Gayi\n\nGam Hi Mile Sahi \nPar Chahat Si Ho Gayi\n\nKarte The Jo Baatain \nRaat Raat Bhar\nAaj Un Se Baat Karne Ki Khwahish Si Ho Gayi\n\nJee Nahi Sakte Ab Us Ke Bin\n**Us Ke Sath Rehne Ki Aadat Si Ho Gayi**\n\n\n✍️@maxprogrammer007")
+    if x==9:
+        await event.edit("Tere Deedar ke lie aate hai\nTeri galiyon me...\n\nWarna awaargi ke lie to\nPura seher pada hai🙂\n\n\n✍️@maxprogrammer007")
+    if x==10:
+        await event.edit("Bass Aakhir baar tere pyaar ko Mehsus karloon\n\nLaut ke fir kabhi tere galiyoon me nhi aaunga\n\nApni barbaad mohabbat ka Zanaja lekar\n**Teri Duniya se bahut dur chala jaunga**\n\n\n✍️@maxprogrammer007")
+    if x==11:
+        await event.edit("Bheed Ki aadat nhi mujhe\nThode me zeena sikh lia humne\n\nDo dost hai,Channd Duae hai\nBass inn khusiyoon ko\nGale laga lia humne🙂\n\n\n✍️@maxprogrammer007")
+    if x==12:
+        await event.edit("दोस्ती जैसे खूबसूरत रिश्ते को \nदफना दिया तुमने \nअब उसे दफन ही रहने दो ।\n\nअब मेरे अंदर कोई जज्बात नहीं बचे \nमर चुका हूं में \nअब तुम मुझे दफन ही रहने दो\n\n\n✍️@maxprogrammer007")
+    if x==13:
+        await event.edit("अगर बुरा न मानो तो कहें???\n\n     हमको भी बुरा लगता है !!!!\n\n\n✍️@maxprogrammer007")
+    if x==14:
+        await event.edit("में क्यों तेरे ख्यालो में खोता रहुँ\n\n     पागल नहीं हूँ में \nजो हर पल तेरे लिए रोता रहुँ !\n\n\n✍️@maxprogrammer007")
+    if x==15:
+        await event.edit("ना **राज** है....जींदगी,...\nना **नाराज**है....जींदगी,...\nबस जो भी है,........\n वो **आज**है....... जींदगी\n\n\n✍️@maxprogrammer007")
+    if x==16:
+        await event.edit("जब तुम नहीं समझे,\nतब मैंने खुद को कितना समझाया\n\nये तुम कभी नहीं समझोगे\n\n\n✍️@maxprogrammer007")
+    if x==17:
+        await event.edit("Kisi Ne Yun Hi Pooch Liya Humse\nKi Dard Ki Keemat Kya Hai,\nHumne Hanste Huye Kaha\nPata Nahin\n\n**Kuch Apne Muft Me De Gaye**\n\n\n✍️@maxprogrammer007")
+    if x==18:
+        await event.edit("Rekhao ka khel he sara\nKya kare taqdeer ka mara\n\nJis qadar uski qadar ki..\n**Uss qadar beqadar huve ham**\n\n\n✍️@maxprogrammer007")
+    if x==19:
+        await event.edit("तेरी हर बात मुझे अपने तरफ खिंचती क्यूँ हैं,\nतू क्या हैं, कौन हैं, मेरे लिए इतना जरूरी क्यूँ हैं\nमेरे साथ साथ तू साये की तरह क्यूँ हैं,\n\nअगर ऐसा ही हैं तो फिर \nतू मुझसे इतना दूर होके भी\n**पास क्यू हैं\n\n\n✍️@maxprogrammer007")
+    if x==20:
+        await event.edit("नज़र को नज़र की खबर ना लगे\nकोई अच्छा भी इस कदर ना लगे \n\nआपको देखा है बस उस नज़र से\nजिस नज़र से आपको नज़र ना लगे\n\n\n✍️@maxprogrammer007")
+    if x==21:
+        await event.edit("Teri muskurahat Meri pahechan he\nTerri Khushi Meri shan he\n\nKuch bhi nhi he meri jindgi me\nItna smaj le bas tu Meri jaan he\n\n\n✍️@maxprogrammer007")
+    if x==22:
+        await event.edit("💞💞💞💞💞💞💞💞💞💞\n\nमेरा इश्क बड़ा नाज़ुक है इसे सहेज के रखना...,\n\n\nइसे उंगलियों से मत पकड़ना...\nहथेलियों पे रखना...,\n\n💞💞💞💞💞💞💞💞💞💞\n\n\n✍️@maxprogrammer007")
+    if x==23:
+        await event.edit("तेरे इश्क की जंग में,\n          हम मुस्कुराके डट गए,\n\nतलवार से तो बच गए,\n          तेरी मुस्कान से कट गए।\n\n\n✍️@maxprogrammer007")
+    if x==24:
+        await event.edit("💖आँखों में देखी जाती हैं..,,\nप्यार की गहराईयाँ.\n\nशब्दों में तो छुप जाती हैं..,,\nबहुत सी तन्हाईयाँ....💖⚡😘\n\n\n✍️@maxprogrammer007")
+    if x==25:
+        await event.edit("Dhadkan Ye Kehti Hai.\nDil Tere Bin Dhadke Na.\nEk Tu Hi Yaar Mera.\nMujhko Kya Duniya Se Lena\n\n\n ✍️ @maxprogrammer007")
+    if x==26:
+        await event.edit("Khud Nahi Jante Kitne Pyare Ho Aap.\nJaan Ho Hamari Par Jaan Se Pyari Ho Aap.\nDuriyon Ke Hone Se Koi Fark Nahi Padta.\nKal Bhi Hamare The Aur Aaj Bhi Hamari Ho Aap\n\n\n✍️ @maxprogrammer007")
+    if x==27:
+        await event.edit("Samandar Kinare baithe hai.\nKabhi to leher aaegi.\nKismat badle ya na badle.\nGand to dhul jaegi.\n\n\n✍️ @maxprogrammer007")
+    if x==28:
+        await event.edit("Mere Dil ke Yeh tukde hai.\nNigaaho se choonu yaara.\nMohabatt ki kahaani hai.\nMohabatt se suno yaara.\n\n\n✍️ @maxprogrammer007")
+    if x==29:
+        await event.edit("Kaunsa Zakhm Tha Jo Taaza Naa Tha.\nItna Gam Milega Andaza Naa Tha.\nAap Ki Jheel Si Aankhon Kaa Kya Kasoor.\nDubne Wale Ko Hi Gehrai Kaa Andaza Naa Tha\n\n\n✍️ @maxprogrammer007")
+    if x==30:
+        await event.edit("Bahte Hue Duriya Ko Kya Modega Koi.\nToote Hue Shishe Ko Kya Jodega Koi.\nChalo Fir Se Dil Lagake Dekhte Hai.\nAb Is Toote Hue Dil Ko Kya Todega Koi\n\n\n✍️ @maxprogrammer007")
+    if x==31:
+        await event.edit("Dil Ko Jalate Hai Ham Diye Ki Tarah,\nTeri Zindagi Main Roshni Lane Ke Liye,\nLe Lete Hai Har Kaaton Ko Apni Zindagi Mein,\nBas Teri Rahon Main Phool Bhichane Ke Liye\n\n\n✍️ @maxprogrammer007")
+    if x==32:
+        await event.edit("Pyase Ko Ek Katra Pani Kafi Hai.\nIshq Mein Char Pal Ki Zindgani Kafi Hai.\nDoobne Ko Samander Mein Jayein Kahan.\nAapki Aankh Se Tapka Voh Pani Kafi Hai.\n\n\n✍️ @maxprogrammer007")
+    if x==33:
+        await event.edit("Not in mood. Sorry!")
