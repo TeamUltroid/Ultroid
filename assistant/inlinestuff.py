3562f8cd13496c738e8028d2690ffbf89dab0ab1
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-03-01 00:24:02 +0530

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -15,6 +15,7 @@ from bs4 import BeautifulSoup
 from search_engine_parser import GoogleSearch, YahooSearch
 from telethon import Button
 from telethon.tl.types import InputWebDocument as wb
+from . import *
 
 gugirl = "https://telegra.ph/file/0df54ae4541abca96aa11.jpg"
 yeah = "https://telegra.ph/file/e3c67885e16a194937516.jpg"
