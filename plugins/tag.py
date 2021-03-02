4e94f0cfb7d1014e7d23f7a10cd2e657afaad356
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-03-02 08:58:26 +0530

    Add files via upload

---
+++ plugins/tag.py
@@ -0,0 +1,87 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available -
+
+• `{i}tagall`
+    Tag Top 100 Members of chat.
+
+• `{i}tagadmins`
+    Tag Admins of that chat.
+
+• `{i}tagowner`
+    Tag Owner of that chat
+    
+• `{i}tagbots`
+    Tag Bots of that chat.
+
+• `{i}tagrec`
+    Tag recently Active Members.
+"""
+
+from telethon.tl.types import ChannelParticipantAdmin as admin
+from telethon.tl.types import ChannelParticipantCreator as owner
+from telethon.tl.types import UserStatusOffline as off
+from telethon.tl.types import UserStatusOnline as on
+from telethon.tl.types import UserStatusRecently as rec
+from telethon.utils import get_display_name
+
+
+@ultroid_cmd(
+    pattern="tag(on|off|all|rec|admins|owner)?(.*)",
+    groups_only=True,
+)
+async def _(e):
+    okk = e.text
+    lll = e.pattern_match.group(2)
+    users = 0
+    o = 0
+    nn = 0
+    rece = 0
+    if lll:
+        xx = f"{lll}"
+    else:
+        xx = ""
+    async for bb in e.client.iter_participants(e.chat_id, 99):
+        users = users + 1
+        x = bb.status
+        y = bb.participant
+        if isinstance(x, on):
+            o = o + 1
+            if "on" in okk:
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(x, off):
+            nn = nn + 1
+            if "off" in okk:
+                if not (bb.bot or bb.deleted):
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if isinstance(x, rec):
+            rece = rece + 1
+            if "rec" in okk:
+                if not (bb.bot or bb.deleted):
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"             
+        if isinstance(y, owner):
+            if "admin" or "owner"  in okk:
+                xx += f"\n꧁[{get_display_name(bb)}](tg://user?id={bb.id})꧂"
+        if isinstance(y, admin):
+            if "admin" in okk:
+                if not bb.deleted:
+                    xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "all" in okk:
+            if not (bb.bot or bb.deleted):
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+        if "bot" in okk:
+            if bb.bot:
+                xx += f"\n[{get_display_name(bb)}](tg://user?id={bb.id})"
+    if len(xx) > 4096:
+        await eor(e, "`Telegram Word Limits Exceeds.`")
+    else:
+        await e.client.send_message(e.chat_id, xx)
+        await e.delete()
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
