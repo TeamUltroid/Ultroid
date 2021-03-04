ea41122903226cc199c9c3b1e2e0900a9f719e73
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-03-04 20:14:57 +0530

    Fixes

--- plugins/tag.py
+++ plugins/tag.py
@@ -36,7 +36,7 @@ from telethon.tl.types import UserStatusOffline as off
 from telethon.tl.types import UserStatusOnline as onn
 from telethon.tl.types import UserStatusRecently as rec
 from telethon.utils import get_display_name
-from . import HELP
+from . import *
 
 
 @ultroid_cmd(
