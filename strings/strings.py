30dceb61bd190d87a2bb0b04703f8069b9020519
Roj Serbest <rojserbest@gmail.com>
2021-03-05 12:04:54 +0300

    Lol

--- strings/strings.py
+++ strings/strings.py
@@ -3,18 +3,20 @@ from typing import Any, Dict, List, Union
 
 from yaml import safe_load
 
-#from pyUltroid import udB
+from pyUltroid import udB
 
 
-#selected_language = udB.get("language") or "en"
-selected_language = "ku"
+selected_language = udB.get("language") or "en"
 languages = {}
 strings_folder = path.join(path.dirname(path.realpath(__file__)), "strings")
 
 for file in listdir(strings_folder):
     if file.endswith(".yml"):
         code = file[:-4]
-        languages[code] = safe_load(open(path.join(strings_folder, file), encoding="UTF-8"))
+        languages[code] = safe_load(
+            open(path.join(strings_folder, file), encoding="UTF-8")
+        )
+
 
 def get_string(key: str) -> Any:
     try:
@@ -25,5 +27,13 @@ def get_string(key: str) -> Any:
         except KeyError:
             return f"Warning: could not load any string with the key {key}"
 
+
 def get_languages() -> Dict[str, Union[str, List[str]]]:
-    return {code: {"name": languages[code]["name"], "natively": languages[code]["natively"], "authors": languages[code]["authors"]} for code in languages}
+    return {
+        code: {
+            "name": languages[code]["name"],
+            "natively": languages[code]["natively"],
+            "authors": languages[code]["authors"],
+        }
+        for code in languages
+    }
