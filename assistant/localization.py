aac15063a840dd1209f3f19165ffa1050386b85f
Aditya <me@xditya.me>
2021-03-06 06:09:29 +0530

    ok done

--- assistant/localization.py
+++ assistant/localization.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 from . import *
 import re
 
