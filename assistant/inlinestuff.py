b5de90ab1edacb585fb0d3cf078e4a107c993efd
buddhhu <buddhhu@users.noreply.github.com>
2021-03-10 19:22:45 +0000

    Pehli Fursat m nikal

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -12,9 +12,9 @@ from urllib.request import urlopen
 import play_scraper
 import requests
 from bs4 import BeautifulSoup
+from pyUltroid.functions import GoogleSearch, YahooSearch
 from rextester_py import rexec_aio
 from rextester_py.rextester_aio import UnknownLanguage
-from pyUltroid.functions import GoogleSearch, YahooSearch
 from telethon import Button
 from telethon.tl.types import InputWebDocument as wb
 
