8b32e53c577357293b10512f836ed9319af9074a
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-03-12 00:13:38 +0530

    Update othervars.py

--- assistant/othervars.py
+++ assistant/othervars.py
@@ -45,7 +45,7 @@ async def _(e):
         + "4. Copy link of that folder."
         + "5. Send all characters which is after id= ."
     )
-    async with ultroid_bot.asst.conversation(ultroid_bot.uid) as conv:
+    async with ultroid_bot.asst.conversation(e.sender_id) as conv:
         reply = conv.wait_event(events.NewMessage(from_users=e.sender_id))
         repl = await reply
         udB.set("GDRIVE_FOLDER_ID", repl.text)
@@ -61,7 +61,7 @@ async def _(e):
     if not e.is_private:
         return
     await e.edit("Send your CLIENT SECRET")
-    async with ultroid_bot.asst.conversation(ultroid_bot.uid) as conv:
+    async with ultroid_bot.asst.conversation(e.sender_id) as conv:
         reply = conv.wait_event(events.NewMessage(from_users=e.sender_id))
         repl = await reply
         udB.set("GDRIVE_CLIENT_SECRET", repl.text)
@@ -77,7 +77,7 @@ async def _(e):
     if not e.is_private:
         return
     await e.edit("Send your CLIENT ID ending with .com")
-    async with ultroid_bot.asst.conversation(ultroid_bot.uid) as conv:
+    async with ultroid_bot.asst.conversation(e.sender_id) as conv:
         reply = conv.wait_event(events.NewMessage(from_users=e.sender_id))
         repl = await reply
         if not repl.text.endswith(".com"):
