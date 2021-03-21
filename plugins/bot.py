c739c8a27019c83473ce6bbb144e6078c4aa4503
Danish <72792730+1Danish-00@users.noreply.github.com>
2021-03-21 11:37:10 +0530

    To fuk mono

--- plugins/bot.py
+++ plugins/bot.py
@@ -71,7 +71,7 @@ async def lol(ult):
     y = Repo().active_branch
     xx = Repo().remotes[0].config_reader.get("url")
     rep = xx.replace(".git", f"/tree/{y}")
-    kk = f"[{y}]({rep})"
+    kk = f" ` [{y}]({rep}) ` "
     als = (get_string("alive_1")).format(
         header,
         OWNER_NAME,
