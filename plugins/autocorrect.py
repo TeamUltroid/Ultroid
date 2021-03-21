bf339142c82971d90e49976e9c17f29aa7f8e80d
1Danish-00 <1Danish-00@users.noreply.github.com>
2021-03-21 07:36:00 +0000

    pyLint: auto-fixes

--- plugins/autocorrect.py
+++ plugins/autocorrect.py
@@ -13,16 +13,18 @@
 
 """
 
-from . import *
 from gingerit.gingerit import GingerIt
-from telethon import events
 from googletrans import Translator
+from telethon import events
+
+from . import *
+
 tr = Translator()
 
 
 @ultroid_cmd(pattern="autocorrect")
 async def acc(e):
-    if Redis("AUTOCORRECT")!="True":
+    if Redis("AUTOCORRECT") != "True":
         udB.set("AUTOCORRECT", "True")
         await eor(e, "AUTOCORRECT Feature On")
     else:
@@ -32,11 +34,11 @@ async def acc(e):
 
 @ultroid_bot.on(events.NewMessage(outgoing=True))
 async def gramme(event):
-    if Redis("AUTOCORRECT")!="True":
+    if Redis("AUTOCORRECT") != "True":
         return
     t = event.text
     tt = tr.translate(t)
-    if t.startswith((HNDLR, ".","?","#","_","*","'","@","[","(","+")):
+    if t.startswith((HNDLR, ".", "?", "#", "_", "*", "'", "@", "[", "(", "+")):
         return
     if t.endswith(("..")):
         return
@@ -44,7 +46,7 @@ async def gramme(event):
         return
     xx = GingerIt()
     x = xx.parse(t)
-    res = (x['result'])
+    res = x["result"]
     await event.edit(res)
 
 
