a6cf25ba77b451ed8949b2a1999cefcaff34cf55
Shrimadhav U K <SpEcHiDe@users.noreply.github.com>
2021-03-22 22:32:57 +0530

    🥺 (#37)
    * fixes
    
    Co-authored-by: Aditya <me@xditya.me>
    Co-authored-by: Danish <72792730+1Danish-00@users.noreply.github.com>
    Co-authored-by: buddhhu <buddhuu0@users.noreply.github.com>
    Co-authored-by: sppidy <sppidy@users.noreply.github.com>
    Co-authored-by: Arnab Paryali <Arnabxd@users.noreply.github.com>
    
    * Fixes
    
    * Update en.yml
    
    * Update en.yml
    
    * try 🤒🥺
    
    * pyLint: auto-fixes
    
    Co-authored-by: Anonymous <69723581+New-dev0@users.noreply.github.com>
    Co-authored-by: Aditya <me@xditya.me>
    Co-authored-by: Danish <72792730+1Danish-00@users.noreply.github.com>
    Co-authored-by: buddhhu <buddhuu0@users.noreply.github.com>
    Co-authored-by: sppidy <sppidy@users.noreply.github.com>
    Co-authored-by: Arnab Paryali <Arnabxd@users.noreply.github.com>
    Co-authored-by: xditya <xditya@users.noreply.github.com>

--- resources/startup/startup.sh
+++ resources/startup/startup.sh
@@ -3,8 +3,12 @@
 # Copyright (C) 2020 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# Please read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
- 
+# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+declare -x IFS
+IFS="
+"
+
 echo "
             ┏┳┓╋┏┓╋╋╋╋┏┓┏┓
             ┃┃┣┓┃┗┳┳┳━╋╋┛┃
