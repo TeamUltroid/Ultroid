e809ea81568aa7a71c564fec849a7e24bb64f493
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-03-22 21:38:20 +0530

    Update start_me_if_u_can.py

--- start_me_if_u_can.py
+++ start_me_if_u_can.py
@@ -7,85 +7,88 @@ from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.phone import GetGroupCallRequest
 from telethon.tl.functions.phone import JoinGroupCallRequest
 from telethon.tl.types import DataJSON
-from pyUltroid import udB, vcbot
+try:
+    from pyUltroid import udB, vcbot
+except:
+    pass
 
-async def get_entity(chat):
-    try:
-        return await vcbot.get_input_entity(chat['id'])
-    except ValueError:
-        if 'username' in chat:
-            return await vcbot.get_entity(chat['username'])
-        raise
+if vcbot:
+    async def get_entity(chat):
+        try:
+            return await vcbot.get_input_entity(chat['id'])
+        except ValueError:
+            if 'username' in chat:
+                return await vcbot.get_entity(chat['username'])
+            raise
 
 
-async def join_call(data):
-    chat = await get_entity(data['chat'])
-    full_chat = await vcbot(GetFullChannelRequest(chat))
-    call = await vcbot(GetGroupCallRequest(full_chat.full_chat.call))
+    async def join_call(data):
+        chat = await get_entity(data['chat'])
+        full_chat = await vcbot(GetFullChannelRequest(chat))
+        call = await vcbot(GetGroupCallRequest(full_chat.full_chat.call))
 
-    result = await vcbot(
-        JoinGroupCallRequest(
-            call=call.call,
-            muted=False,
-            join_as='me',
-            params=DataJSON(
-                data=json.dumps({
-                    'ufrag': data['ufrag'],
-                    'pwd': data['pwd'],
-                    'fingerprints': [{
-                        'hash': data['hash'],
-                        'setup': data['setup'],
-                        'fingerprint': data['fingerprint'],
-                    }],
-                    'ssrc': data['source'],
-                }),
+        result = await vcbot(
+            JoinGroupCallRequest(
+                call=call.call,
+                muted=False,
+                join_as='me',
+                params=DataJSON(
+                    data=json.dumps({
+                        'ufrag': data['ufrag'],
+                        'pwd': data['pwd'],
+                        'fingerprints': [{
+                            'hash': data['hash'],
+                            'setup': data['setup'],
+                            'fingerprint': data['fingerprint'],
+                        }],
+                        'ssrc': data['source'],
+                    }),
+                ),
             ),
-        ),
-    )
+        )
 
-    transport = json.loads(result.updates[0].call.params.data)['transport']
+        transport = json.loads(result.updates[0].call.params.data)['transport']
 
-    return {
-        '_': 'get_join',
-        'data': {
-            'chat_id': data['chat']['id'],
-            'transport': {
-                'ufrag': transport['ufrag'],
-                'pwd': transport['pwd'],
-                'fingerprints': transport['fingerprints'],
-                'candidates': transport['candidates'],
+        return {
+            '_': 'get_join',
+            'data': {
+                'chat_id': data['chat']['id'],
+                'transport': {
+                    'ufrag': transport['ufrag'],
+                    'pwd': transport['pwd'],
+                    'fingerprints': transport['fingerprints'],
+                    'candidates': transport['candidates'],
+                },
             },
-        },
-    }
+        }
 
 
-async def websocket_handler(request):
-    ws = web.WebSocketResponse()
-    await ws.prepare(request)
+    async def websocket_handler(request):
+        ws = web.WebSocketResponse()
+        await ws.prepare(request)
 
-    async for msg in ws:
-        if msg.type == WSMsgType.TEXT:
-            try:
-                data = json.loads(msg.data)
-            except JSONDecodeError:
-                await ws.close()
-                break
+        async for msg in ws:
+            if msg.type == WSMsgType.TEXT:
+                try:
+                    data = json.loads(msg.data)
+                except JSONDecodeError:
+                    await ws.close()
+                    break
 
-            response = None
-            if data['_'] == 'join':
-                response = await join_call(data['data'])
+                response = None
+                if data['_'] == 'join':
+                    response = await join_call(data['data'])
 
-            if response is not None:
-                await ws.send_json(response)
+                if response is not None:
+                    await ws.send_json(response)
 
-    return ws
+        return ws
 
 
-def main():
-    app = web.Application()
-    app.router.add_route('GET', '/', websocket_handler)
-    web.run_app(app, port=os.environ.get("PORT", 6969))
+    def main():
+        app = web.Application()
+        app.router.add_route('GET', '/', websocket_handler)
+        web.run_app(app, port=os.environ.get("PORT", 6969))
 
-if udB.get("VC_SESSION"):
     vcbot.start()
     main()
