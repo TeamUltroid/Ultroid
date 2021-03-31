fc2f2cd159c7855cc9b71d6cdfb94e8c873d055c
Aditya <me@xditya.me>
2021-03-31 11:57:20 +0530

    Update save.py

--- plugins/save.py
+++ plugins/save.py
@@ -18,9 +18,9 @@ from . import *
 async def saf(e):
      x = await e.get_reply_message()
      if not x:
-         return await eor(e, "Reply to Any Message to save it to ur saved messages")
+         return await eod(e, "Reply to Any Message to save it to ur saved messages", time=5)
      await e.forward_to("me")
-     await eor(e, "Message saved at saved messages")
+     await eod(e, "Message saved at saved messages", time=5)
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
