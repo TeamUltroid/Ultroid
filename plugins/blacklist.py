9ab008b2d731b5382eb4855b03ec3515dcf15040
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-04-01 00:40:50 +0530

    Update blacklist.py

--- plugins/blacklist.py
+++ plugins/blacklist.py
@@ -75,4 +75,4 @@ async def bl(e):
                     pass
 
 
-HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=Var.HNDLR)}"})
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
