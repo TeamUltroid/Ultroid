fb3cb9c5dd230353588c6884118c4b4a191c8bdc
Danish <danish@ultroid.tech>
2021-04-02 09:13:39 +0530

    Fixes

--- plugins/glitch.py
+++ plugins/glitch.py
@@ -27,12 +27,12 @@ async def _(e):
     xx = await eor(e, "`Gliching...`")
     ok = await bot.download_media(reply.media)
     cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{ok}' ult.gif"
-    process = await asyncio.create_subprocess_exec(
+    process = await asyncio.create_subprocess_shell(
         cmd,
         stdout=asyncio.subprocess.PIPE,
         stderr=asyncio.subprocess.PIPE,
     )
-    await process.communicate()
+    stdout, stderr = await process.communicate()
     await ultroid_bot.send_file(
         e.chat_id, "ult.gif", force_document=False, reply_to=reply
     )
