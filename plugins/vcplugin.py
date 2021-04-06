a366a8c25f4ac3cf351ff390805dc680983187be
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-04-06 15:53:39 +0530

    Update vcplugin.py

--- plugins/vcplugin.py
+++ plugins/vcplugin.py
@@ -5,6 +5,8 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
+"""
+"""
 
 from pyUltroid.functions.vc_sudos import (add_vcsudo, del_vcsudo, get_vcsudos,
                                           is_vcsudo)
