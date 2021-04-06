ae8f592b23a4b9e7c27451e1b3aa4c21aed84117
Aditya <xditya@ultroid.tech>
2021-04-06 19:08:08 +0530

    go suk danish

--- plugins/updater.py
+++ plugins/updater.py
@@ -11,7 +11,7 @@ from os import execl, path, remove
 
 from git import Repo
 from git.exc import GitCommandError, InvalidGitRepositoryError, NoSuchPathError
-from . import get_string, ultroid_version, HELP
+from . import get_string, ultroid_version
 
 UPSTREAM_REPO_URL = "https://github.com/TeamUltroid/Ultroid"
 requirements_path = path.join(
