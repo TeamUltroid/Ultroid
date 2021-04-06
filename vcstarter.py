6f5f61b2bde268f59be56016ed192a77b480e042
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-04-06 22:14:35 +0530

    @arnab431 check

--- vcstarter.py
+++ vcstarter.py
@@ -10,6 +10,7 @@ from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.phone import (GetGroupCallRequest,
                                          JoinGroupCallRequest,
                                          LeaveGroupCallRequest)
+from telethon.errors import ChannelPrivateError
 from telethon.tl.types import DataJSON
 
 bot = TelegramClient(None, Var.API_ID, Var.API_HASH).start(bot_token=Var.BOT_TOKEN)
@@ -72,6 +73,9 @@ if vcbot:
                 Var.LOG_CHANNEL,
                 f"`Joined Voice Chat in {(await bot.get_entity(data['chat']['id'])).title}`",
             )
+        except ChannelPrivateError:
+            str = (await vcbot.get_me()).first_name
+            return await bot.send_message(data["chat"]["id"], f"Please add {str} in this group.`")
         except Exception as ex:
             return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
 
@@ -90,36 +94,38 @@ if vcbot:
             },
         }
 
-    async def leave_vc(data):
-        await bot.send_message(Var.LOG_CHANNEL, "Received Leave Request")
-        try:
-            await get_entity(data["chat"]["id"])
-        except Exception as ex:
-            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
-        try:
-            full_chat = await vcbot(GetFullChannelRequest(chat))
-        except Exception as ex:
-            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
-        try:
-            call = await vcbot(GetGroupCallRequest(full_chat.full_chat.call))
-        except:
-            call = None
 
-        try:
-            result = await vcbot(
-                LeaveGroupCallRequest(
-                    call=call.call,
-                    source=data["source"],
-                ),
-            )
-            await bot.send_message(
-                Var.LOG_CHANNEL,
-                f"`Left Voice Chat in {(await bot.get_entity(data['chat']['id'])).title}`",
-            )
-        except Exception as ex:
-            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+#    async def leave_vc(data):
+#        await bot.send_message(Var.LOG_CHANNEL, "Received Leave Request")
+#        try:
+#            await get_entity(data["chat"]["id"])
+#        except Exception as ex:
+#            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+#        try:
+#            full_chat = await vcbot(GetFullChannelRequest(chat))
+#        except Exception as ex:
+#            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+#        try:
+#            call = await vcbot(GetGroupCallRequest(full_chat.full_chat.call))
+#        except:
+#            call = None
+#
+#        try:
+#            result = await vcbot(
+#                LeaveGroupCallRequest(
+#                    call=call.call,
+#                    source=data["source"],
+#                ),
+#            )
+#            await bot.send_message(
+#                Var.LOG_CHANNEL,
+#                f"`Left Voice Chat in {(await bot.get_entity(data['chat']['id'])).title}`",
+#            )
+#        except Exception as ex:
+#            return await bot.send_message(data["chat"]["id"], "`" + str(ex) + "`")
+#
+#        return {"_": "left_vc", "data": {"chat_id": data["chat"]["id"]}}
 
-        return {"_": "left_vc", "data": {"chat_id": data["chat"]["id"]}}
 
     async def websocket_handler(request):
         ws = web.WebSocketResponse()
@@ -137,8 +143,8 @@ if vcbot:
                 if data["_"] == "join":
                     response = await join_call(data["data"])
 
-                if data["_"] == "leave":
-                    response = await leave_vc(data["data"])
+#                if data["_"] == "leave":
+#                    response = await leave_vc(data["data"])
 
                 if response is not None:
                     await ws.send_json(response)
