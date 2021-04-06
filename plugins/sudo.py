113f71614d7429af2d8c6c8aca18789bc69d0f9f
Danish <danish@ultroid.tech>
2021-04-06 11:47:24 +0530

    Update sudo.py

--- plugins/sudo.py
+++ plugins/sudo.py
@@ -214,7 +214,9 @@ async def _(ult):
         else:
             msg += f"• `{i}` -> Invalid User\n"
     m = udB.get("SUDO") if udB.get("SUDO") else "False"
-    return await ok.edit(f"**SUDO MODE : {m}\n\nList of SUDO Users :**\n{msg}")
+    if m == "False":
+        m = "[False](https://telegra.ph/Ultroid-04-06)"
+    return await ok.edit(f"**SUDO MODE : {m}\n\nList of SUDO Users :**\n{msg}", link_preview = False)
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
