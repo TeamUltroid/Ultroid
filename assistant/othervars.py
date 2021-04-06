18a7eb9cdfd8dad08fc466aa6f527813fc8392c1
Danish <danish@ultroid.tech>
2021-04-06 20:02:10 +0530

    Update othervars.py

--- assistant/othervars.py
+++ assistant/othervars.py
@@ -853,3 +853,32 @@ async def name(event):
                 ),
                 buttons=get_back_button("vcb"),
             )
+
+@callback("vcw")
+@owner
+async def name(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "WEBSOCKET_URL"
+    name = "WEBSOCKET URL"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(
+            "**WEBSOCKET URL**\nEnter your websocket url means\n`https://{HEROKU_APP_NAME}.herokuapp.com`\nIn place of HEROKU_APP_NAME put ur heroku app name\n\nUse /cancel to terminate the operation.",
+        )
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message(
+                "Cancelled!!",
+                buttons=get_back_button("vcb"),
+            )
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message(
+                "{} changed to {}\n\nAfter Setting All Things Do restart".format(
+                    name,
+                    themssg,
+                ),
+                buttons=get_back_button("vcb"),
+            )
