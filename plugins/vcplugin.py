da8d667fa6f8bf1de5150953894943c66b4e025c
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-04-06 15:52:24 +0530

    Add help menu

--- plugins/vcplugin.py
+++ plugins/vcplugin.py
@@ -1,3 +1,11 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+
 from pyUltroid.functions.vc_sudos import (add_vcsudo, del_vcsudo, get_vcsudos,
                                           is_vcsudo)
 from telethon.tl.functions.channels import GetFullChannelRequest as getchat
