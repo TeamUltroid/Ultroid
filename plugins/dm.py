a2b36993f680fa6376cf48dd629cd5ad28a29264
Danish <danish@ultroid.tech>
2021-04-14 15:09:40 +0530

    K

--- plugins/dm.py
+++ plugins/dm.py
@@ -16,8 +16,9 @@ from . import *
 
 @ultroid_cmd(pattern="dm ?(.*)")
 async def dm(e):
-    if not e.text[3] == " ":  # weird fix
-        return
+    if len(event.text) > 3:
+        if not event.text[3] == " ":  # weird fix
+            return
     d = e.pattern_match.group(1)
     c = d.split(" ")
     try:
