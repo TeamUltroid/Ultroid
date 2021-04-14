110719697056fe53bc57b885d893fb1f2cda5e6b
Danish <danish@ultroid.tech>
2021-04-14 13:45:13 +0530

    Update glitch.py

--- plugins/glitch.py
+++ plugins/glitch.py
@@ -24,6 +24,9 @@ async def _(e):
     reply = await e.get_reply_message()
     if not (reply and reply.media):
         return await eor(e, "Reply to any media")
+    wut = mediainfo(reply.media)
+    if not wut.startswith(("pic", "sticker")):
+        return await eor("`Unsupported Media`")
     xx = await eor(e, "`Gliching...`")
     ok = await bot.download_media(reply.media)
     cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{ok}' ult.gif"
