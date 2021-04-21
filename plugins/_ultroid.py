164997a5cb326028b0a81586afee0df4369c667f
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-04-21 23:59:59 +0530

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -7,6 +7,7 @@
 
 
 from telethon.errors import ChatSendInlineForbiddenError
+from telethon.errors.rpcerrorlist import BotMethodInvalidError as bmi
 
 from . import *
 
@@ -24,5 +25,5 @@ async def repify(e):
         await q[0].click(e.chat_id)
         if e.sender_id == ultroid_bot.uid:
             await e.delete()
-    except ChatSendInlineForbiddenError:
+    except ChatSendInlineForbiddenError or bmi:
         await eor(e, REPOMSG)
