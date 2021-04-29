27ab03bf16898dceb9f1b8be6394df76c832b55d
buddhhu <buddhhu@ultroid.tech>
2021-04-29 18:44:48 +0000

    pylint: auto fixes

--- plugins/dayevents.py
+++ plugins/dayevents.py
@@ -12,29 +12,31 @@
     `Get Event of the Day`
 """
 
-from . import *
 import re
+
 import requests as r
 from bs4 import BeautifulSoup as bs
 
+from . import *
 
-@ultroid_cmd(pattern='eod ?(.*)')
+
+@ultroid_cmd(pattern="eod ?(.*)")
 async def diela(e):
-	match = e.pattern_match.group(1) 
-	m = await eor(e, '`Processing... `') 
-	li = "https://daysoftheyear.com"
-	te = "♻️ **Events of the Day**\n\n"
-	if match:
-		date = match.split('/')[0]
-		month = match.split('/')[1]
-		li += '/days/2021/' + month + '/' + date
-		te = f"♻️ **Events for {match}/2021**\n\n"
-	ct = r.get(li).content
-	bt = bs(ct, 'html.parser', from_encoding='utf-8')
-	ml = bt.find_all('a', 'js-link-target', href=re.compile('daysoftheyear.com/days')) 
-	for eve in ml[:5]:
-		te += '• ' + f'[{eve.text}]({eve["href"]})\n'
-	await m.edit(te, link_preview=False)
-	
+    match = e.pattern_match.group(1)
+    m = await eor(e, "`Processing... `")
+    li = "https://daysoftheyear.com"
+    te = "♻️ **Events of the Day**\n\n"
+    if match:
+        date = match.split("/")[0]
+        month = match.split("/")[1]
+        li += "/days/2021/" + month + "/" + date
+        te = f"♻️ **Events for {match}/2021**\n\n"
+    ct = r.get(li).content
+    bt = bs(ct, "html.parser", from_encoding="utf-8")
+    ml = bt.find_all("a", "js-link-target", href=re.compile("daysoftheyear.com/days"))
+    for eve in ml[:5]:
+        te += "• " + f'[{eve.text}]({eve["href"]})\n'
+    await m.edit(te, link_preview=False)
+
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
