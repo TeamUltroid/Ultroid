02577d0b8048119a30be56ef0220b0cf39c44acb
buddhhu <buddhhu@ultroid.tech>
2021-04-30 07:00:46 +0000

    pylint: auto fixes

--- plugins/updater.py
+++ plugins/updater.py
@@ -12,14 +12,24 @@
 
 from . import *
 
-@ultroid_cmd(
-  pattern="update$")
+
+@ultroid_cmd(pattern="update$")
 async def _(e):
-  x = await updater()
-  if x is not None:
-    await eor(e, f'<strong><a href="t.me/c/{x.peer_id.channel_id}/{x.id}">[ChangeLogs]</a></strong>', parse_mode='html', link_preview=False)
-  else:
-    await eor(e, '<code>Your BOT is </code><strong>up-to-date</strong> with <strong><a href="https://github.com/TeamUltroid/Ultroid/tree/dev">[Dev]</a></strong>', parse_mode='html', link_preview=False)
+    x = await updater()
+    if x is not None:
+        await eor(
+            e,
+            f'<strong><a href="t.me/c/{x.peer_id.channel_id}/{x.id}">[ChangeLogs]</a></strong>',
+            parse_mode="html",
+            link_preview=False,
+        )
+    else:
+        await eor(
+            e,
+            '<code>Your BOT is </code><strong>up-to-date</strong> with <strong><a href="https://github.com/TeamUltroid/Ultroid/tree/dev">[Dev]</a></strong>',
+            parse_mode="html",
+            link_preview=False,
+        )
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
