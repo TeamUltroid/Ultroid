d155170e83eaaea4ceb2343c9c03f0f6f5c2d3f9
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-01 13:37:51 +0530

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -14,6 +14,7 @@ from . import *
 REPOMSG = (
     "• **ULTROID USERBOT** •\n\n",
     "• Repo - [Click Here](https://github.com/TeamUltroid/Ultroid)\n",
+    "• Addons - [Click Here](https://github.com/TeamUltroid/UltroidAddons)\n",
     "• Support - @UltroidSupport",
 )
 
