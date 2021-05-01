cc2e162b24e1105b8d8fefd583a56a6b759aec12
Akash Pattnaik <akashjio66666@gmail.com>
2021-05-01 11:26:25 +0530

    added console log

--- ecmaHelper/eval.d.js
+++ ecmaHelper/eval.d.js
@@ -5,17 +5,21 @@ console.log("Command -> `" + String(process.argv.slice(2)).replace(',', ' ').rep
 
 truncate('./ecmaHelper/evalJs.run.js', 0, function() { 
     console.log('Script File Truncated');
-    writeFileSync('./ecmaHelper/evalJs.run.js', String(process.argv.slice(2)), () => {});
-
 });
+
 truncate('./ecmaHelper/evalJs.result.d.txt', 0, function() { 
     console.log('Result File Truncated');
 });
+
+
+writeFileSync('./ecmaHelper/evalJs.run.js', String(process.argv.slice(2)), () => {});
+console.log('written script');
 
 const evalJs = exec('node ./ecmaHelper/evalJs.run.js');
 
 evalJs.stdout.on('data', (data) => {
     console.log(data)
+    console.log('here')
     appendFileSync('./ecmaHelper/evalJs.result.d.txt', `${data.toString()}\n`, () => {});
 })
 
