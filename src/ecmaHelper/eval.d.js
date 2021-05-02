77491bf73b26ed41902fc455242d621706f5482d
Akash Pattnaik <akashjio66666@gmail.com>
2021-05-02 09:03:20 +0530

    shit

--- src/ecmaHelper/eval.d.js
+++ src/ecmaHelper/eval.d.js
@@ -1,7 +1,7 @@
 const { exec } = require('child_process');
 const { appendFile, truncate } = require('fs');
 (async () => {
-    const evalJs = exec('node ./ecmaHelper/evalJs.run.js');
+    const evalJs = exec('node ./src/ecmaHelper/evalJs.run.js');
     
     truncate('./src/ecmaHelper/evalJs.result.d.txt', 0, function() { 
         console.log('Result File Truncated');
