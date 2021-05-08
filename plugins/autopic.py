a50b021da2174933b7189f18b1040f6c785fc71d
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-08 23:32:41 +0530

    Silence pls

--- plugins/autopic.py
+++ plugins/autopic.py
@@ -38,10 +38,10 @@ async def autopic(e):
     await eor(e, get_string("autopic_3").format(search))
     udB.set("AUTOPIC", "True")
     while True:
-        ge = udB.get("AUTOPIC")
-        if not ge == "True":
-            return
         for lie in clls:
+            ge = udB.get("AUTOPIC")
+            if not ge == "True":
+                return
             au = "https://unsplash.com" + lie["href"]
             et = await ultroid_bot(getweb(au))
             try:
@@ -56,7 +56,7 @@ async def autopic(e):
             file = await ultroid_bot.upload_file(kar)
             await ultroid_bot(UploadProfilePhotoRequest(file))
             os.remove(kar)
-            await asyncio.sleep(1100)
+            await asyncio.sleep(1111) #shubh ank
 
 
 @ultroid_cmd(pattern="stoppic$")
