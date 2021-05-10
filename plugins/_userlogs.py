eecaf630a71293bf35421d383e2c71c60677c313
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-10 22:41:42 +0530

    Update _userlogs.py

--- plugins/_userlogs.py
+++ plugins/_userlogs.py
@@ -161,7 +161,7 @@ async def leave_ch_at(event):
     cht = event.data_match.group(1).decode("UTF-8")
     ch_id, client = cht.split("|")
     if client == "bot":
-        await asst.delete_dialog(ch_id)
+        await asst.delete_dialog(int(ch_id))
     elif client == "user":
-        await ultroid.delete_dialog(ch_id)
+        await ultroid.delete_dialog(int(ch_id))
     await event.edit(f"Left `{ch_id}`")
