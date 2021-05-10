ac326adbc93a66a5111a4770017da425c55e7890
Akash Pattnaik <akashjio66666@gmail.com>
2021-05-10 19:47:39 +0530

    #proness

--- src/handlers/play.ts
+++ src/handlers/play.ts
@@ -21,7 +21,7 @@ export const playHandler = Composer.command('play', async (ctx) => {
         return await ctx.reply('I can only play in groups.');
     }
     try {
-        if (ctx.message.reply_to_message && JSON.parse(ctx.message.reply_to_message).audio) {
+        if (ctx.message.reply_to_message && ctx.message.reply_to_message["audio"]) {
             await ctx.reply(JSON.stringify(ctx.message.reply_to_message));
         } else {
             await ctx.reply("Its Not A Audio File...");
