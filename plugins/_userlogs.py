f33266fd0229a85c4675c6a7bcb2091abcc9b9a8
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-13 20:09:00 +0530

    Behtar

--- plugins/_userlogs.py
+++ plugins/_userlogs.py
@@ -109,23 +109,13 @@ async def when_asst_added_to_chat(event):
         user = await event.get_user()
         chat = (await event.get_chat()).title
         tmp = event.added_by
-        if user.id == asst.me.id:
-            if int(tmp.id) == OWNER_ID or str(tmp.id) in (Redis("SUDOS").split(" ")):
+        if user.is_self:
                 buttons = Button.inline(
                     "Leave Chat", data=f"leave_ch_{event.chat_id}|bot"
                 )
                 return await asst.send_message(
                     int(udB.get("LOG_CHANNEL")),
-                    f"#ADD_LOG\n\nSudo Users added me to {chat}.",
-                    buttons=buttons,
-                )
-            else:
-                buttons = Button.inline(
-                    "Leave Chat", data=f"leave_ch_{event.chat_id}|bot"
-                )
-                return await asst.send_message(
-                    int(udB.get("LOG_CHANNEL")),
-                    f"#ADD_LOG\n\n`{tmp.id}` added me to {chat}.",
+                    f"#ADD_LOG\n\n[{tmp.first_name}](tg://user?id={tmp.id}) added [{user.first_name}](tg://user?id={user.id}) to {chat}.",
                     buttons=buttons,
                 )
 
@@ -139,21 +129,21 @@ async def when_ultd_added_to_chat(event):
         user = await event.get_user()
         chat = (await event.get_chat()).title
         tmp = event.added_by
-        if user.id == OWNER_ID:
+        if user.is_self:
             buttons = Button.inline("Leave Chat", data=f"leave_ch_{event.chat_id}|user")
             return await asst.send_message(
                 int(udB.get("LOG_CHANNEL")),
-                f"#ADD_LOG\n\n`{tmp.id}` just added you to {chat}.",
+                f"#ADD_LOG\n\n[{tmp.first_name}](tg://user?id={tmp.id}) just added [{user.first_name}](tg://user?id={user.id}) to {chat}.",
                 buttons=buttons,
             )
     elif event.user_joined:
         user = await event.get_user()
         chat = (await event.get_chat()).title
-        if user.id == OWNER_ID:
+        if user.is_self:
             buttons = Button.inline("Leave Chat", data=f"leave_ch_{event.chat_id}|user")
             return await asst.send_message(
                 int(udB.get("LOG_CHANNEL")),
-                f"#JOIN_LOG\n\nYou just joined {chat}.",
+                f"#JOIN_LOG\n\n[{user.first_name}](tg://user?id={user.id}) just joined {chat}.",
                 buttons=buttons,
             )
 
