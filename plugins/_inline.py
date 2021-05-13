e235a8dbb1b8999acb07d3eada1b33409ba4c332
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-13 05:50:44 +0000

    pylint: auto fixes

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -5,7 +5,6 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-import random
 import re
 import time
 from datetime import datetime
