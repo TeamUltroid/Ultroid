758aedf9122135e5a42a08b0ab627195b7285c19
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-14 09:26:52 +0530

    Update antiflood.py

--- plugins/antiflood.py
+++ plugins/antiflood.py
@@ -26,12 +26,14 @@ from pyUltroid.functions.antiflood_db import get_flood_limit, rem_flood, set_flo
     pattern="setflood ?(.*)",
 )
 async def set_flood(e):
-    input = int(e.pattern_match.group(1))
+    input = e.pattern_match.group(1)
     if not input:
-        return await eor(e, "`What?`")
-    m = setflood(e.chat_id, input)
+        return await eod(e, "`What?`")
+    if not isinstance(input, int):
+        return await eod(e, "Gwan Myad?")
+    m = set_flood(e.chat_id, input)
     if m:
-        return await eor(
+        return await eod(
             e, f"`Successfully Updated Antiflood Settings to {input} in this chat.`"
         )
 
@@ -42,7 +44,9 @@ async def set_flood(e):
 async def remove_flood(e):
     hmm = rem_flood(e.chat_id)
     if hmm is True:
-        return await eor(e, "Antiflood Settings Disabled>`")
+        return await eod(e, "Antiflood Settings Disabled>`")
+    else:
+        await eod(e, "`No flood limits in this chat.`")
 
 
 @ultroid_cmd(
@@ -51,9 +55,9 @@ async def remove_flood(e):
 async def get_flood(e):
     ok = get_flood_limit(e.chat_id)
     if ok:
-        return await eor(e, f"`Flood limit for this chat is {ok}.`")
+        return await eod(e, f"`Flood limit for this chat is {ok}.`")
     else:
-        await eor(e, "`No flood limits in this chat.`")
+        await eod(e, "`No flood limits in this chat.`")
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
