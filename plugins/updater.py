5572faec234a231bb42eec165179a7d366ab867e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-15 16:47:31 +0530

    Update updater.py

--- plugins/updater.py
+++ plugins/updater.py
@@ -4,6 +4,7 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 """
 ✘ Commands Available -
 • `{i}update`
