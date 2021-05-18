b1fd53b757e0794fd2b10a6dafa66697587ddce6
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-18 17:25:00 +0530

    Create pinterest.py

---
+++ plugins/pinterest.py
@@ -0,0 +1,51 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available -
+
+• `{i}pntrst <link/id>`
+    Download and send pinterest pins.
+"""
+
+
+from requests import get
+from bs4 import BeautifulSoup as bs
+from urllib.request import urlretrieve as donl
+import os
+from . import *
+
+_base = "https://pinterestdownloader.com/download?url="
+
+def gib_link(link):
+    colon = "%3A"
+    slash = "%2F"
+    if link.startswith('https'):
+        return _base + link.replace(':', colon).replace('/', back_slash)
+    else:
+        return _base + f"https{colon}{slash}{slash}pin.it{slash}{link}"
+
+@ultroid_cmd(
+pattern="pntrst ?(.*)",
+)
+async def pinterest(e):
+    m = e.pattern_match.group(1)
+    get_link = get(gib_link(m)).text
+    hehe = bs(get_link, 'html.parser')
+    hulu = hehe.find_all('a', {'class':'download_button'})
+    if len(hulu) > 1:
+        donl(hulu[0]['href'], "pinterest.mp4")
+        donl(hulu[1]['href'], "pinterest.jpg")
+        await e.delete()
+        await e.client.send_file(e.chat_id, "pinterest.mp4", thumb="pinterest.jpg", caption=f"Pin:- {m}")
+        os.remove("pinterest.mp4")
+        os.remove("pinterest.jpg")
+    else:
+        await e.delete()
+        await e.client.send_file(e.chat_id, hulu[0]['href'], caption=f"Pin:- {m}")
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
