daa09b4c80ac882209afc67295ed2cb760178b05
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-19 09:55:42 +0530

    Fix

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -22,7 +22,7 @@ from . import *
 
 # ================================================#
 notmine = f"This bot is for {OWNER_NAME}"
-ULTROID_PIC = "resources/extras/teamultroid.jpg"
+ULTROID_PIC = "https://telegra.ph/file/115f149ed8e154641708b.jpg"
 helps = get_string("inline_1")
 
 add_ons = udB.get("ADDONS")
