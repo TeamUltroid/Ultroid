920e89ac6d1fe528b7fdb1a0ec0f0998f3362a70
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-05-19 19:07:25 +0530

    Create locks.py

---
+++ plugins/locks.py
@@ -0,0 +1,53 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available -
+
+• `{i}lock <msgs/media/sticker/gif/games/inline/polls/invite/pin/changeinfo>
+    Lock the Used Setting in Used Group.
+
+• `{i}unlock <msgs/media/sticker/gif/games/inline/polls/invite/pin/changeinfo>
+    UNLOCK the Used Setting in Used Group.
+    
+"""
+
+from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
+from pyUltroid.functions.all import lucks, unlucks
+
+
+@ultroid_cmd(pattern='lock ?(.*)',
+groups_only=True,
+admins_only=True)
+async def lockho(e):
+    mat = e.pattern_match.group(1)
+    if not mat:
+        return await eod(e, '`What to Lock  ?`')
+    try:
+        ml = lucks(mat)
+    except:
+        return await eod(e, '`Incorrect Input`')
+    await ultroid_bot(EditChatDefaultBannedRightsRequest(e.chat_id, ml)) 
+    await eor(e,f'Locked - `{mat}` ! ')
+
+
+@ultroid_cmd(pattern='unlock ?(.*)',
+groups_only=True,
+admins_only=True)
+async def unlckho(e):
+    mat = e.pattern_match.group(1)
+    if not mat:
+        return await eod(e, '`What to Lock  ?`')
+    try:
+        ml = unlucks(mat)
+    except:
+        return await eod(e, '`Incorrect Input`')
+    await ultroid_bot(EditChatDefaultBannedRightsRequest(e.chat_id, ml)) 
+    await eor(e, f'Unlocked : {mat}!`')
+
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
