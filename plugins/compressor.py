3ad979e6ad537e940ba836ec18ea36a5f1ece545
Danish <danish@ultroid.tech>
2021-05-20 13:28:18 +0530

    Update compressor.py

--- plugins/compressor.py
+++ plugins/compressor.py
@@ -51,15 +51,8 @@ async def _(e):
             await xxx.edit(
                 f"`Downloaded {file.name} of {humanbytes(o_size)} in {diff}.\nNow Compressing...`"
             )
-            cmds = [
-                "ffmpeg -i",
-                file.name,
-                "-preset ultrafast -c:v -vcodec libx265 -crf",
-                crf,
-                "-map 0:v -c:a aac -map 0:a -c:s copy -map 0:s?",
-                file_name + "_compressed.mp4",
-            ]
-            await bash(*cmds)
+            cmd = f'ffmpeg -i "{file.name}" -preset ultrafast -c:v -vcodec libx265 -crf {crf} -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{file_name}_compressed.mkv"'
+            await bash(cmd)
             c_size = os.path.getsize(f"{file_name}_compressed.mp4")
             f_time = time.time()
             difff = time_formatter((f_time - d_time) * 1000)
