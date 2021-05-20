32ecadd493bfc47e9b6ff5b5260d0672a82f3732
Danish <danish@ultroid.tech>
2021-05-20 12:44:29 +0530

    Update compressor.py

--- plugins/compressor.py
+++ plugins/compressor.py
@@ -1,25 +1,42 @@
+# Ultroid - UserBot
+# Copyright (C) 2020 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+"""
+✘ Commands Available -
+
+•`{i}compress`
+
+"""
+
 import os
 import time
+from datetime import datetime as dt
 
 from . import *
 
 
-@ultroid_cmd(pattern="compress ?(.*)")
+@ultroid_cmd(pattern="compress$")
 async def _(e):
     xxx = await eor(e, "`Trying To Download...`")
-    crf = e.pattern_match.group(1)
-    if not crf:
-        crf = 28
-    if e.reply_to_msg_id:
-        vido = await e.get_reply_message()
-        if (
-            vido.document.mime_type == "video/mp4"
-            or vido.document.mime_type == "video/x-matroska"
-        ):
+    vido = await e.get_reply_message()
+    if video and video.media:
+        if "video" in mediainfo(vido.media):
+            if hasattr(vido.media, "document"):
+                vfile = vido.media.document
+                name = vido.file.name
+            else:
+                vfile = vido.media
+                name = ""
+            if not name:
+                name = "video_" + dt.now().isoformat("_", "seconds") + ".mp4"
             c_time = time.time()
             file = await downloader(
-                "resources/downloads/" + vido.file.name,
-                vido.media.document,
+                "resources/downloads/" + name,
+                vfile,
                 xxx,
                 c_time,
                 "Downloading " + vido.file.name + "...",
@@ -31,29 +48,24 @@ async def _(e):
             await xxx.edit(
                 f"`Downloaded {file.name} of {humanbytes(o_size)} in {diff}.\nNow Compressing...`"
             )
-            cmds = [
-                "ffmpeg -i",
-                file.name,
-                "-preset ultrafast -vcodec libx265 -crf",
-                crf,
-                file_name + "-compressed.mp4",
-            ]
-            await bash(*cmds)
-            c_size = os.path.getsize(f"{file_name}-compressed.mp4")
+            await bash(
+                f'ffmpeg -i "{file.name}" -preset ultrafast -c:v libx265 -crf 27 -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? "{file_name}_compressed.mkv"'
+            )
+            c_size = os.path.getsize(f"{file_name}-compressed.mkv")
             f_time = time.time()
             difff = time_formatter((f_time - d_time) * 1000)
             await xxx.edit(
                 f"`Compressed {humanbytes(o_size)} to {humanbytes(c_size)} in {difff}\nTrying to Upload...`"
             )
             differ = 100 - ((c_size / o_size) * 100)
-            caption = f"`File: ``{file_name}-compressed.mp4`\n"
+            caption = f"`File: ``{file_name}-compressed.mkv`\n"
             caption += f"`Original Size: ``{humanbytes(o_size)}`\n"
             caption += f"`Compressed Size: ``{humanbytes(c_size)}`\n"
             caption += f"`Compression Ratio: ``{differ:.2f}%`\n"
             caption += f"`Time Taken To Compress: ``{difff}`"
             mmmm = await uploader(
-                f"{file_name}-compressed.mp4",
-                f"{file_name}",
+                f"{file_name}_compressed.mkv",
+                f"{file_name}_compressed.mkv",
                 f_time,
                 xxx,
                 "Uploading " + file_name + "...",
@@ -61,8 +73,11 @@ async def _(e):
             await e.client.send_file(
                 e.chat_id,
                 mmmm,
-                thumb="resources/extras/new_thumb.jpg",
+                thumb="resources/extras/ultroid.jpg",
                 caption=caption,
                 force_document=True,
             )
             await xxx.delete()
+
+
+HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
