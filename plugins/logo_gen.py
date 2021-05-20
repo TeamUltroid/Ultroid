46009c8d05ba81e43f34699ecdcf270226222958
Aditya <xditya@ultroid.tech>
2021-05-20 11:41:27 +0530

    ok

--- plugins/logo_gen.py
+++ plugins/logo_gen.py
@@ -13,14 +13,12 @@
 
 """
 
-import os
 import random
-import time
-
+import os
 from PIL import Image, ImageDraw, ImageFont
-
 from . import *
-
+import time
+from telethon.tl.types import InputMessagesFilterPhotos
 
 @ultroid_cmd(pattern="logo ?(.*)")
 async def logo_gen(event):
@@ -29,11 +27,13 @@ async def logo_gen(event):
     if not name:
         await eod(xx, "`Give a name too!`", time=5)
     fpath_ = "./resources/fonts/"
-    bpath_ = "./resources/bgs/"
     f = random.choice(os.listdir(fpath_))
-    bg = random.choice(os.listdir(bpath_))
+    pics = []
+    async for i in ultroid.iter_messages("@UltroidLogos", filter=InputMessagesFilterPhotos):
+        pics.append(i)
+    id_ = random.choice(pics)
+    bg_ = await id_.download_media()
     font_ = fpath_ + f
-    bg_ = bpath_ + bg
     # next level logic, ignore
     if len(name) < 8:
         fnt_size = 150
@@ -48,33 +48,22 @@ async def logo_gen(event):
     draw = ImageDraw.Draw(img)
     font = ImageFont.truetype(font_, fnt_size)
     w, h = draw.textsize(name, font=font)
-    h += int(h * 0.21)
+    h += int(h*0.21)
     image_width, image_height = img.size
-    draw.text(
-        ((image_width - w) / 2, (image_height - h) / 2),
-        name,
-        font=font,
-        fill=(255, 255, 255),
-    )
-    x = (image_width - w) / 2
-    y = (image_height - h) / 2
-    draw.text(
-        (x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black"
-    )
+    draw.text(((image_width-w)/2, (image_height-h)/2), name, font=font, fill=(255, 255, 255))
+    x = (image_width-w)/2
+    y = (image_height-h)/2
+    draw.text((x, y), name, font=font, fill="white", stroke_width=strke, stroke_fill="black")
     flnme = f"ultd.png"
     img.save(flnme, "png")
     await xx.edit("`Done!`")
     if os.path.exists(flnme):
         tt = time.time()
         up = await uploader(flnme, flnme, tt, xx, "Uploading...")
-        await ultroid.send_file(
-            event.chat_id,
-            file=up,
-            caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})",
-            foce_document=True,
-        )
+        await ultroid.send_file(event.chat_id, file=up, caption=f"Logo by [{OWNER_NAME}](tg://user?id={OWNER_ID})", foce_document=True)
         os.remove(flnme)
         await xx.delete()
-
+    if os.path.exists(bg_):
+        os.remove(bg_)
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
