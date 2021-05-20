0f6d15ed703427973a799521c9c1bf9d02c90eb9
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-20 14:44:28 +0530

    Chal ja mc

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -66,7 +66,7 @@ async def _(event):
     OUT = f"**☞ BASH\n\n• COMMAND:**\n`{cmd}` \n\n"
     if stderr:
         OUT += f"**• ERROR:** \n`{stderr}`\n"
-    elif not (stderr and stdout):
+    elif not stdout:
         OUT += f"**• OUTPUT:**\n`Success`"
     else:
         _o = stdout.split("\n")
