bf52cdf617a15491e618183564ab024a255c11ff
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-20 11:26:57 +0000

    pylint: auto fixes

--- plugins/compressor.py
+++ plugins/compressor.py
@@ -12,9 +12,9 @@
 
 """
 
+import asyncio
 import os
 import time
-import asyncio
 from datetime import datetime as dt
 
 from . import *
@@ -53,31 +53,37 @@ async def _(e):
             await xxx.edit(
                 f"`Downloaded {file.name} of {humanbytes(o_size)} in {diff}.\nNow Compressing...`"
             )
-            x, y = await bash(f'mediainfo --fullscan """{file.name}""" | grep "Frame count"')
-            total_frames = x.split(":")[1].split('\n')[0]
+            x, y = await bash(
+                f'mediainfo --fullscan """{file.name}""" | grep "Frame count"'
+            )
+            total_frames = x.split(":")[1].split("\n")[0]
             progress = "progress.txt"
             with open(progress, "w") as fk:
                 pass
-            proce = await asyncio.create_subprocess_shell(f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{file.name}""" -preset ultrafast -c:v libx265 -crf {crf} -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y', stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE)
+            proce = await asyncio.create_subprocess_shell(
+                f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{file.name}""" -preset ultrafast -c:v libx265 -crf {crf} -map 0:v -c:a aac -map 0:a -c:s copy -map 0:s? """{out}""" -y',
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
             while proce.returncode != 0:
                 await asyncio.sleep(3)
-                with open(progress, 'r+') as fil:
+                with open(progress, "r+") as fil:
                     text = fil.read()
-                    frames=re.findall("frame=(\d+)", text)
-                    size=re.findall("total_size=(\d+)", text)
+                    frames = re.findall("frame=(\\d+)", text)
+                    size = re.findall("total_size=(\\d+)", text)
 
                     if len(frames):
                         elapse = int(frames[-1])
                     if len(size):
                         size = int(size[-1])
-                        per = elapse*100/int(total_frames)
+                        per = elapse * 100 / int(total_frames)
                         progress_str = "|[{0}{1}] {2}%\n`".format(
                             "".join(["●" for i in range(math.floor(per / 5))]),
                             "".join(["" for i in range(20 - math.floor(per / 5))]),
-                        round(per, 2),
+                            round(per, 2),
                         )
                         e_size = humanbytes(size)
-                        await xxx.edit(progress_str+"\n"+"`"+e_size+"`")
+                        await xxx.edit(progress_str + "\n" + "`" + e_size + "`")
             os.remove(file.name)
             c_size = os.path.getsize(out)
             f_time = time.time()
