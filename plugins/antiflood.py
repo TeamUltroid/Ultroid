08edd8d348de9c03779ae1fc879287e9ab7bdada
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-21 18:30:42 +0000

    pylint: auto fixes

--- plugins/antiflood.py
+++ plugins/antiflood.py
@@ -58,7 +58,13 @@ if Redis("ANTIFLOOD") is not (None or ""):
                 )
                 del _check_flood[event.chat_id]
                 await event.reply("#AntiFlood\n\n`You have been muted.`")
-                await asst.send_message(int(Redis("LOG_CHANNEL")), f"#Antiflood\n\n`Muted `[{name}](tg://user?id={event.sender_id})` in {chat}", buttons=Button.inline("Unmute", data=f"anti_{event.sender_id}_{event.chat_id}")) 
+                await asst.send_message(
+                    int(Redis("LOG_CHANNEL")),
+                    f"#Antiflood\n\n`Muted `[{name}](tg://user?id={event.sender_id})` in {chat}",
+                    buttons=Button.inline(
+                        "Unmute", data=f"anti_{event.sender_id}_{event.chat_id}"
+                    ),
+                )
             except BaseException:
                 pass
 
