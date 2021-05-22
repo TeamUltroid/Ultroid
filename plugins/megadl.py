0a82fbd05d9355542fe8068ade14ef79c887dc88
Danish <danish@ultroid.tech>
2021-05-22 22:11:59 +0530

    Fixes

--- plugins/megadl.py
+++ plugins/megadl.py
@@ -24,6 +24,9 @@ async def _(e):
     link = e.pattern_match.group(1)
     if not os.path.isdir("mega"):
         os.mkdir("mega")
+    else:
+        os.system("rm -rf mega")
+        os.mkdir("mega")
     xx = await eor(e, f"Processing...\nTo Check Progress : `{HNDLR}ls mega`")
     s = datetime.now()
     x, y = await bash(f"megadl {link} --path mega")
@@ -31,7 +34,19 @@ async def _(e):
     ok = [*sorted(afl)]
     tt = time.time()
     c = 0
-    for kk in ok:
+    k = []
+    for x in ok:
+        if os.path.isdir(x):
+            k.append(x)
+            break
+    if k:
+        await xx.edit(
+            "Your Unzipped File Saved in `mega` folder.\nDo `{i}ls mega` and browse storage\nUse `{i}ul <path>` To upload.".format(
+                i=HNDLR
+            )
+        )
+    else:
+      for kk in ok:
         try:
             res = await uploader(kk, kk, tt, xx, "Uploading...")
             await ultroid_bot.send_file(
@@ -44,19 +59,19 @@ async def _(e):
             c += 1
         except Exception as er:
             LOGS.info(er)
-    ee = datetime.now()
-    t = time_formatter(((ee - s).seconds) * 1000)
-    size = 0
-    for path, dirs, files in os.walk("mega"):
+      ee = datetime.now()
+      t = time_formatter(((ee - s).seconds) * 1000)
+      size = 0
+      for path, dirs, files in os.walk("mega"):
         for f in files:
             fp = os.path.join(path, f)
             size += os.path.getsize(fp)
-    await xx.delete()
-    await ultroid_bot.send_message(
+      await xx.delete()
+      await ultroid_bot.send_message(
         e.chat_id,
         f"Downloaded And Uploaded Total - `{c}` files of `{humanbytes(size)}` in `{t}`",
-    )
-    os.system("rm -rf mega")
+      )
+      os.system("rm -rf mega")
 
 
 HELP.update({f"{__name__.split('.')[1]}": f"{__doc__.format(i=HNDLR)}"})
