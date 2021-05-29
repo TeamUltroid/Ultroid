ebe5324d0f3838796c43733a2c6d55865f670c99
xditya <xditya@ultroid.tech>
2021-05-29 07:59:06 +0000

    pylint: auto fixes

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -12,8 +12,8 @@ from pyUltroid.dB.core import *
 from pyUltroid.functions.all import *
 from pyUltroid.functions.broadcast_db import *
 from pyUltroid.functions.gban_mute_db import *
-from pyUltroid.functions.greetings_db import *
 from pyUltroid.functions.google_image import googleimagesdownload
+from pyUltroid.functions.greetings_db import *
 from pyUltroid.functions.sudos import *
 from pyUltroid.functions.ytdl import *
 from pyUltroid.utils import *
