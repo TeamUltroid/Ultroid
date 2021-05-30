1539006ad56224c832cb5a05d67e374ab6f3b608
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-05-31 00:59:54 +0530

    Update fontgen.py

--- plugins/fontgen.py
+++ plugins/fontgen.py
@@ -2,6 +2,28 @@
 {i}
 """
 
-from . import _double_stroke, _monospace, _small_caps
+from . import _double_stroke, _monospace, _small_caps, _default
 
-fonts = [_small_caps, _monospace, _double_stroke]
+fonts = ["small caps ", "monospace ", "double stroke "]
+
+
+@ultroid_cmd(
+pattern="font ?(.*)",
+)
+async def _(e):
+    input = e.pattern_match.group(1)
+    try:
+        font = input.split(':', maxsplit=1)[0]
+        text = input.split(':', maxsplit=1)[1]
+    except:
+        pass #todo
+    if font not in fonts:
+        return #todo
+    if font == "small caps ":
+        font = _small_caps
+    if font == "monospace ":
+        font = _small_caps
+    if font == "double stroke ":
+        font = _small_caps
+    await eor(e, str(font))
+    #todo tmrw
