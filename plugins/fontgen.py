9e54eff6744b57cf04399d342c37347e7be01a22
xditya <xditya@ultroid.tech>
2021-05-30 19:30:50 +0000

    pylint: auto fixes

--- plugins/fontgen.py
+++ plugins/fontgen.py
@@ -2,23 +2,23 @@
 {i}
 """
 
-from . import _double_stroke, _monospace, _small_caps, _default
+from . import _small_caps
 
 fonts = ["small caps ", "monospace ", "double stroke "]
 
 
 @ultroid_cmd(
-pattern="font ?(.*)",
+    pattern="font ?(.*)",
 )
 async def _(e):
     input = e.pattern_match.group(1)
     try:
-        font = input.split(':', maxsplit=1)[0]
-        text = input.split(':', maxsplit=1)[1]
-    except:
-        pass #todo
+        font = input.split(":", maxsplit=1)[0]
+        text = input.split(":", maxsplit=1)[1]
+    except BaseException:
+        pass  # todo
     if font not in fonts:
-        return #todo
+        return  # todo
     if font == "small caps ":
         font = _small_caps
     if font == "monospace ":
@@ -26,4 +26,4 @@ async def _(e):
     if font == "double stroke ":
         font = _small_caps
     await eor(e, str(font))
-    #todo tmrw
+    # todo tmrw
