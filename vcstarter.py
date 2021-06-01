c5ff4d2a48405a698b62852a027966f4ecd1d95a
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-01 22:27:27 +0530

    Saved from here

--- vcstarter.py
+++ vcstarter.py
@@ -17,7 +17,7 @@ from telethon.tl.types import DataJSON
 LOG_CHANNEL = int(udB.get("LOG_CHANNEL"))
 if vcbot is not None:
 
-    bot = TelegramClient("ultroid_vc", Var.API_ID, Var.API_HASH).start(bot_token=udB.get("BOT_TOKEN"))
+    bot = TelegramClient(None, Var.API_ID, Var.API_HASH).start(bot_token=udB.get("BOT_TOKEN"))
 
     async def get_entity(chat):
         try:
