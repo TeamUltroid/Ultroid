4d9bf995fdf4728c946748ad28b6b61be1545e4f
Aditya <xditya@ultroid.tech>
2021-06-03 21:50:01 +0530

    move functions

--- plugins/bot.py
+++ plugins/bot.py
@@ -36,7 +36,6 @@ from datetime import datetime as dt
 from platform import python_version as pyver
 
 import heroku3
-import requests
 from git import Repo
 from pyUltroid import __version__ as UltVer
 from telethon import __version__, events
@@ -165,49 +164,3 @@ async def _(event):
         await def_logs(event)
     else:
         await def_logs(event)
-
-
-async def heroku_logs(event):
-    if HEROKU_API is None and HEROKU_APP_NAME is None:
-        return await eor(
-            event, "Please set `HEROKU_APP_NAME` and `HEROKU_API` in vars."
-        )
-    await eor(event, "`Downloading Logs...`")
-    ok = app.get_log()
-    with open("ultroid-heroku.log", "w") as log:
-        log.write(ok)
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": ok})
-        .json()
-        .get("result")
-        .get("key")
-    )
-    url = f"https://nekobin.com/{key}"
-    await ultroid.send_file(
-        event.chat_id,
-        file="ultroid-heroku.log",
-        thumb="resources/extras/ultroid.jpg",
-        caption=f"**Ultroid Heroku Logs.**\nPasted [here]({url}) too!",
-    )
-    os.remove("ultroid-heroku.log")
-
-
-async def def_logs(ult):
-    xx = await eor(ult, "`Processing...`")
-    with open("ultroid.log") as f:
-        k = f.read()
-    key = (
-        requests.post("https://nekobin.com/api/documents", json={"content": k})
-        .json()
-        .get("result")
-        .get("key")
-    )
-    url = f"https://nekobin.com/{key}"
-    await ultroid.send_file(
-        ult.chat_id,
-        file="ultroid.log",
-        thumb="resources/extras/ultroid.jpg",
-        caption=f"**Ultroid Logs.**\nPasted [here]({url}) too!",
-    )
-    await xx.edit("Done")
-    await xx.delete()
