c42739d772c846c144a9cb6ee0f24cba267bbb48
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-03 09:39:02 +0530

    https://t.me/c/1237141420/363138

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -103,12 +103,11 @@ def get_data(types, data):
     audio = []
     video = []
     for m in data["formats"]:
-        k = m["format_note"]
         id = m["format_id"]
         note = m["format_note"]
         size = humanbytes(m["filesize"])
         j = f"{id} {note} {size}"
-        if k == "tiny":
+        if note == "tiny":
             audio.append(j)
         else:
             video.append(j)
