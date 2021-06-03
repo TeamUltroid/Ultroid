f2884250ef9208de7aed4ac925a0839d3246ad41
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-03 12:35:01 +0530

    Update ytdl.py

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -114,7 +114,7 @@ def get_data(types, data):
                 audio.append(j)
             else:
                 if m["acodec"] == "none":
-                    id = str(m["format_id"]) + "+" + str(audio[-1].split()[-2])
+                    id = str(m["format_id"]) + "+" + str(audio[-1].split()[1])
                     j = f"{id} {note} {size}"
                     video.append(j)
                 else:
