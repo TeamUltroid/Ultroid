e66389b7347c69c2f92089c96b996d76e007c3dc
Rifkiarisman <83812990+Rifkiarisman@users.noreply.github.com>
2021-06-05 21:06:28 +0700

    Santet.py

---
+++ Santet.py
@@ -0,0 +1,323 @@
+dari  waktu  impor  tidur
+dari  userbot  impor  CMD_HELP
+dari  userbot . daftar impor acara  
+
+
+@ register ( keluar = Benar , pola = r'^\.santet(?: |$)(.*)' )
+async  def  mesin tik ( typew ):
+    ketik . pola_cocok . kelompok ( 1 )
+    menunggu  ketik . edit ( "`Lord Telah Mengaktifkan Perintah Santet Online `" )
+    tidur ( 2 )
+    menunggu  ketik . edit ( "`Mencari Nama Orang Ini...`" )
+    tidur ( 1 )
+    menunggu  ketik . edit ( "`Santet Online Segera Dilakukan`" )
+    tidur ( 1 )
+    menunggu  ketik . edit ( "0%" )
+    nomor  =  1
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . mengedit ( str ( angka ) +  "% " )
+    nomor  = angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    nomor  = angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( nomor ) +  "% " )
+    bilangan  =  bilangan +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( nomor ) +  "% " )
+    bilangan  =  bilangan +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( nomor ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    bilangan  =  bilangan  + 1
+    tidur (0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    angka  =  angka  +  1
+    tidur ( 0,03 )
+    menunggu  ketik . edit ( str ( angka ) +  "% " )
+    tidur ( 1 )
+    menunggu  ketik . edit ( "`Target Berhasil Tersantet Online:v`" )
+
+
+CMD_HELP .memperbarui ({
+    'santetonline' :
+    ''`.santet` \
+        \n Penggunaan: Santet Online Buat Bercanda.'
+})
