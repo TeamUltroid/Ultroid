c61b8be6c5135ba82c4b07915c6eb42af0b207dc
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-05 11:11:05 +0530

    Update admintools.py

--- plugins/admintools.py
+++ plugins/admintools.py
@@ -424,7 +424,9 @@ async def _(e):
 async def get_pinned(event):
     x = await eor(event, get_string("com_1"))
     chat_id = (str(event.chat_id)).replace("-100", "")
-    chat_name = (await event.get_chat()).title
+    chat_name = 'This Chat'
+    if_not event.is_private:
+        chat_name = (await event.get_chat()).title
     tem = ""
     c = 0
 
