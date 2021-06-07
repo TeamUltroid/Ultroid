375f1f537b948bb9e4a0cf44297d10a983754870
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-07 13:31:23 +0530

    Update ping.py

--- assistant/ping.py
+++ assistant/ping.py
@@ -13,8 +13,8 @@ from datetime import datetime
 async def _(event):
     start = datetime.now()
     end = datetime.now()
-    ms = (end - start).microseconds
+    ms = (end - start).microseconds / 1000
     await asst.send_message(
         event.chat_id,
-        f"**Pong!!**\n `{ms} microseconds`",
+        f"**Pong!!**\n `{ms} milliseconds`",
     )
