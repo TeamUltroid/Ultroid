50a35c223cfd12bf09b04420a4ed915c466657d5
xditya <xditya@ultroid.tech>
2021-06-07 08:02:27 +0000

    pylint: auto fixes

--- plugins/megadl.py
+++ plugins/megadl.py
@@ -12,7 +12,6 @@
   It Downloads and Upload Files from mega.nz links.
 """
 
-import glob
 import time
 from datetime import datetime
 
@@ -32,30 +31,30 @@ async def _(e):
     x, y = await bash(f"megadl {link} --path mega")
     ok = get_all_files("mega")
     tt = time.time()
-    c = 0    
+    c = 0
     for kk in ok:
-            try:
-                res = await uploader(kk, kk, tt, xx, "Uploading...")
-                await ultroid_bot.send_file(
-                    e.chat_id,
-                    res,
-                    caption="`" + kk.split("/")[-1] + "`",
-                    force_document=True,
-                    thumb="resources/extras/ultroid.jpg",
-                )
-                c += 1
-            except Exception as er:
-                LOGS.info(er)
+        try:
+            res = await uploader(kk, kk, tt, xx, "Uploading...")
+            await ultroid_bot.send_file(
+                e.chat_id,
+                res,
+                caption="`" + kk.split("/")[-1] + "`",
+                force_document=True,
+                thumb="resources/extras/ultroid.jpg",
+            )
+            c += 1
+        except Exception as er:
+            LOGS.info(er)
     ee = datetime.now()
     t = time_formatter(((ee - s).seconds) * 1000)
     size = 0
     for path, dirs, files in os.walk("mega"):
-            for f in files:
-                fp = os.path.join(path, f)
-                size += os.path.getsize(fp)
+        for f in files:
+            fp = os.path.join(path, f)
+            size += os.path.getsize(fp)
     await xx.delete()
     await ultroid_bot.send_message(
-            e.chat_id,
-            f"Downloaded And Uploaded Total - `{c}` files of `{humanbytes(size)}` in `{t}`",
+        e.chat_id,
+        f"Downloaded And Uploaded Total - `{c}` files of `{humanbytes(size)}` in `{t}`",
     )
     os.system("rm -rf mega")
