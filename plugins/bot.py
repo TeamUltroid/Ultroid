a342267bc698bec35bd919608fde3b8f666a9487
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-11 19:17:11 +0530

    We can use chats=[] in logs and ping

--- plugins/bot.py
+++ plugins/bot.py
@@ -43,19 +43,7 @@ from telethon.errors.rpcerrorlist import ChatSendMediaForbiddenError
 
 from . import *
 
-HEROKU_API = None
-HEROKU_APP_NAME = None
 
-try:
-    if Var.HEROKU_API and Var.HEROKU_APP_NAME:
-        HEROKU_API = Var.HEROKU_API
-        HEROKU_APP_NAME = Var.HEROKU_APP_NAME
-        Heroku = heroku3.from_key(Var.HEROKU_API)
-        heroku_api = "https://api.heroku.com"
-        app = Heroku.app(Var.HEROKU_APP_NAME)
-except BaseException:
-    HEROKU_API = None
-    HEROKU_APP_NAME = None
 
 
 @ultroid_cmd(
