545560c026070805e78218a552411a1e00e52627
Danish <danish@ultroid.tech>
2021-06-14 22:18:46 +0530

    Update api_setter.py

--- assistant/api_setter.py
+++ assistant/api_setter.py
@@ -16,13 +16,12 @@ async def apiset(event):
     await event.edit(
         get_string("ast_1"),
         buttons=[
-            [Button.inline("Remove.bg", data="rmbg")],
+            [Button.inline("Remove.bg API", data="rmbg")],
+            [Button.inline("DEEP API", data="dapi")],
+            [Button.inline("OCR API", data="oapi")],
             [Button.inline("« Back", data="setter")],
         ],
     )
-
-
-# remove.bg api
 
 
 @callback("rmbg")
@@ -32,6 +31,55 @@ async def rmbgapi(event):
     pru = event.sender_id
     var = "RMBG_API"
     name = "Remove.bg API Key"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(get_string("ast_2"))
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message(
+                "Cancelled!!",
+                buttons=get_back_button("apiset"),
+            )
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message(
+                f"{name} changed to {themssg}",
+                buttons=get_back_button("apiset"),
+            )
+
+
+@callback("dapi")
+@owner
+async def rmbgapi(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "DEEP_API"
+    name = "DEEP AI API Key"
+    async with event.client.conversation(pru) as conv:
+        await conv.send_message(get_string("ast_2"))
+        response = conv.wait_event(events.NewMessage(chats=pru))
+        response = await response
+        themssg = response.message.message
+        if themssg == "/cancel":
+            return await conv.send_message(
+                "Cancelled!!",
+                buttons=get_back_button("apiset"),
+            )
+        else:
+            await setit(event, var, themssg)
+            await conv.send_message(
+                f"{name} changed to {themssg}",
+                buttons=get_back_button("apiset"),
+            )
+
+@callback("oapi")
+@owner
+async def rmbgapi(event):
+    await event.delete()
+    pru = event.sender_id
+    var = "OCR_API"
+    name = "OCR API Key"
     async with event.client.conversation(pru) as conv:
         await conv.send_message(get_string("ast_2"))
         response = conv.wait_event(events.NewMessage(chats=pru))
