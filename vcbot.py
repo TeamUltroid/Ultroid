d1197cf8e4b660716461a59e152b085035889fad
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-20 12:11:05 +0530

    Create vcbot.py

---
+++ vcbot.py
@@ -0,0 +1,28 @@
+import logging
+
+from pyUltroid import udB, vcbot
+from pyUltroid.dB.database import Var
+
+from pyrogram import Client, filters
+from pytgcalls import PyTgCalls
+
+LOG_CHANNEL = int(udB.get("LOG_CHANNEL"))
+
+logging.basicConfig(level=logging.WARNING)
+
+SESSION = udB.get("VC_SESSION")
+
+Client = Client(SESSION or ":memory:",
+                api_id=Var.API_ID,
+                api_hash=Var.API_HASH)
+
+CallsClient = PyTgCalls(Client)
+
+@CallsClient.on_message(filters.me & filters.group & filters.regex("^.play (.*)"))
+async def startup(_, message):
+    song = message.matches[0].group(1)
+    await CallsClient.join_group_call(message.chat.id, song)
+
+
+if vcbot:
+  CallsClient.run()
