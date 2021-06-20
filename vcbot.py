c6a8ebb090efdf8280de433ef11c045279f49029
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-20 13:56:06 +0530

    Update vcbot.py

--- vcbot.py
+++ vcbot.py
@@ -18,19 +18,23 @@ Client = Client(SESSION,
 
 CallsClient = PyTgCalls(Client, log_mode=PyLogs.ultra_verbose)
 
-@Client.on_message(filters.me & filters.group & filters.regex("^.play (.*)"))
+
+@Client.on_message(filters.me & filters.group & filters.regex("^.play ?(.*)"))
 async def startup(_, message):
     song = message.matches[0].group(1)
     await message.edit_text('`Processing...`')
-    await CallsClient.join_group_call(message.chat.id, song)
+    CallsClient.join_group_call(message.chat.id, song)
+
 
 @CallsClient.on_stream_end()
 async def handler(chat_id: int):
-	await CallsClient.leave_group_call(chat_id)
+	CallsClient.leave_group_call(chat_id)
+
 
 @Client.on_message(filters.me & filters.group & filters.regex("^.lvc"))
 async def handler(_, message):
 	await message.edit_text('`Left...`')
 	await CallsClient.leave_group_call(message.chat.id)
+
 
 CallsClient.run()
