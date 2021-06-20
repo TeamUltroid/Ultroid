aefb66557d28cf6b09018af87abea566cfa07177
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-20 14:31:16 +0530

    Update vcbot.py

--- vcbot.py
+++ vcbot.py
@@ -19,11 +19,20 @@ Client = Client(SESSION,
 CallsClient = PyTgCalls(Client, log_mode=PyLogs.ultra_verbose)
 
 
-@Client.on_message(filters.me & filters.group & filters.regex("^.play ?(.*)"))
+@Client.on_message(filters.me & filters.group & filters.command(["play"], prefixes="."))
 async def startup(_, message):
-    song = message.matches[0].group(1)
-    await message.edit_text('`Processing...`')
+    msg = await message.edit_text('`Processing...`')
+    song = message.text.split(" ")
+    if not message.reply_to_message and len(song) > 1:
+	song = song[1]
+    elif not message.reply_to_message.audio:
+	return await msg.edit_text("Pls Reply to Audio File or Give Search Query...")
+    else:
+	dl = await message.reply_to_message.download()
+	song = f"{message.chat.id}_VCSONG.raw"
+	await bash(f"ffmpeg -i {dl} -f s16le -ac 1 -acodec pcm_s16le -ar 4800 {song}")
     CallsClient.join_group_call(message.chat.id, song)
+    await msg.delete()
 
 
 @CallsClient.on_stream_end()
