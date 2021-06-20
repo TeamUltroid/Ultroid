e7e2bb7d674a44221c4f4d0ad7d8a956783ab345
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-20 12:22:22 +0530

    Update vcbot.py

--- vcbot.py
+++ vcbot.py
@@ -8,11 +8,11 @@ from pytgcalls import PyTgCalls
 
 LOG_CHANNEL = int(udB.get("LOG_CHANNEL"))
 
-logging.basicConfig(level=logging.WARNING)
+logging.basicConfig(level=logging.INFO)
 
 SESSION = udB.get("VC_SESSION")
 
-Client = Client(SESSION or ":memory:",
+Client = Client(SESSION,
                 api_id=Var.API_ID,
                 api_hash=Var.API_HASH)
 
