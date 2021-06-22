dc4ebf22b31239787080c569f6b1107a37c04f81
Error <error@notavailable.live>
2021-06-22 10:22:15 +0530

    Uh

--- vcbot.py
+++ vcbot.py
@@ -161,7 +161,7 @@ async def radio(_, message):
     radio = message.text.split(" ", maxsplit=1)
     file = f"VCRADIO_{message.chat.id}.raw"
     if re.search("youtube|youtu.be", radio[1]):
-        is_live_vid = await bash(f'youtube-dl -j "{radio[1]}" | jq ".is_live"')[0]
+        is_live_vid = (await bash(f'youtube-dl -j "{radio[1]}" | jq ".is_live"'))[0]
         if is_live_vid == "true":
             the_input = f'"$(youtube-dl -x -g "{radio[1]}")"'
         else:
