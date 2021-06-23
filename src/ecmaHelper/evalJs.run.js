9855cc35f645fde05a30751c1382d0e614958fbb
Anonymous <69723581+New-dev0@users.noreply.github.com>
2021-06-23 11:12:13 +0530

    Create evalJs.run.js

---
+++ src/ecmaHelper/evalJs.run.js
@@ -0,0 +1,9 @@
+// Ultroid - UserBot
+// Copyright (C) 2020 TeamUltroid
+//
+// This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+// PLease read the GNU Affero General Public License in
+// <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+'use-strict';
+sgrywryhio
