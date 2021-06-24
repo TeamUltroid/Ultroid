40cc13f611023ca1548427fa578c2479486b809d
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-24 05:53:03 +0000

    pylint: auto fixes

--- vcbot/skipvc.py
+++ vcbot/skipvc.py
@@ -2,7 +2,8 @@
 # Copyright (C) 2021 TeamUltroid
 #
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
-# PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 from . import *
 from .play import streamhandler
