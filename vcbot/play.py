e2e67ad950fbcd89ce5592e96abe81e058e85443
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-06-25 03:20:44 +0000

    pylint: auto fixes

--- vcbot/play.py
+++ vcbot/play.py
@@ -78,7 +78,9 @@ async def startup(_, message):
     await msg.edit_reply_markup(reply_markup)
 
 
-@Client.on_message(filters.me & filters.command(["play","cplay"], HNDLR) & ~filters.edited)
+@Client.on_message(
+    filters.me & filters.command(["play", "cplay"], HNDLR) & ~filters.edited
+)
 async def cstartup(_, message):
     await startup(_, message)
 
