981ea4b26fbd84617fb7ddc75c96ed7562c72a35
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-07 18:25:19 +0530

    ultmanager doesn't want exists anymore
    I doubt, is this file loads too?

--- assistant/manager/__init__.py
+++ assistant/manager/__init__.py
@@ -5,7 +5,6 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-from pyUltroid.misc._assistant import ultmanager
 
 from plugins import *
 
