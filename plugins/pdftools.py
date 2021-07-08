7feba79a24ebcca1f5ffdc58a86b2f72e0841b8c
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-08 10:54:55 +0530

    Update pdftools.py

--- plugins/pdftools.py
+++ plugins/pdftools.py
@@ -299,7 +299,7 @@ async def savepdf(event):
         os.remove("o.png")
     elif ultt.endswith(".pdf"):
         a = dani_ck("pdf/scan.pdf")
-        await ultroid_bot.download_media(ok, a)
+        await event.client.download_media(ok, a)
         await eor(
             event,
             f"Done, Now Reply Another Image/pdf if completed then use {hndlr}pdsend to merge nd send all as pdf",
