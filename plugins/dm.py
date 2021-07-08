ba51f3c76901c62a0a60632bfafa7abdb9c2b672
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-08 10:12:01 +0530

    Update dm.py

--- plugins/dm.py
+++ plugins/dm.py
@@ -26,22 +26,21 @@ async def dm(e):
     try:
         chat_id = await get_user_id(c[0])
     except Exception as ex:
-        return await eod(e, "`" + str(ex) + "`", time=5)
+        return await eod(e, "`" + str(ex) + "`")
     msg = ""
     masg = await e.get_reply_message()
     if e.reply_to_msg_id:
-        await ultroid_bot.send_message(chat_id, masg)
-        await eod(e, "`⚜️Message Delivered!`", time=4)
+        await e.client.send_message(chat_id, masg)
+        await eod(e, "`⚜️Message Delivered!`")
     for i in c[1:]:
         msg += i + " "
     if msg == "":
         return
     try:
-        await ultroid_bot.send_message(chat_id, msg)
-        await eod(e, "`⚜️Message Delivered!⚜️`", time=4)
+        await e.client.send_message(chat_id, msg)
+        await eod(e, "`⚜️Message Delivered!⚜️`")
     except BaseException:
         await eod(
             e,
-            "`{i}help dm`",
-            time=4,
+            "`{i}help dm`"
         )
