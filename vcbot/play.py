eb33170c77a32796292c8e40b5fd1f0ece36c577
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-07-12 19:03:28 +0000

    pylint: auto fixes

--- vcbot/play.py
+++ vcbot/play.py
@@ -5,7 +5,8 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-import os, asyncio
+import asyncio
+import os
 
 from . import *
 
