f2da6247d1eaebc2819a8ac26ef54ed94102130f
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-13 20:24:20 +0530

    Update google.py

--- plugins/google.py
+++ plugins/google.py
@@ -32,7 +32,7 @@ from strings import get_string
 from . import *
 
 
-@ultroid_cmd(pattern="google ?(.*)", type=["official", "manager"])
+@ultroid_cmd(pattern="google ?(.*)", type=["official", "manager"], ignore_dualmode=True)
 async def google(event):
     inp = event.pattern_match.group(1)
     if not inp:
