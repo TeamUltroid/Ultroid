bbe35e71ec5d574212d6f6cb3610f8bc40874a11
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-15 12:24:04 +0530

    Update skipvc.py

--- vcbot/skipvc.py
+++ vcbot/skipvc.py
@@ -7,15 +7,23 @@
 
 from . import *
 
-# from .play import streamhandler
-
 
 @asst.on_message(filters.command("skipvc") & filters.user(VC_AUTHS()) & ~filters.edited)
 async def skiplife(_, message):
     mst = message.text.split(" ", maxsplit=1)
     try:
-        chat = mst[1]
+        chat = (await Client.get_chat(mst[1])).id
     except BaseException:
         chat = message.chat.id
-    CallsClient.pause_stream(chat)
-    # await streamhandler(_, message)
+    try:
+        song, title, from_user = get_from_queue(chat)
+        CallsClient.change_stream(chat, song)
+        await asst.send_message(
+            chat_id, f"**Playing :** {title}\n**Requested by**: {from_user}"
+        )
+        pos = list(QUEUE[int(chat_id)])[0]
+        QUEUE[chat_id].pop(pos)
+    except IndexError:
+        CallsClient.leave_group_call(chat)
+    except Exception as Ex:
+        await asst.send_message(LOG_CHANNEL, "Vc Error\n\n" + str(Ex))
