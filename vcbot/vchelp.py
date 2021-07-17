e9fb3cd6a8633a5951736e690f9d6573c523219e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-07-17 18:19:39 +0000

    pylint: auto fixes

--- vcbot/vchelp.py
+++ vcbot/vchelp.py
@@ -1,4 +1,3 @@
-  
 # Ultroid - UserBot
 # Copyright (C) 2021 TeamUltroid
 #
@@ -14,26 +13,27 @@ HELP_TEXT = f"""
   - `/play <search query/youtube link/reply to File>`
   - `/play <channel username/id, where to play><search query/youtube link/reply to File>`
       Plays Music in Voice Call of Specified or Current Chat.
- 
+
  -  `/radio <M3U8 url/Radio Link/Youtube Live Stream Link>`
       Plays Music Live, Depending on Entered Query.
- 
+
   - `/playfrom <Channel Username/Id> ; <limit of songs>`
       Takes Music Files from Channel/Chat and Plays it.
- 
+
   - `/leave` : `Leave Vc of Current/Specified Chat..`
-  
+
   - `/skip` : `Skip the Song Playing in Chat.`
   - `/pause` : `Pause the Voice Call Songs.`
   - `/resume` : `resume the Paused Song.`
-  
+
   - `/queue` : `Get List of Songs Added in Queue.`
   - `/listvc` : `Get List of Chats, where Vc is Active.`
   - `/clearqueue` : `Clear All the Songs, added to Queue.`
-  
+
  `Note` :  `You can Also Use the same Commands from VC Account with Handler` `{HNDLR}`
 
 """
+
 
 @asst.on_message(
     filters.command(["vchelp", f"vchelp@{vcusername}"])
@@ -42,8 +42,13 @@ HELP_TEXT = f"""
     & filters.group
 )
 async def pass_it(_, message):
-  await eor(message, HELP_TEXT)
+    await eor(message, HELP_TEXT)
 
-@Client.on_message(filters.command("vchelp", HNDLR) & filters.outgoing & ~(filters.edited | filters.forwarded))
+
+@Client.on_message(
+    filters.command("vchelp", HNDLR)
+    & filters.outgoing
+    & ~(filters.edited | filters.forwarded)
+)
 async def always(_, message):
-  await eor(message, HELP_TEXT)
+    await eor(message, HELP_TEXT)
