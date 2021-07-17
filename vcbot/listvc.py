dbed855d8530b6a2656bedd05f56db6bd93be7cd
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-07-17 22:14:21 +0530

    Update listvc.py

--- vcbot/listvc.py
+++ vcbot/listvc.py
@@ -20,7 +20,7 @@ async def list_handler(_, message):
     OUT += "List of All Active Calls\n\n"
     for ke in CallsClient.active_calls.keys():
         stat = CallsClient.active_calls[ke]
-        OUT += f"`{ke}` : __{stat}__\n"
+        OUT += f"• `{ke}` : `{stat}`\n"
     await eor(message, OUT)
 
 
@@ -29,25 +29,3 @@ async def list_handler(_, message):
 )
 async def llhnf(_, message):
     await list_handler(_, message)
-
-
-@asst.on_message(
-    filters.command(["queue", f"queue@{vcusername}"])
-    & filters.user(VC_AUTHS())
-    & ~filters.edited
-)
-async def queuee(_, e):
-    mst = e.text.split(" ", maxsplit=1)
-    try:
-        chat = (await Client.get_chat(mst[1])).id
-    except BaseException:
-        chat = e.chat.id
-    txt = list_queue(chat)
-    if txt:
-        return await eor(e, txt)
-    await eor(e, "No Queue Found !")
-
-
-@Client.on_message(filters.outgoing & filters.command("queue", HNDLR) & ~filters.edited)
-async def queue_vc(_, message):
-    await queuee(_, message)
