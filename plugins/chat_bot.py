3c3d1ae5f773e88bc10d70e897655d69af0bb60d
Danish <danish@ultroid.tech>
2021-07-19 18:00:01 +0530

    Update chat_bot.py

--- plugins/chat_bot.py
+++ plugins/chat_bot.py
@@ -52,7 +52,7 @@ async def rem_chatBot(event):
 
 @ultroid_cmd(pattern="listai")
 async def lister(event):
-    users = get_all_added()
+    users = get_all_added(event.chat.id)
     if udB.get("CHATBOT_USERS") is None:
         return await eod(event, "`No user has AI added.`")
     msg = ""
