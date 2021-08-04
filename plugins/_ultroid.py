adb1e69fddd32ababe6b85be39d75cd96bfadeb0
Devesh Pal <newdev0@outlook.com>
2021-08-04 19:07:15 +0530

    Update _ultroid.py

--- plugins/_ultroid.py
+++ plugins/_ultroid.py
@@ -18,17 +18,17 @@ REPOMSG = """
 
 RP_BUTTONS = [
     [
-        Button.inline("Repo", "https://github.com/TeamUltroid/Ultroid"),
-        Button.inline("Addons", "https://github.com/TeamUltroid/UltroidAddons"),
+        Button.url("Repo", "https://github.com/TeamUltroid/Ultroid"),
+        Button.url("Addons", "https://github.com/TeamUltroid/UltroidAddons"),
     ],
-    [Button.inline("Support Group", "t.me/ultroidsupport")],
+    [Button.url("Support Group", "t.me/ultroidsupport")],
 ]
 
 
 @ultroid_cmd(pattern="repo$", type=["official", "manager"], ignore_dualmode=True)
 async def repify(e):
     if e.client._bot:
-        return await e.reply("**Ultroid Userbot**", buttons=RP_BUTTONS)
+        return await e.reply("• **Ultroid Userbot** •", buttons=RP_BUTTONS)
     try:
         q = await e.client.inline_query(asst.me.username, "repo")
         await q[0].click(e.chat_id)
