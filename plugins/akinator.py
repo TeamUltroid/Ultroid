6577a5f62dd5f1099187a00ba9f0254a4576d69f
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-08 06:15:03 +0000

    pylint: auto fixes

--- plugins/akinator.py
+++ plugins/akinator.py
@@ -1,19 +1,25 @@
 import akinator
-from . import *
 from telethon.errors import BotMethodInvalidError
 
+from . import *
+
 games = {}
-aki_photo="https://telegra.ph/file/b0ff07069e8637783fdae.jpg"
+aki_photo = "https://telegra.ph/file/b0ff07069e8637783fdae.jpg"
+
 
 @ultroid_cmd(pattern="akinator")
 async def doit(e):
     sta = akinator.Akinator()
-    games.update({e.chat_id:{e.id:sta}})
+    games.update({e.chat_id: {e.id: sta}})
     try:
         m = await e.client.inline_query(asst.me.username, f"aki_{e.chat_id}_{e.id}")
         await m[0].click(e.chat_id)
     except BotMethodInvalidError:
-        return await asst.send_file(e.chat_id, aki_photo, buttons=Button.inline("Start Game", data=f"aki_{e.chat_id}_{e.id}"))
+        return await asst.send_file(
+            e.chat_id,
+            aki_photo,
+            buttons=Button.inline("Start Game", data=f"aki_{e.chat_id}_{e.id}"),
+        )
     if e.out:
         await e.delete()
 
@@ -45,7 +51,7 @@ async def doai(e):
 @owner
 async def okah(e):
     mk = e.pattern_match.group(1).decode("utf-8").split("_")
-    ch = int(mk[0]) 
+    ch = int(mk[0])
     mid = int(mk[1])
     ans = mk[2]
     try:
@@ -68,10 +74,21 @@ async def okah(e):
     bts = [bts, cts]
     await e.edit(text, buttons=bts)
 
+
 @in_pattern(re.compile("aki_?(.*)"))
 @in_owner
 async def eiagx(e):
     bts = Button.inline("Start Game", data=e.text)
-    ci = types.InputWebDocument(aki_photo, 0, "image/jpeg", []) 
-    ans = [await e.builder.article("Akinator", type="photo", content=ci, text="Akinator",thumb=ci, buttons=bts, include_media=True)]
+    ci = types.InputWebDocument(aki_photo, 0, "image/jpeg", [])
+    ans = [
+        await e.builder.article(
+            "Akinator",
+            type="photo",
+            content=ci,
+            text="Akinator",
+            thumb=ci,
+            buttons=bts,
+            include_media=True,
+        )
+    ]
     await e.answer(ans)
