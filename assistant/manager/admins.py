d08b0f91eab62525a43061eebebbd50a8d106f61
Devesh Pal <newdev0@outlook.com>
2021-08-15 15:26:41 +0530

    Update admins.py

--- assistant/manager/admins.py
+++ assistant/manager/admins.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 from . import *
 
 
@@ -27,20 +34,5 @@ async def dowj(e):
         await replied.delete()
         await e.client.edit_permissions(e.chat_id, user, view_messages=False)
         await eor(e, "Banned Successfully!")
-    except Exception as E:
-        await eor(e, str(E))
-
-
-@ultroid_cmd(pattern="dmute", type=["manager", "official"])
-async def dowj(e):
-    if e.is_reply:
-        replied = await e.get_reply_message()
-        user = (await replied.get_sender()).id
-    else:
-        return await eor(e, "Reply to a message...")
-    try:
-        await replied.delete()
-        await e.client.edit_permissions(e.chat_id, user, send_messages=False)
-        await eor(e, "Muted Successfully!")
     except Exception as E:
         await eor(e, str(E))
