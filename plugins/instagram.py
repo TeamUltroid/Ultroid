9d8bdc9955661aebfd74fdb66c4650b6e37b9f3e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-16 13:46:44 +0000

    pylint: auto fixes

--- plugins/instagram.py
+++ plugins/instagram.py
@@ -14,50 +14,58 @@
 """
 
 import os
-from . import *
+
 from instagrapi import Client
+
+from . import *
 
 CLIENT = []
 
+
 def create_client(username, password):
-  try:
-    return CLIENT[0]
-  except IndexError:
-    cl = Client()
-    cl.login(username, password)
-    CLIENT.append(cl)
-    return cl
-  
+    try:
+        return CLIENT[0]
+    except IndexError:
+        cl = Client()
+        cl.login(username, password)
+        CLIENT.append(cl)
+        return cl
+
 
 @ultroid_cmd(pattern="instadl ?(.*)")
 async def insta_dl(e):
-  match = e.pattern_match.group(1)
-  replied = await e.get_reply_message()
-  if match:
-    text = match
-  elif e.is_reply and "insta" in replied.message:
-    text = replied.message
-  else:
-    return await eor(e, "Provide a Link to Download...")
-  un = udB.get("INSTA_USERNAME")
-  up = udB.get("INSTA_PASSWORD")
-  if un and up:
-    try:
-        CL = create_client(un, up)
-        media = CL.video_download(CL.media_pk_from_url(text))
-        await e.reply(f"**Uploaded Successfully\nLink :** {text}", file=media)
-        await e.delete()
-        os.remove(media)
-        return
-    except Exception as B:
-        return await eor(e, B)
-    if isinstance(e.media, types.MessageMediaWebPage) and isinstance(e.media.webpage, types.WebPage):
-      photo = e.media.webpage.photo or e.media.webpage.document
-      if not photo:
-        return await eor(e, "Please Fill `INSTA_USERNAME` and `INSTA_PASSWORD` to Use This Comamand!")
-      await e.delete()
-      return await e.reply(f"**Link:{text}\n\nIf This Wasnt Excepted Result, Please Fill `INSTA_USERNAME` and `INSTA_PASSWORD`...", file=photo)
-    await eor(e, "Please Fill Instagram Credential to Use this Command...")
-      
-        
-
+    match = e.pattern_match.group(1)
+    replied = await e.get_reply_message()
+    if match:
+        text = match
+    elif e.is_reply and "insta" in replied.message:
+        text = replied.message
+    else:
+        return await eor(e, "Provide a Link to Download...")
+    un = udB.get("INSTA_USERNAME")
+    up = udB.get("INSTA_PASSWORD")
+    if un and up:
+        try:
+            CL = create_client(un, up)
+            media = CL.video_download(CL.media_pk_from_url(text))
+            await e.reply(f"**Uploaded Successfully\nLink :** {text}", file=media)
+            await e.delete()
+            os.remove(media)
+            return
+        except Exception as B:
+            return await eor(e, B)
+        if isinstance(e.media, types.MessageMediaWebPage) and isinstance(
+            e.media.webpage, types.WebPage
+        ):
+            photo = e.media.webpage.photo or e.media.webpage.document
+            if not photo:
+                return await eor(
+                    e,
+                    "Please Fill `INSTA_USERNAME` and `INSTA_PASSWORD` to Use This Comamand!",
+                )
+            await e.delete()
+            return await e.reply(
+                f"**Link:{text}\n\nIf This Wasnt Excepted Result, Please Fill `INSTA_USERNAME` and `INSTA_PASSWORD`...",
+                file=photo,
+            )
+        await eor(e, "Please Fill Instagram Credential to Use this Command...")
