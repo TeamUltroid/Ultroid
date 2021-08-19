643b96d6e4df9276316a18c273ecb99e09c2e075
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-19 11:23:15 +0530

    Update ytdl.py

--- assistant/ytdl.py
+++ assistant/ytdl.py
@@ -164,8 +164,8 @@ async def _(event):
         title = ytdl_data["title"]
         artist = ytdl_data["uploader"]
         views = numerize.numerize(ytdl_data["view_count"])
-        urlretrieve(f"https://i.ytimg.com/vi/{vid_id}/hqdefault.jpg", f"{title}.jpg")
-        thumb = f"{title}.jpg"
+        thumb = await fast_download(f"https://i.ytimg.com/vi/{vid_id}/hqdefault.jpg", file_name=f"{title}.jpg")
+
         duration = ytdl_data["duration"]
         try:
             os.rename(f"{ytdl_data['id']}.mp4", f"{title}.mp4")
