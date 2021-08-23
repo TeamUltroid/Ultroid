6c8783e3d687662cb33ca6441c06308ac0db2cf3
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-23 15:54:30 +0000

    pylint: auto fixes

--- vcbot/vc_connections.py
+++ vcbot/vc_connections.py
@@ -5,8 +5,9 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-from . import *
 from pytgcalls.exceptions import NotConnectedError
+
+from . import *
 
 
 @vc_asst("joinvc")
