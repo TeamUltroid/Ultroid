f1ba8e1f91e9908ff82f044aadcc70b5e0c76546
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-25 16:49:47 +0000

    pylint: auto fixes

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -5,13 +5,14 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-from pyUltroid.dB.core import VC_HELP
 from telethon import Button
+
 from . import *
 
 
 @vc_asst("vchelp")
 async def helper(event):
     await event.reply(
-        "**VCBot Help Menu**\n\n", buttons=Button.inline("Voice Chat Help", data="vc_helper")
+        "**VCBot Help Menu**\n\n",
+        buttons=Button.inline("Voice Chat Help", data="vc_helper"),
     )
