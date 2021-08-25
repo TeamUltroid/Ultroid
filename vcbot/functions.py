065cc9f5b56a3ea7bd0b6559877594b9124578c5
Danish <danish@ultroid.tech>
2021-08-25 19:22:42 +0530

    Update functions.py

--- vcbot/functions.py
+++ vcbot/functions.py
@@ -29,30 +29,35 @@ from . import *
 
 @vc_asst("mutevc")
 async def mute(event):
+    ultSongs = Player(event.chat_id)
     await ultSongs.group_call.set_is_mute(True)
     await eor(event, "`Muted playback in this chat.`")
 
 
 @vc_asst("unmutevc")
 async def unmute(event):
+    ultSongs = Player(event.chat_id)
     await ultSongs.group_call.set_is_mute(False)
     await eor(event, "`UnMuted playback in this chat.`")
 
 
 @vc_asst("pausevc")
 async def pauser(event):
+    ultSongs = Player(event.chat_id)
     ultSongs.group_call.pause_playout()
     await eor(event, "`Paused playback in this chat.`")
 
 
 @vc_asst("resumevc")
 async def resumer(event):
+    ultSongs = Player(event.chat_id)
     ultSongs.group_call.resume_playout()
     await eor(event, "`Resumed playback in this chat.`")
 
 
 @vc_asst("stopvc")
 async def stopper(event):
+    ultSongs = Player(event.chat_id)
     ultSongs.group_call.stop_playout()
     await eor(event, "`Stopped playback in this chat.`")
     try:
@@ -63,5 +68,6 @@ async def stopper(event):
 
 @vc_asst("replay")
 async def replayer(event):
+    ultSongs = Player(event.chat_id)
     ultSongs.group_call.restart_playout()
     await eor(event, "`Re-playing the current song.`")
