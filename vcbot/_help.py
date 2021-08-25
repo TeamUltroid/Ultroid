64086195eb2dff045f4e3ac810e5172454bdbd14
Devesh Pal <newdev0@outlook.com>
2021-08-25 21:27:08 +0530

    Update _help.py

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 from pyUltroid.dB.core import VC_HELP
 
 from . import *
