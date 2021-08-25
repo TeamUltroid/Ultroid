a64c06dd81354e07113a4b1a6c84a7eceb00a8bf
Danish <danish@ultroid.tech>
2021-08-25 19:55:57 +0530

    no

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -3,7 +3,7 @@ from pyUltroid.dB.core import VC_HELP
 from . import *
 
 
-@vc_asst("vchelp$")
+@vc_asst("vchelp")
 async def helper(event):
     await eor(
         event, "**VCBot Help Menu**\n\n" + "".join(VC_HELP[i] + "\n" for i in VC_HELP)
