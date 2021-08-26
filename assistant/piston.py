cb05ca5db68a6d48c31c005ed207bd0cdf3e7d7a
Devesh Pal <newdev0@outlook.com>
2021-08-26 22:41:25 +0530

    Create piston.py

--- assistant/piston.py
+++ assistant/piston.py
@@ -1,32 +1,29 @@
-from pistonapi import PistonAPI
 from pyUltroid.dB.core import *
+from rextester_py import rexec_aio
 from telethon import events
+from pistonapi import PistonAPI
+from . import *
 
-# By @TechiError
+#By @TechiError
 
-
-@tgbot.on(events.InlineQuery(pattern=r"run (.*)"))
+@in_pattern(r"run (.*)")
 async def teamultroid(event: events.InlineQuery.Event):
     builder = event.builder
-    bot = await tgbot.get_me()
-    bot.username
     piston = PistonAPI()
-    if event.query.user_id in sed:
-
-        omk = event.text.split(" ", maxsplit=1)[1]
-        if omk is not None:
-            if "|" in omk:
-                lang, code = omk.split("|")
-            else:
-                lang = "python 3"
-                code = omk
-            output = piston.execute(language=lang, code=code)
-            outputt = output
-            resultm = builder.article(
-                title="Code",  # By @TechiError
-                description=f"Language-`{lang}` & Code-`{code}`",
-                text=f"Language:\n`{lang}`\n\nCode:\n`{code}`\n\nResult:\n`{outputt}`",
-            )
-            await event.answer([resultm])
+    try:
+        omk = event.text.split(' ', maxsplit=1)[1]
+    except IndexError:
+        return await event.answer([], switch_pm="Enter Code...", switch_pm_param="start")
+    if "|" in omk:
+        lang, code = omk.split("|")
     else:
-        return  # By @TechiError
+        lang = "python 3"
+        code = omk
+    output = piston.execute(language=lang, code=code)
+    outputt = output
+    resultm = builder.article(
+            title="Code",#By @TechiError
+            description=f"Language-`{lang}` & Code-`{code}`",
+            text=f"Language:\n`{lang}`\n\nCode:\n`{code}`\n\nResult:\n`{outputt}`",
+    )
+    await event.answer([resultm])
