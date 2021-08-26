46f035cd4cc77db0dc446ac3c62b127084668e29
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-26 17:12:24 +0000

    pylint: auto fixes

--- assistant/piston.py
+++ assistant/piston.py
@@ -1,19 +1,22 @@
-from pyUltroid.dB.core import *
-from rextester_py import rexec_aio
-from telethon import events
 from pistonapi import PistonAPI
+from pyUltroid.dB.core import *
+from telethon import events
+
 from . import *
 
-#By @TechiError
+# By @TechiError
+
 
 @in_pattern(r"run (.*)")
 async def teamultroid(event: events.InlineQuery.Event):
     builder = event.builder
     piston = PistonAPI()
     try:
-        omk = event.text.split(' ', maxsplit=1)[1]
+        omk = event.text.split(" ", maxsplit=1)[1]
     except IndexError:
-        return await event.answer([], switch_pm="Enter Code...", switch_pm_param="start")
+        return await event.answer(
+            [], switch_pm="Enter Code...", switch_pm_param="start"
+        )
     if "|" in omk:
         lang, code = omk.split("|")
     else:
@@ -22,8 +25,8 @@ async def teamultroid(event: events.InlineQuery.Event):
     output = piston.execute(language=lang, code=code)
     outputt = output
     resultm = builder.article(
-            title="Code",#By @TechiError
-            description=f"Language-`{lang}` & Code-`{code}`",
-            text=f"Language:\n`{lang}`\n\nCode:\n`{code}`\n\nResult:\n`{outputt}`",
+        title="Code",  # By @TechiError
+        description=f"Language-`{lang}` & Code-`{code}`",
+        text=f"Language:\n`{lang}`\n\nCode:\n`{code}`\n\nResult:\n`{outputt}`",
     )
     await event.answer([resultm])
