277c25534fda08e9a14966e1e206198b23c47554
Danish <danish@ultroid.tech>
2021-08-27 17:24:15 +0530

    ur issue ig

--- plugins/updater.py
+++ plugins/updater.py
@@ -26,7 +26,7 @@ async def _(e):
         if "fast" in e.pattern_match.group(1) or "soft" in e.pattern_match.group(1):
             await bash("git pull && pip3 install -r requirements.txt")
             await xx.edit("`Fast Soft Updating...`")
-            execl(sys.executable, sys.executable, "-m", "pyUltroid")
+            execl(sys.executable, "python3", "-m", "pyUltroid")
     m = updater()
     branch = (Repo.init()).active_branch
     if m:
