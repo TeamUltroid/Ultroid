ba8bee0bbcad725712a75b6a409327be4e138d44
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-28 07:24:27 +0000

    pylint: auto fixes

--- vcbot/play.py
+++ vcbot/play.py
@@ -20,7 +20,7 @@ from . import *
 @vc_asst("play")
 async def play_music_(event):
     if "playfrom" in event.text.split()[0]:
-        return # For PlayFrom Conflict
+        return  # For PlayFrom Conflict
     xx = await eor(event, get_string("com_1"))
     chat = event.chat_id
     from_user = inline_mention(event.sender)
