314181f2f51d47d5e07414f511bf2e4bebcb8223
Danish <danish@ultroid.tech>
2021-08-28 10:31:54 +0530

    Update volume.py

--- vcbot/volume.py
+++ vcbot/volume.py
@@ -17,14 +17,29 @@ from . import *
 
 @vc_asst("volume")
 async def volume_setter(event):
-    ultSongs = Player(event.chat_id)
     if len(event.text.split()) > 1:
-        vol = event.text.split()[1]
-        if vol.isdigit():
-            await ultSongs.group_call.set_my_volume(int(vol))
-            if int(vol) > 200:
-                vol = 200
-            elif int(vol) < 1:
-                vol = 1
-            return await eor(event, "• Volume Changed to `{}%` •".format(vol))
-    await eor(event, "`Please specify a volume from 1 to 200!`")
+        inp = event.text.split()
+        if inp[1].startswith("@"):
+            chat = inp[1]
+            vol = int(inp[2])
+        elif inp[1].startswith("-"):
+            chat = int(inp[1])
+            vol = int(inp[2])
+            try:
+                chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+            except Exception as e:
+                return await eor(event, "**ERROR:**\n{}".format(str(e)))
+        elif inp[1].isdigit() and len(inp)==2:
+            vol = int(inp[1])
+            chat = event.chat_id
+    else:
+        return await eor(event, "`Please specify a volume from 1 to 200!`")
+    ultSongs = Player(chat)
+    if vol:
+        await ultSongs.group_call.set_my_volume(int(vol))
+        if vol > 200:
+            vol = 200
+        elif vol < 1:
+            vol = 1
+        return await eor(event, "• Volume Changed to `{}%` •".format(vol))
+    
