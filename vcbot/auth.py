d91947638d395ce97593308902b6ca7b7b8bf858
Devesh Pal <newdev0@outlook.com>
2021-08-30 17:06:56 +0530

    Update auth.py

--- vcbot/auth.py
+++ vcbot/auth.py
@@ -14,3 +14,21 @@ async def auth_group(event):
     else:
         udB.set("VC_AUTH_GROUPS", str(chat))
     await event.reply("Added to AUTH Groups successfully!")
+
+
+@vc_asst("remauth", from_users=[udB["OWNER"], *sudoers])
+async def auth_group(event):
+    chat = event.chat_id
+    VC_SUDO_GRPS = udB.get("VC_AUTH_GROUPS")
+    if not VC_SUDO_GRPS:
+        return await eor(event, "Vc Auths Group's List is Empty...")
+    spli = VC_SUDO_GRPS.split(" ")
+    if str(chat) not in spli:
+        return await eor(event, "Chat is Not Added to Vc Auths.!")
+    spli = spli.remove(str(chat))
+    if not spli:
+        udB.delete("VC_AUTH_GROUPS")
+    else:
+        VC_SUDO_GRPS = "".join(f" {a}" for a in spli)[1:]
+        udB.set("VC_AUTH_GROUPS", VC_SUDO_GRPS)
+    await eor(event, "Removed from Vc AUTH Groups!")
