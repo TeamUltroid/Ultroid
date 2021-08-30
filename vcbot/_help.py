91e936085ef1e80cdbd0bfb1d4c3b78a34be86ae
Devesh Pal <newdev0@outlook.com>
2021-08-30 23:05:32 +0530

    Update _help.py

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -9,10 +9,24 @@ from telethon import Button
 
 from . import *
 
+ID_CACHE = []
 
 @vc_asst("vchelp")
 async def helper(event):
-    await event.reply(
-        "**VCBot Help Menu**\n\n",
-        buttons=Button.inline("Voice Chat Help", data="vc_helper"),
-    )
+    if not ID_CACHE:
+        ID_CACHE.append((await vcClient.get_me()).id)
+    res = await event.client.inline_query(asst.me.username, "vchelp")
+    try:
+        await res[0].click(event.chat_id)
+    except Exception as e:
+        await eor(event, e)
+
+@in_pattern("vchelp")
+async def wiqhshd(e):
+    if not ID_CACHE:
+        ID_CACHE.append((await vcClient.get_me()).id)
+    if not e.sender_id in [*owner_and_sudos(), *ID_CACHE]:
+        return
+    builder = e.builder
+    res = [await builder.article(title="Vc Help", text="**VCBot Help Menu**\n\n", buttons=Button.inline("Voice Chat Help", data="vc_helper"))]
+    await e.answer(res)
