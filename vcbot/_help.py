02093f003e683481f05b83727c0a0b1f7b8063ca
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-30 17:36:36 +0000

    pylint: auto fixes

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -11,6 +11,7 @@ from . import *
 
 ID_CACHE = []
 
+
 @vc_asst("vchelp")
 async def helper(event):
     if not ID_CACHE:
@@ -21,12 +22,19 @@ async def helper(event):
     except Exception as e:
         await eor(event, e)
 
+
 @in_pattern("vchelp")
 async def wiqhshd(e):
     if not ID_CACHE:
         ID_CACHE.append((await vcClient.get_me()).id)
-    if not e.sender_id in [*owner_and_sudos(), *ID_CACHE]:
+    if e.sender_id not in [*owner_and_sudos(), *ID_CACHE]:
         return
     builder = e.builder
-    res = [await builder.article(title="Vc Help", text="**VCBot Help Menu**\n\n", buttons=Button.inline("Voice Chat Help", data="vc_helper"))]
+    res = [
+        await builder.article(
+            title="Vc Help",
+            text="**VCBot Help Menu**\n\n",
+            buttons=Button.inline("Voice Chat Help", data="vc_helper"),
+        )
+    ]
     await e.answer(res)
