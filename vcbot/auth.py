f8d84424863d7163359ee347b2c60bca8156dff9
Devesh Pal <newdev0@outlook.com>
2021-08-30 16:55:15 +0530

    Update auth.py

--- vcbot/auth.py
+++ vcbot/auth.py
@@ -1,7 +1,7 @@
 from . import *
 
 
-@vc_asst("authg")
+@vc_asst("authg", from_users=[udB["OWNER"], *sudoers])
 async def auth_group(event):
     chat = event.chat_id
     VC_SUDO_GRPS = udB.get("VC_AUTH_GROUPS")
@@ -11,7 +11,6 @@ async def auth_group(event):
             return await event.reply("Already Authed This Chat!")
         VC_SUDO_GRPS += f" {str(chat)}"
         udB.set("VC_AUTH_GROUPS", str(VC_SUDO_GRPS))
-        return await event.reply("Added to AUTH Groups successfully!")
     else:
         udB.set("VC_AUTH_GROUPS", str(chat))
-        return await event.reply("Added to AUTH Groups successfully!")
+    await event.reply("Added to AUTH Groups successfully!")
