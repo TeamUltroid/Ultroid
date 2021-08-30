385cb917c5daa3cbc45091f618f621c7dfac64bc
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-30 11:21:05 +0000

    pylint: auto fixes

--- vcbot/auth.py
+++ vcbot/auth.py
@@ -1,16 +1,17 @@
 from . import *
 
+
 @vc_asst("authg")
 async def auth_group(event):
-	chat = event.chat_id
-	VC_SUDO_GRPS = udB.get("VC_AUTH_GROUPS")
-	if VC_SUDO_GRPS:
-		xnxx = VC_SUDO_GRPS.split(" ")
-		if str(chat) in xnxx:
-			return await event.reply("Already Authed This Chat!")
-		VC_SUDO_GRPS += f" {str(chat)}"
-		udB.set("VC_AUTH_GROUPS", str(VC_SUDO_GRPS))
-		return await event.reply("Added to AUTH Groups successfully!")
-	else:
-		udB.set("VC_AUTH_GROUPS", str(chat))
-		return await event.reply("Added to AUTH Groups successfully!")
+    chat = event.chat_id
+    VC_SUDO_GRPS = udB.get("VC_AUTH_GROUPS")
+    if VC_SUDO_GRPS:
+        xnxx = VC_SUDO_GRPS.split(" ")
+        if str(chat) in xnxx:
+            return await event.reply("Already Authed This Chat!")
+        VC_SUDO_GRPS += f" {str(chat)}"
+        udB.set("VC_AUTH_GROUPS", str(VC_SUDO_GRPS))
+        return await event.reply("Added to AUTH Groups successfully!")
+    else:
+        udB.set("VC_AUTH_GROUPS", str(chat))
+        return await event.reply("Added to AUTH Groups successfully!")
