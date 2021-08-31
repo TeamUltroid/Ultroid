f3ff35ecc800963d5b9a476cd30a0ae542eb3e81
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-08-31 11:29:38 +0000

    pylint: auto fixes

--- vcbot/__init__.py
+++ vcbot/__init__.py
@@ -11,7 +11,6 @@ import subprocess
 from os import remove
 from time import time
 
-
 from pytgcalls import GroupCallFactory
 from pyUltroid import LOGS, asst, udB, vcClient
 from pyUltroid.functions.all import (
