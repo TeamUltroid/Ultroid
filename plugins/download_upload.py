5621dd5db5d5ad167df95b355cd85a986444a17d
Devesh Pal <newdev0@outlook.com>
2021-09-01 22:06:51 +0530

    Update download_upload.py

--- plugins/download_upload.py
+++ plugins/download_upload.py
@@ -130,6 +130,8 @@ async def download(event):
     pattern="ul ?(.*)",
 )
 async def download(event):
+    if event.text[1:].startswith("ultroid"):
+        return
     xx = await eor(event, get_string("com_1"))
     hmm = event.pattern_match.group(1)
     try:
