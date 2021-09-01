8a844c912de0f6d61e5a836126f2d21ac2983c29
Devesh Pal <newdev0@outlook.com>
2021-09-01 22:46:55 +0530

    test time

--- plugins/carbon.py
+++ plugins/carbon.py
@@ -12,6 +12,9 @@
 
 • `{i}rcarbon <text/reply to msg/reply to document>`
     Carbonise the text, with random bg colours.
+
+• `{i}ccarbon <color ><text/reply to msg/reply to document>`
+    Carbonise the text, with custom bg colours.
 """
 import random
 
@@ -224,6 +227,44 @@ async def crbn(event):
     )
     xx = await carbon.memorize("ultroid_carbon")
     await xxxx.delete()
+    await event.reply(
+        f"Carbonised by {inline_mention(event.sender)}",
+        file=xx,
+    )
+
+ 
+@ultroid_cmd(
+    pattern="ccarbon ?(.*)",
+)
+async def crbn(event):
+    match = event.pattern_match.group(1)
+    if not match:
+        return await eor(event, "`Give Custom Color to Create Carbon...`")
+    msg = await eor(event, "Processing")
+    if event.reply_to_msg_id:
+        temp = await event.get_reply_message()
+        if temp.media:
+            b = await event.client.download_media(temp)
+            with open(b) as a:
+                code = a.read()
+            os.remove(b)
+        else:
+            code = temp.message
+    else:
+        try:
+            match = match.split(" ", maxsplit=1)
+            code = match[1]
+            match = match[0]
+        except IndexError:
+            return await eor(msg, "`Reply to Message or readable file..`")
+    carbon = Carbon(
+        base_url="https://carbonara.vercel.app/api/cook", code=code, background=match
+    )
+    try:
+        xx = await carbon.memorize("ultroid_carbon")
+    except Exception as er:
+        return await msg.edit(str(er))
+    await msg.delete()
     await event.reply(
         f"Carbonised by {inline_mention(event.sender)}",
         file=xx,
