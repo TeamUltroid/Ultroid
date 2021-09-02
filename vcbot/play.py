84849341f5e52f283fbc920788ab20619e226a4a
Devesh Pal <newdev0@outlook.com>
2021-09-02 09:34:21 +0530

    Update play.py

--- vcbot/play.py
+++ vcbot/play.py
@@ -58,7 +58,7 @@ async def play_music_(event):
         song, thumb, song_name, duration = await download(song, chat, ts)
     ultSongs = Player(chat, event)
     if len(song_name) > 37:
-        somg_name = song_name[:35] + "..."
+        song_name = song_name[:35] + "..."
     if not ultSongs.group_call.is_connected:
         if not (await ultSongs.vc_joiner()):
             return
@@ -78,3 +78,52 @@ async def play_music_(event):
             xx,
             f"▶ Added 🎵 **{song_name}** to queue at #{list(VC_QUEUE[chat].keys())[-1]}.",
         )
+
+
+@vc_asst("playfrom")
+async def play_music_(event):
+    msg = await eor(event, get_string("com_1"))
+    chat = event.chat_id
+    limit = 10
+    from_user = inline_mention(event.sender)
+    if not len(event.text.split()) > 1:
+        return await msg.edit("Use in Proper Format\n`.playfrom <channel username> ; <limit>`")
+    input = event.text.split(maxsplit=1)[1]
+    if ";" in input:
+        try:
+            limit = input.split(" ")
+            input = limit[0]
+            limit = int(limit[1])
+        except IndexError:
+            pass
+    try:
+        fromchat = (await event.client.get_entity(await get_user_id(input))).id
+    except Exception as er:
+        return await eor(msg, str(er))
+    await eor(msg, "`• Starting Playing from Channel....`")
+    send_message = True
+    ultSongs = Player(chat, event)
+    async for song in event.client.iter_messages(fromchat,limit=limit, wait_for=10, filter=types.InputMessagesFilterMusic):
+        song, thumb, song_name, duration = await file_download(msg, song, chat, ts, str(time()).split(".")[0])
+        if len(song_name) > 37:
+            song_name = song_name[:35] + "..."
+        if not ultSongs.group_call.is_connected:
+            if not (await ultSongs.vc_joiner()):
+                return
+            ultSongs.group_call.input_filename = song
+            await msg.reply(
+            "🎸 **Now playing:** `{}`\n⏰ **Duration:** `{}`\n👥 **Chat:** `{}`\n🙋‍♂ **Requested by:** {}".format(
+                song_name, duration, chat, from_user
+            ),
+            file=thumb,
+            )
+            if thumb and os.path.exists(thumb):
+                remove(thumb)
+        else:
+            add_to_queue(chat, song, song_name, thumb, from_user, duration)
+            if send_message:
+                await eor(
+            msg,
+            f"▶ Added 🎵 **{song_name}** to queue at #{list(VC_QUEUE[chat].keys())[-1]}.",
+            )
+                send_message=False
