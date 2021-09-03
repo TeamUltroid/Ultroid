7713c41e549dd9f19a537483989d2e3cf2092020
Danish <danish@ultroid.tech>
2021-09-03 11:26:38 +0530

    Update vidstream.py

--- vcbot/vidstream.py
+++ vcbot/vidstream.py
@@ -40,14 +40,19 @@ def on_converter(link, song):
 
 
 @vc_asst("vidstream")
-async def fhehejs(ult):
+async def video_c(ult):
     try:
         match = ult.text.split(" ", maxsplit=1)[1]
     except IndexError:
         return await eor(ult, "`Give link to play...`")
     file = "vcbot/downloads/" + str(ult.chat_id) + ".raw"
     on_converter(match, file)
-    ultSongs = Player(ult.chat_id)
-    await ultSongs.startCall()
+    await asyncio.sleep(3)
+    if not os.path.exists(file):
+        return await eor(ult, "Unplayable Link")
+    ultSongs = Player(ult.chat_id, ult, True)
+    if not (await ultSongs.vc_joiner()):
+        return
     ultSongs.group_call.input_filename = file
     await ultSongs.group_call.set_video_capture(match)
+    await eor(ult, f"Playing {match}")
