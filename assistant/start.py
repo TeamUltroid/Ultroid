f1324728c2e8496a229bd5146c3a5a1470c9f148
userbotrakesh <90086841+userbotrakesh@users.noreply.github.com>
2021-09-05 10:53:50 +0530

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -23,7 +23,7 @@ Owner_info_msg = f"""
 
 <strong>Message Forwards</strong> - {udB.get("PMBOT")}
 
-<stong>Ultroid <a href=https://github.com/TeamUltroid/Ultroid>[v{ultroid_version}]</a>, powered by @TeamUltroid</strong>
+<stong>iasbabu <a href=https://t.me/ias_factory>[v{ultroid_version}]</a>, powered by @ias_factory</strong>
 """
 
 _settings = [
@@ -85,7 +85,7 @@ async def ultroid(event):
                 if udB.get("PMBOT") == "True":
                     ok = "You can contact my master using this bot!!\n\nSend your Message, I will Deliver it To Master."
                 await event.reply(
-                    f"Hey there [{get_display_name(u)}](tg://user?id={u.id}), this is Ultroid Assistant of [{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})!\n\n{ok}",
+                    f"Hey there [{get_display_name(u)}](tg://user?id={u.id}), this is  Secretary of iasBabu😊 ™️ of [{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})!\n\n{ok}",
                     buttons=[Button.inline("Info.", data="ownerinfo")],
                 )
             else:
@@ -124,7 +124,7 @@ async def ultroid(event):
 @owner
 async def botstat(event):
     ok = len(get_all_users())
-    msg = """Ultroid Assistant - Stats
+    msg = """iasbabu Assistant - Stats
 Total Users - {}""".format(
         ok,
     )
