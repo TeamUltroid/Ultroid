26edc9464eb734a43817a950e339539d993310b3
userbotrakesh <90086841+userbotrakesh@users.noreply.github.com>
2021-09-05 11:21:37 +0530

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -22,7 +22,7 @@ from . import *
 # ================================================#
 notmine = f"This bot is for {OWNER_NAME}"
 
-TLINK = "https://telegra.ph/file/d9c9bc13647fa1d96e764.jpg"
+TLINK = "https://telegra.ph/file/4c301486dcd40bea0c2b0.jpg"
 helps = get_string("inline_1")
 
 add_ons = udB.get("ADDONS")
@@ -63,7 +63,7 @@ SUP_BUTTONS = [
         Button.url("Repo", url="https://github.com/TeamUltroid/Ultroid"),
         Button.url("Addons", url="https://github.com/TeamUltroid/UltroidAddons"),
     ],
-    [Button.url("Support", url="t.me/UltroidSupport")],
+    [Button.url("Support", url="t.me/ias_factory")],
 ]
 
 # --------------------BUTTONS--------------------#
@@ -74,12 +74,12 @@ SUP_BUTTONS = [
 async def inline_alive(o):
     if len(o.text) == 0:
         b = o.builder
-        MSG = "• **Ultroid Userbot •**"
+        MSG = "• **iasbabu Userbot •**"
         uptime = time_formatter((time.time() - start_time) * 1000)
         MSG += f"\n\n• **Uptime** - `{uptime}`\n"
         MSG += f"• **OWNER** - `{OWNER_NAME}`"
         WEB0 = InputWebDocument(
-            "https://telegra.ph/file/55dd0f381c70e72557cb1.jpg", 0, "image/jpg", []
+            "https://telegra.ph/file/4c301486dcd40bea0c2b0.jpg", 0, "image/jpg", []
         )
         RES = [
             InputBotInlineResult(
@@ -90,7 +90,7 @@ async def inline_alive(o):
                     media=True,
                     buttons=SUP_BUTTONS,
                 ),
-                title="Ultroid Userbot",
+                title="iasbabu Userbot",
                 description="Userbot | Telethon",
                 url=TLINK,
                 thumb=WEB0,
@@ -438,7 +438,7 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = f"{plugin_name} has no detailed help..."
     else:
         reply_pop_up_alert = help_string
-    reply_pop_up_alert += "\n© @TeamUltroid"
+    reply_pop_up_alert += "\n© @ias_factory"
     buttons = [
         [
             Button.inline(
@@ -497,7 +497,7 @@ async def on_plug_in_callback_query_handler(event):
         reply_pop_up_alert = f"{plugin_name} has no detailed help..."
     else:
         reply_pop_up_alert = help_string
-    reply_pop_up_alert += "\n© @TeamUltroid"
+    reply_pop_up_alert += "\n© @ias_factory"
     buttons = [
         [
             Button.inline(
