114f7a79f554139cae09eea74219874d64ea4a83
Devesh Pal <newdev0@outlook.com>
2021-09-07 11:08:45 +0530

    Update tools.py

--- plugins/tools.py
+++ plugins/tools.py
@@ -40,7 +40,7 @@ import cv2
 from google_trans_new import google_translator
 from hachoir.metadata import extractMetadata
 from hachoir.parser import createParser
-from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.errors.rpcerrorlist import YouBlockedUserError, MessageTooLongError
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantsBots
 from telethon.tl.types import DocumentAttributeVideo as video
 from telethon.utils import pack_bot_file_id
@@ -341,7 +341,13 @@ async def _(e):
     if not hb(fos + fls):
         ttol = "0 B"
     text += f"\n\n`Folders` :  `{foc}` :   `{tfos}`\n`Files` :       `{flc}` :   `{tfls}`\n`Total` :       `{flc+foc}` :   `{ttol}`"
-    await eor(e, text)
+    try:
+        await eor(e, text)
+    except MessageTooLongError:
+        with io.BytesIO(str.encode(text)) as out_file:
+            out_file.name = "output.txt"
+            await e.reply(f"`{e.text}`", file=out_file,thumb="resources/extras/ultroid.jpg")
+        await e.delete()
 
 
 @ultroid_cmd(
