809a774bc10b3a714b2671025d89529a4b566cfa
Muhamad Risman Aziz <62795826+mrismanaziz@users.noreply.github.com>
2021-09-07 20:31:43 +0700

    added more fake actions

--- plugins/fakeaction.py
+++ plugins/fakeaction.py
@@ -20,6 +20,21 @@
 • `{i}fgame <time/in secs>`
     `Show Fake Game Playing Action in current chat. `
 
+• `{i}fround <time/in secs>`
+    `Show Fake video message action in current chat. `
+
+• `{i}fphoto <time/in secs>`
+    `Show Fake sending photo in current chat. `
+
+• `{i}fdocument <time/in secs>`
+    `Show Fake sending photo in current chat. `
+
+• `{i}flocation <time/in secs>`
+    `Show Fake share location in current chat. `
+
+• `{i}fcontact <time/in secs>`
+    `Show Fake share contact in current chat. `
+
 • `{i}stopaction`
    `Stop any ongoing Chat Action going in Chat.`
 """
@@ -96,6 +111,96 @@ async def _(e):
                 return await eod(e, "`Incorrect Format`")
     await eod(e, f"Starting Fake Game Playing For {t} sec.")
     async with e.client.action(e.chat_id, "game"):
+        await asyncio.sleep(t)
+
+
+@ultroid_cmd(pattern="fround ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Starting Fake video message recording For {t} sec.")
+    async with e.client.action(e.chat_id, "record-round"):
+        await asyncio.sleep(t)
+
+
+@ultroid_cmd(pattern="fphoto ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Start Fake Sending Photos For {t} sec.")
+    async with e.client.action(e.chat_id, "photo"):
+        await asyncio.sleep(t)
+
+
+@ultroid_cmd(pattern="fdocument ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Starting Fake sending document For {t} sec.")
+    async with e.client.action(e.chat_id, "document"):
+        await asyncio.sleep(t)
+
+
+@ultroid_cmd(pattern="flocation ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Starting Fake share location For {t} sec.")
+    async with e.client.action(e.chat_id, "location"):
+        await asyncio.sleep(t)
+
+
+@ultroid_cmd(pattern="fcontact ?(.*)")
+async def _(e):
+    t = e.pattern_match.group(1)
+    if not (t or t.isdigit()):
+        t = 100
+    else:
+        try:
+            t = int(t)
+        except BaseException:
+            try:
+                t = await ban_time(e, t)
+            except BaseException:
+                return await eod(e, "`Incorrect Format`")
+    await eod(e, f"Starting Fake share contact For {t} sec.")
+    async with e.client.action(e.chat_id, "contact"):
         await asyncio.sleep(t)
 
 
