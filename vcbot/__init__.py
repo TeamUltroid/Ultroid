a6805c721e673a7b199c91c70103667cbb5b0d8a
Devesh Pal <newdev0@outlook.com>
2021-09-08 11:08:38 +0530

    Update __init__.py

--- vcbot/__init__.py
+++ vcbot/__init__.py
@@ -5,7 +5,7 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-# -----------------------------------------------------------#
+# ----------------------------------------------------------#
 #                                                           #
 #    _   _ _   _____ ____   ___ ___ ____   __     ______    #
 #   | | | | | |_   _|  _ \ / _ \_ _|  _ \  \ \   / / ___|   #
