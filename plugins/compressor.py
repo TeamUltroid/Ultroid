1850b3f3dcfda6db697a536a8787976b383f8f56
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-09 14:50:46 +0530

    Idk where to add queue😐

--- plugins/compressor.py
+++ plugins/compressor.py
@@ -32,6 +32,8 @@ from telethon.tl.types import DocumentAttributeVideo
 
 from . import *
 
+compressor_queue = {}
+
 
 @ultroid_cmd(pattern="compress ?(.*)")
 async def _(e):
@@ -76,102 +78,107 @@ async def _(e):
             f'mediainfo --fullscan """{file.name}""" | grep "Frame count"'
         )
         total_frames = x.split(":")[1].split("\n")[0]
-        progress = "progress.txt"
+        number = random.randint(1, 10000)
+        progress = f"progress-{number}.txt"
         with open(progress, "w") as fk:
             pass
-        proce = await asyncio.create_subprocess_shell(
-            f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{file.name}""" -preset ultrafast -vcodec libx265 -crf {crf} """{out}""" -y',
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-        while proce.returncode != 0:
-            await asyncio.sleep(3)
-            with open(progress, "r+") as fil:
-                text = fil.read()
-                frames = re.findall("frame=(\\d+)", text)
-                size = re.findall("total_size=(\\d+)", text)
+        compressor_queue.append(progress)
+        while os.path.exists(progress) and len(compressor_queue) != 0:
+            proce = await asyncio.create_subprocess_shell(
+                f'ffmpeg -hide_banner -loglevel quiet -progress {progress} -i """{file.name}""" -preset ultrafast -vcodec libx265 -crf {crf} """{out}""" -y',
+                stdout=asyncio.subprocess.PIPE,
+                stderr=asyncio.subprocess.PIPE,
+            )
+            while proce.returncode != 0 and:
+                await asyncio.sleep(3)
+                with open(progress, "r+") as fil:
+                    text = fil.read()
+                    frames = re.findall("frame=(\\d+)", text)
+                    size = re.findall("total_size=(\\d+)", text)
 
-                if len(frames):
-                    elapse = int(frames[-1])
-                if len(size):
-                    size = int(size[-1])
-                    per = elapse * 100 / int(total_frames)
-                    time_diff = time.time() - int(d_time)
-                    speed = round(elapse / time_diff, 2)
-                if int(speed) != 0:
-                    some_eta = ((int(total_frames) - elapse) / speed) * 1000
-                    text = f"`Compressing {file_name} at {crf} CRF.\n`"
-                    progress_str = "`[{0}{1}] {2}%\n\n`".format(
-                        "".join("●" for i in range(math.floor(per / 5))),
-                        "".join("" for i in range(20 - math.floor(per / 5))),
-                        round(per, 2),
-                    )
-
-                    e_size = humanbytes(size) + " of ~" + humanbytes((size / per) * 100)
-                    eta = "~" + time_formatter(some_eta)
-                    try:
-                        await xxx.edit(
-                            text
-                            + progress_str
-                            + "`"
-                            + e_size
-                            + "`"
-                            + "\n\n`"
-                            + eta
-                            + "`"
+                    if len(frames):
+                        elapse = int(frames[-1])
+                    if len(size):
+                        size = int(size[-1])
+                        per = elapse * 100 / int(total_frames)
+                        time_diff = time.time() - int(d_time)
+                        speed = round(elapse / time_diff, 2)
+                    if int(speed) != 0:
+                        some_eta = ((int(total_frames) - elapse) / speed) * 1000
+                        text = f"`Compressing {file_name} at {crf} CRF.\n`"
+                        progress_str = "`[{0}{1}] {2}%\n\n`".format(
+                            "".join("●" for i in range(math.floor(per / 5))),
+                            "".join("" for i in range(20 - math.floor(per / 5))),
+                            round(per, 2),
                         )
-                    except MessageNotModifiedError:
-                        pass
-        os.remove(file.name)
-        c_size = os.path.getsize(out)
-        f_time = time.time()
-        difff = time_formatter((f_time - d_time) * 1000)
-        await xxx.edit(
-            f"`Compressed {humanbytes(o_size)} to {humanbytes(c_size)} in {difff}\nTrying to Upload...`"
-        )
-        differ = 100 - ((c_size / o_size) * 100)
-        caption = f"**Original Size: **`{humanbytes(o_size)}`\n"
-        caption += f"**Compressed Size: **`{humanbytes(c_size)}`\n"
-        caption += f"**Compression Ratio: **`{differ:.2f}%`\n"
-        caption += f"\n**Time Taken To Compress: **`{difff}`"
-        mmmm = await uploader(
-            out,
-            out,
-            f_time,
-            xxx,
-            "Uploading " + out + "...",
-        )
-        if to_stream:
-            metadata = extractMetadata(createParser(out))
-            duration = metadata.get("duration").seconds
-            hi, _ = await bash(f'mediainfo "{out}" | grep "Height"')
-            wi, _ = await bash(f'mediainfo "{out}" | grep "Width"')
-            height = int(hi.split(":")[1].split("pixels")[0].replace(" ", ""))
-            width = int(wi.split(":")[1].split("pixels")[0].replace(" ", ""))
-            attributes = [
-                DocumentAttributeVideo(
-                    duration=duration, w=width, h=height, supports_streaming=True
+
+                        e_size = humanbytes(size) + " of ~" + humanbytes((size / per) * 100)
+                        eta = "~" + time_formatter(some_eta)
+                        try:
+                            await xxx.edit(
+                                text
+                                + progress_str
+                                + "`"
+                                + e_size
+                                + "`"
+                                + "\n\n`"
+                                + eta
+                                + "`"
+                            )
+                        except MessageNotModifiedError:
+                            pass
+            os.remove(file.name)
+            c_size = os.path.getsize(out)
+            f_time = time.time()
+            difff = time_formatter((f_time - d_time) * 1000)
+            await xxx.edit(
+                f"`Compressed {humanbytes(o_size)} to {humanbytes(c_size)} in {difff}\nTrying to Upload...`"
+            )
+            differ = 100 - ((c_size / o_size) * 100)
+            caption = f"**Original Size: **`{humanbytes(o_size)}`\n"
+            caption += f"**Compressed Size: **`{humanbytes(c_size)}`\n"
+            caption += f"**Compression Ratio: **`{differ:.2f}%`\n"
+            caption += f"\n**Time Taken To Compress: **`{difff}`"
+            mmmm = await uploader(
+                out,
+                out,
+                f_time,
+                xxx,
+                "Uploading " + out + "...",
+            )
+            if to_stream:
+                metadata = extractMetadata(createParser(out))
+                duration = metadata.get("duration").seconds
+                hi, _ = await bash(f'mediainfo "{out}" | grep "Height"')
+                wi, _ = await bash(f'mediainfo "{out}" | grep "Width"')
+                height = int(hi.split(":")[1].split("pixels")[0].replace(" ", ""))
+                width = int(wi.split(":")[1].split("pixels")[0].replace(" ", ""))
+                attributes = [
+                    DocumentAttributeVideo(
+                        duration=duration, w=width, h=height, supports_streaming=True
+                    )
+                ]
+                await e.client.send_file(
+                    e.chat_id,
+                    mmmm,
+                    thumb="resources/extras/ultroid.jpg",
+                    caption=caption,
+                    attributes=attributes,
+                    force_document=False,
+                    reply_to=e.reply_to_msg_id,
                 )
-            ]
-            await e.client.send_file(
-                e.chat_id,
-                mmmm,
-                thumb="resources/extras/ultroid.jpg",
-                caption=caption,
-                attributes=attributes,
-                force_document=False,
-                reply_to=e.reply_to_msg_id,
-            )
+            else:
+                await e.client.send_file(
+                    e.chat_id,
+                    mmmm,
+                    thumb="resources/extras/ultroid.jpg",
+                    caption=caption,
+                    force_document=True,
+                    reply_to=e.reply_to_msg_id,
+                )
+            await xxx.delete()
+            os.remove(out)
+            os.remove(progress)
+            compressor_queue.remove(progress)
         else:
-            await e.client.send_file(
-                e.chat_id,
-                mmmm,
-                thumb="resources/extras/ultroid.jpg",
-                caption=caption,
-                force_document=True,
-                reply_to=e.reply_to_msg_id,
-            )
-        await xxx.delete()
-        os.remove(out)
-    else:
-        await eor(e, "`Reply To Video File Only`", time=5)
+            await eor(e, "`Reply To Video File Only`", time=5)
