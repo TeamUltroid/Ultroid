fe392d3bd4ad51a993a0c9e58c19b2578db9b24c
Devesh Pal <newdev0@outlook.com>
2021-09-10 13:01:44 +0530

    Ok ignore

--- plugins/_userlogs.py
+++ plugins/_userlogs.py
@@ -62,7 +62,7 @@ async def all_messages_catcher(e):
                 try:
                     media = await e.download_media()
                     await asst.send_message(
-                        NEEDTOLOG, e.message, file=media, buttons=buttons
+                        NEEDTOLOG, e.message.text, file=media, buttons=buttons
                     )
                     return os.remove(media)
                 except Exception as er:
