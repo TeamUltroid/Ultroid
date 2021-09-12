57df7da67470110e435c1602203e4586eb9603c2
Pikachu <82699010+Gladiator-007@users.noreply.github.com>
2021-09-12 18:22:12 +0530

    Update banuser.py

--- assistant/pmbot/banuser.py
+++ assistant/pmbot/banuser.py
@@ -22,8 +22,8 @@ async def banhammer(event):
         await asst.send_message(event.chat_id, f"#BAN\nUser - {target}")
         await asst.send_message(
             target,
-            "`GoodBye! You have been banned.`\n**Further messages you send will not be forwarded.**",
-        )
+            "`Fuck off! You have been banned.`\n**Further messages you send will not be forwarded.**",
+
     else:
         return await asst.send_message(event.chat_id, f"User is already banned!")
 
