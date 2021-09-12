4346caa2cfe0d95d37e7e760ad9d0c453194a510
Devesh Pal <newdev0@outlook.com>
2021-09-12 22:29:12 +0530

    Maybe someday back

--- plugins/fakeaction.py
+++ plugins/fakeaction.py
@@ -20,9 +20,6 @@
 • `{i}fgame <time/in secs>`
     `Show Fake Game Playing Action in current chat.`
 
-• `{i}fsticker <time/in secs>`
-    `Show Fake sticker Action in current chat.`
-
 • `{i}flocation <time/in secs>`
     `Show Fake location Action in current chat.`
 
@@ -45,7 +42,7 @@ from . import *
 
 
 @ultroid_cmd(
-    pattern="f(typing|audio|contact|document|game|sticker|location|photo|round|video) ?(.*)"
+    pattern="f(typing|audio|contact|document|game|location|photo|round|video) ?(.*)"
 )
 async def _(e):
     act = e.pattern_match.group(1)
