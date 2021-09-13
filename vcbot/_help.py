aaf914309eff4f1b5de43ba7bdb970bda7bec4cf
Devesh Pal <newdev0@outlook.com>
2021-09-13 18:01:56 +0530

    Update _help.py

--- vcbot/_help.py
+++ vcbot/_help.py
@@ -5,17 +5,12 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-from telethon import Button, events
-
+from telethon import Button
 from . import *
-
-ID_CACHE = []
 
 
 @vc_asst("vchelp")
 async def helper(event):
-    if not ID_CACHE:
-        ID_CACHE.append(str((await vcClient.get_me()).id))
     res = await event.client.inline_query(asst.me.username, "vchelp")
     try:
         await res[0].click(event.chat_id)
@@ -23,12 +18,8 @@ async def helper(event):
         await eor(event, e)
 
 
-@asst.on(events.InlineQuery(pattern="vchelp"))
-async def wiqhshd(e):
-    if not ID_CACHE:
-        ID_CACHE.append(str((await vcClient.get_me()).id))
-    if str(e.sender_id) not in [*owner_and_sudos(), *ID_CACHE]:
-        return
+@in_pattern("vchelp")
+async def wiqhshd(e)
     builder = e.builder
     res = [
         await builder.article(
