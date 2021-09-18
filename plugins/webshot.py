b3220438ea92bd0cc94413a0e671d199678bf86f
Danish <danish@ultroid.tech>
2021-09-18 12:04:53 +0530

    Update webshot.py

--- plugins/webshot.py
+++ plugins/webshot.py
@@ -14,34 +14,19 @@
 """
 import os
 
-import requests
 from htmlwebshot import WebShot
 
 from . import *
 
 
-@ultroid_cmd(pattern="webshot")
+@ultroid_cmd(pattern="webshot ?(.*)")
 async def webss(event):
     xx = await eor(event, get_string("com_1"))
-    mssg = event.text.split(" ", maxsplit=2)
-    try:
-        xurl = mssg[1]
-    except IndexError:
+    xurl = e.pattern_match.group(1)
+    if not xurl:
         return await eor(xx, "`Give a URL please!`", time=5)
-    try:
-        requests.get(xurl)
-    except requests.ConnectionError:
+    elif not is_url_ok(xurl):
         return await eor(xx, "Invalid URL!", time=5)
-    except requests.exceptions.MissingSchema:
-        try:
-            xurl = "https://" + xurl
-            requests.get(xurl)
-        except requests.ConnectionError:
-            try:
-                xurl = "http://" + xurl
-                requests.get(xurl)
-            except requests.ConnectionError:
-                return await eor(xx, "Invalid URL!", time=5)
     shot = WebShot(quality=88, flags=["--enable-javascript", "--no-stop-slow-scripts"])
     pic = await shot.create_pic_async(url=xurl)
     await xx.reply(
