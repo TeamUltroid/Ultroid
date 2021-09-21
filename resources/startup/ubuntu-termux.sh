0ef6e06ea8ce68d49de00afb36e8e3f52b19d009
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-21 20:55:08 +0530

    Paths left

---
+++ resources/startup/ubuntu-termux.sh
@@ -0,0 +1,40 @@
+#!/usr/bin/bash
+
+directory="ubuntu-for-ultroid"
+architecture=$(dpkg --print-architecture)
+
+install_ubuntu(){
+    if [ "$architecture" = "arm" ];
+    then
+        architecture="armhf"
+    elif [ "$architecture" = "aarch64" ];
+    then
+        architecture="arm64"
+    elif [ "$architecture" = "amd64" ];
+    then
+        architecture="amd64"
+    elif [ "$architecture" = "x86_64" ];
+    then
+        architecture="amd64"
+    else
+        echo "Unknown Architecture - $architecture!!"
+        exit 1
+    fi
+
+    echo "Your architecture is $architecture. Downloading ubuntu package for your architecture."
+    wget http://cdimage.ubuntu.com/ubuntu-base/releases/21.04/release/ubuntu-base-21.04-base-${architecture}.tar.gz -O "ubuntu.tar.gz"
+
+    root=`pwd`
+    mkdir -p $directory && cd $directory
+    tar -zxf $root/ubuntu.tar.gz --exclude='dev'||:
+    echo "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > etc/resolv.conf
+    stubs=()
+    stubs+=('usr/bin/groups')
+    for e in ${stubs[@]};
+    do
+        echo -e "#!/bin/sh\nexit" > "$e"
+    done
+    cd $root
+}       
+
+install_ubuntu
