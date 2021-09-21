a10c6016e7f84cf2b1b274ad6c700a4f0bc5d9f8
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-21 20:54:13 +0530

    Update termux.sh

--- resources/startup/termux.sh
+++ resources/startup/termux.sh
@@ -7,9 +7,6 @@
 # PLease read the GNU Affero General Public License in <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
 
-directory="ubuntu-for-ultroid"
-architecture=$(dpkg --print-architecture)
-    
 main(){
     install_ubuntu
     update_packages
@@ -19,40 +16,6 @@ main(){
     install_dependencies
     # should_start
 }
-
-install_ubuntu(){
-    if [ "$architecture" = "arm" ];
-    then
-        architecture="armhf"
-    elif [ "$architecture" = "aarch64" ];
-    then
-        architecture="arm64"
-    elif [ "$architecture" = "amd64" ];
-    then
-        architecture="amd64"
-    elif [ "$architecture" = "x86_64" ];
-    then
-        architecture="amd64"
-    else
-        echo "Unknown Architecture - $architecture!!"
-        exit 1
-    fi
-
-    echo "Your architecture is $architecture. Downloading ubuntu package for your architecture."
-    wget http://cdimage.ubuntu.com/ubuntu-base/releases/21.04/release/ubuntu-base-21.04-base-${architecture}.tar.gz -O "ubuntu.tar.gz"
-
-    root=`pwd`
-    mkdir -p $directory && cd $directory
-    tar -zxf $root/ubuntu.tar.gz --exclude='dev'||:
-    echo "nameserver 8.8.8.8\nnameserver 8.8.4.4\n" > etc/resolv.conf
-    stubs=()
-    stubs+=('usr/bin/groups')
-    for e in ${stubs[@]};
-    do
-        echo -e "#!/bin/sh\nexit" > "$e"
-    done
-    cd $root
-}       
 
 update_packages(){
     echo "Updating Packages..."
