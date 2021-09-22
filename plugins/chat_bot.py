d269d928c78bad77a973c2582d7d9344e95d9aed
Devesh Pal <newdev0@outlook.com>
2021-09-23 02:02:40 +0530

    Update chat_bot.py

--- plugins/chat_bot.py
+++ plugins/chat_bot.py
@@ -21,7 +21,7 @@
    List the currently AI added users.
 """
 
-from pyUltroid.functions.chatBot_db import *
+from pyUltroid.dB.chatBot_db import *
 from pyUltroid.functions.helper import get_chatbot_reply
 
 
