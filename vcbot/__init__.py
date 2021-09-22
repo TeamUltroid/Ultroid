6a35f34d5fad4f39c668b65671a989661fc42632
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-22 23:08:53 +0530

    Update __init__.py

--- vcbot/__init__.py
+++ vcbot/__init__.py
@@ -37,8 +37,7 @@ from pyUltroid.functions.helper import (
 from pyUltroid.functions.tools import is_url_ok
 from pyUltroid.functions.ytdl import get_videos_link
 from pyUltroid.functions.info import get_user_id
-from pyUltroid.functions.vc_group import check_vcauth
-from pyUltroid.functions.vc_group import get_chats as get_vc
+from pyUltroid.dB.vc_group import check_vcauth, get_chats as get_vc
 from pyUltroid.misc import owner_and_sudos, sudoers
 from pyUltroid.misc._assistant import admin_check, in_pattern
 from pyUltroid.misc._wrappers import eod, eor
