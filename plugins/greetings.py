cd532357cefdcfe09bd4e650e65ba8fd4f788965
Devesh Pal <newdev0@outlook.com>
2021-09-23 16:15:27 +0530

    Update greetings.py

--- plugins/greetings.py
+++ plugins/greetings.py
@@ -33,7 +33,7 @@
 """
 import os
 
-from pyUltroid.functions.greetings_db import *
+from pyUltroid.dB.greetings_db import *
 from telegraph import upload_file as uf
 from telethon.utils import pack_bot_file_id
 
