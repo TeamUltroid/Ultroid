533f8d53f6bfde3d249f54c7c6264c62f38202a2
TechiError <error@notavailable.live>
2021-09-24 20:05:56 +0530

    Kuch toh karlo

--- plugins/words.py
+++ plugins/words.py
@@ -68,7 +68,7 @@ async def mean(event):
 )
 async def mean(event):
     wrd = event.text.split(" ", maxsplit=1)[1]
-    ok = await get_synonyms_or_antonyms(wrd)
+    ok = await get_synonyms_or_antonyms(wrd, "synonyms")
     x = get_string("wrd_2").format(wrd)
     try:
         for c, i in enumerate(ok, start=1):
@@ -97,7 +97,7 @@ async def mean(event):
 async def mean(event):
     evid = event.message.id
     wrd = event.text.split(" ", maxsplit=1)[1]
-    ok = await get_synonyms_or_antonyms(wrd)
+    ok = await get_synonyms_or_antonyms(wrd, "antonyms")
     x = get_string("wrd_3").format(wrd)
     c = 1
     try:
