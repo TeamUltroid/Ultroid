d8d1bcb22e82f2ca569e185b5113a88623b02f76
Devesh Pal <newdev0@outlook.com>
2021-09-25 01:24:04 +0530

    Update unsplash.py

--- plugins/unsplash.py
+++ plugins/unsplash.py
@@ -9,12 +9,9 @@
 
 • {i}unsplash <search query> ; <no of pics>
     Unsplash Image Search.
-
 """
 
-import requests as r
-from bs4 import BeautifulSoup as bs
-
+from pyUltroid.functions.misc import unsplashsearch
 from . import *
 
 
@@ -29,22 +26,16 @@ async def searchunsl(ult):
     else:
         num = 5
         query = match
-    tep = await eor(ult, "`Processing... `")
-    res = autopicsearch(query)
-    if len(res) == 0:
+    tep = await eor(ult, get_string("com_1"))
+    res = await unsplashsearch(query, limit=num)
+    if not res:
         return await eor(ult, "No Results Found !", time=5)
-    qas = res[:num]
-    dir = "resources/downloads"
-    CL = []
-    nl = 0
-    for rp in qas:
-        li = "https://unsplash.com" + rp["href"]
-        ct = r.get(li).content
-        bst = bs(ct, "html.parser", from_encoding="utf-8")
-        ft = bst.find_all("img", "oCCRx")[0]["src"]
-        Hp = dir + "img" + f"{nl}.png"
-        await download_file(ft, Hp)
+    dir = "resources/downloads/"
+    CL, nl= [], 0
+    for rp in res:
+        Hp = await downloas_file(ft, f"{dir}img-{nl}.png"
         CL.append(Hp)
         nl += 1
-    await ult.client.send_file(ult.chat_id, CL, caption=f"Uploaded {len(qas)} Images\n")
+    await ult.client.send_file(ult.chat_id, CL, caption=f"Uploaded {len(res)} Images\n")
     await tep.delete()
+    [os.remove(img) for img in CL)
