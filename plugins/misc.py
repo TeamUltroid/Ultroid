ae8d10a167dfd0d87db423624bce1c2afe2f4dde
Devesh Pal <newdev0@outlook.com>
2021-09-27 00:39:39 +0530

    Update and rename misctools.py to misc.py

--- plugins/misc.py
+++ plugins/misc.py
@@ -17,6 +17,9 @@
 
 • `{i}randomuser`
    Generate details about a random user.
+
+• `{i}ascii <reply image>`
+    Convert replied image into html.
 """
 
 import os
@@ -117,3 +120,26 @@ async def _gen_data(event):
     msg, pic = get_random_user_data()
     await event.reply(file=pic, message=msg)
     await x.delete()
+
+
+from htmlwebshot import WebShot
+from img2html.converter import Img2HTMLConverter
+
+
+@ultroid_cmd(
+    pattern="ascii ?(.*)",
+)
+async def _(e):
+    if not e.reply_to_msg_id:
+        return await eor(e, get_string("ascii_1"))
+    m = await eor(e, get_string("ascii_2"))
+    img = await (await e.get_reply_message()).download_media()
+    char = "■" if not e.pattern_match.group(1) else e.pattern_match.group(1)
+    converter = Img2HTMLConverter(char=char)
+    html = converter.convert(img)
+    shot = WebShot(quality=85)
+    pic = await shot.create_pic_async(html=html)
+    await m.delete()
+    await e.reply(file=pic)
+    os.remove(pic)
+    os.remove(img)
