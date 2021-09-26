4c1d810becbdccc3cc62703dd61b426baab892c4
Devesh Pal <newdev0@outlook.com>
2021-09-27 00:53:27 +0530

    Update imagetools.py

--- plugins/imagetools.py
+++ plugins/imagetools.py
@@ -7,9 +7,6 @@
 """
 ✘ Commands Available -
 
-• `{i}image <text or reply to html or any doc file>`
-   Creates image from html and any text.
-
 • `{i}border <reply to photo/sticker>`
     To create border around that media..
     Ex - `{i}border 12,22,23`
@@ -70,33 +67,6 @@ from telethon.errors.rpcerrorlist import (
 )
 
 from . import *
-
-
-@ultroid_cmd(pattern="image ?(.*)")
-async def f2i(e):
-    txt = e.pattern_match.group(1)
-    if txt:
-        html = e.text.split(maxsplit=1)[1]
-    elif e.reply_to:
-        r = await e.get_reply_message()
-        if r.media:
-            html = await e.client.download_media(r.media)
-        elif r.text:
-            html = r.text
-    else:
-        return await eod(e, "`Either reply to any file or give any text`")
-    html = html.replace("\n", "<br>")
-    shot = WebShot(quality=85)
-    css = "body {background: white;} p {color: red;}"
-    pic = await shot.create_pic_async(html=html, css=css)
-    try:
-        await e.reply(file=pic)
-    except BaseException:
-        await e.reply(file=pic, force_document=True)
-    os.remove(pic)
-    if os.path.exists(html):
-        os.remove(html)
-
 
 @ultroid_cmd(
     pattern="sketch$",
