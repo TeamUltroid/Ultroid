88889080c046c3c7c400c96e150e1d5853ec4e1c
Devesh Pal <newdev0@outlook.com>
2021-09-27 00:22:30 +0530

    Update searches.py

--- plugins/searches.py
+++ plugins/searches.py
@@ -7,6 +7,9 @@
 """
 ✘ Commands Available -
 
+• `{i}saavn <search query>`
+    Download songs from Saavn.
+
 • `{i}google <query>`
     For doing google search.
 
@@ -174,3 +177,37 @@ async def reverse(event):
     )
     rmtree(f"./resources/downloads/{text}/")
     os.remove(dl)
+
+
+from telethon.tl.types import DocumentAttributeAudio
+
+
+@ultroid_cmd(
+    pattern="saavn ?(.*)",
+)
+async def siesace(e):
+    song = e.pattern_match.group(1)
+    if not song:
+        return await eor(e, "`Give me Something to Search", time=5)
+    hmm = time.time()
+    lol = await eor(e, f"`Searching {song} on Saavn...`")
+    song, duration, performer, thumb = await saavn_dl(song)
+    if not song:
+        return await eod(lol, "`Song not found...`")
+    title = song.split(".")[0]
+    okk = await uploader(song, song, hmm, lol, "Uploading " + title + "...")
+    await e.reply(
+        file=okk,
+        message="`" + title + "`" + "\n`From Saavn`",
+        attributes=[
+            DocumentAttributeAudio(
+                duration=int(duration),
+                title=title,
+                performer=performer,
+            )
+        ],
+        supports_streaming=True,
+        thumb=thumb,
+    )
+    await lol.delete()
+    [os.remove(x) for x in [song, thumb]]
