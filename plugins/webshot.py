09565ce80f05270290463ca88e43d9b85082cbf6
Devesh Pal <newdev0@outlook.com>
2021-09-26 08:58:35 +0530

    Update webshot.py

--- plugins/webshot.py
+++ plugins/webshot.py
@@ -24,13 +24,13 @@ async def webss(event):
     xx = await eor(event, get_string("com_1"))
     xurl = event.pattern_match.group(1)
     if not xurl:
-        return await eor(xx, "`Give a URL please!`", time=5)
+        return await eor(xx, get_string("wbs_1"), time=5)
     elif not is_url_ok(xurl):
-        return await eor(xx, "Invalid URL!", time=5)
+        return await eor(xx, get_string("wbs_2"), time=5)
     shot = WebShot(quality=88, flags=["--enable-javascript", "--no-stop-slow-scripts"])
     pic = await shot.create_pic_async(url=xurl)
     await xx.reply(
-        f"**WebShot Generated**\n**URL**: {xurl}",
+        get_string("wbs_3").format(xurl), 
         file=pic,
         link_preview=False,
         force_document=True,
