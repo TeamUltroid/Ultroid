d24ee61573f6f8068235956d9175b09741ef1344
Danish <danish@ultroid.tech>
2021-09-27 15:43:39 +0530

    nu kang

--- plugins/botecho.py
+++ plugins/botecho.py
@@ -12,48 +12,19 @@
 """
 
 import re
+from pyUltroid.functions.tools import create_tl_btn, format_btn, get_msg_button
 
 from . import *
 
-regex = r"\[(.*)\]\((\S*)\)"
-
-
-def generate_url_button(text):
-    btns = []
-    if not text:
-        return None
-    bt_txt = re.sub(regex, "", text) or None
-    matches = re.finditer(regex, text, re.MULTILINE)
-    if not matches:
-        return None
-    for i, match in enumerate(matches):
-        if match.group(2).endswith(":same"):
-            btnurl = match.group(2)[:-5]
-            if i == 0:
-                btns.append([Button.url(text=match.group(1), url=btnurl)])
-            else:
-                btns[-1].append(Button.url(text=match.group(1), url=btnurl))
-        else:
-            btns.append([Button.url(text=match.group(1), url=match.group(2))])
-    if not btns:
-        btns = None
-    return bt_txt, btns
-
-
-@ultroid_cmd(pattern="botecho")
-async def button_parser(event):
-    try:
-        text = event.text.split(" ", 1)[1]
-    except IndexError:
+@ultroid_cmd(pattern="button")
+async def butt(event):
+    text = event.text.split(maxsplit=1)
+    if not len(text) > 1:
         return await eor(
             event,
-            f"**Please give some text!**\n**Format:** `{HNDLR}botecho text \n[button_text_1](https://t.me/TheUltroid)\n[button_text_2](https://t.me/TeamUltroid)`",
+            f"**Please give some text in correct format.**",
         )
-    text, buttons = generate_url_button(text)
-    try:
-        if text is None:
-            return await eor(event, "`Please provide a text too!`")
-        await asst.send_message(event.chat_id, text, buttons=buttons)
-        await eor(event, "Done. Message sent.")
-    except Exception as e:
-        await eor(event, "**ERROR:**\n{}".format(str(e)), time=5)
+    text, buttons = get_msg_button(text)
+    if buttons:
+        buttons = create_tl_btn(buttons)
+    return await something(event, text, None, btn)
