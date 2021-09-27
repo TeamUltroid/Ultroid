e6718d16640262b3e038f8edf028b1f81a8200c4
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-09-27 10:03:53 +0000

    pylint: auto fixes

--- plugins/asstcmd.py
+++ plugins/asstcmd.py
@@ -19,10 +19,10 @@
 import os
 
 from pyUltroid.dB.asstcmd_db import *
+from pyUltroid.functions.tools import create_tl_btn, format_btn, get_msg_button
 from telegraph import upload_file as uf
 from telethon.utils import pack_bot_file_id
-from pyUltroid.functions.tools import create_tl_btn, format_btn, get_msg_button
-from ._builder import something
+
 from . import *
 
 
