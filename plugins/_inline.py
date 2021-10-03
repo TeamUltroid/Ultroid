75c7b186f4ddbc60c8b9293f503a62db6ad93292
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-03 12:27:03 +0000

    pylint: auto fixes

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -20,7 +20,7 @@ from telethon import Button
 from telethon.tl.types import InputWebDocument, Message
 from telethon.utils import resolve_bot_file_id
 
-from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, start_time, udB, INLINE_PIC
+from . import HNDLR, INLINE_PIC, LOGS, OWNER_NAME, asst, get_string, start_time, udB
 from ._help import _main_help_menu
 
 # ================================================#
