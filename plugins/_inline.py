883fd8444b9d3677c3d028b6bd71568f631a07f9
Devesh Pal <newdev0@outlook.com>
2021-10-03 17:56:14 +0530

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -20,7 +20,7 @@ from telethon import Button
 from telethon.tl.types import InputWebDocument, Message
 from telethon.utils import resolve_bot_file_id
 
-from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, start_time, udB
+from . import HNDLR, LOGS, OWNER_NAME, asst, get_string, start_time, udB, INLINE_PIC
 from ._help import _main_help_menu
 
 # ================================================#
