c3f683b3b82ac2c6dfadecaa31f87240d65d9949
Danish <danish@ultroid.tech>
2021-10-05 10:37:34 +0530

    Update globaltools.py

--- plugins/globaltools.py
+++ plugins/globaltools.py
@@ -390,7 +390,7 @@ async def _(e):
     await xx.edit(gb_msg)
 
 
-@ultroid_cmd(pattern="g(admin|)cast ?(.*)", fullsudo=True)
+@ultroid_cmd(pattern="g(admin|)cast ?(.+)", fullsudo=True)
 async def gcast(event):
     text, btn, reply = "", None, None
     xx = event.pattern_match.group(2)
