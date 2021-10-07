b35864585a4f45998eac16ec235d83eac3646169
Devesh Pal <newdev0@outlook.com>
2021-10-07 11:29:50 +0530

    Update controls.py

--- vcbot/controls.py
+++ vcbot/controls.py
@@ -16,6 +16,9 @@
 
 • `{i}rejoin`
    Re-join the voice chat, incase of errors.
+
+• `{i}volume <number>`
+   Put number between 1 to 100
 """
 
 from pytgcalls.exceptions import NotConnectedError
@@ -79,3 +82,35 @@ async def rejoiner(event):
     except NotConnectedError:
         return await eor(event, "You haven't connected to a voice chat!")
     await eor(event, "`Re-joining this voice chat.`")
+
+
+@vc_asst("volume")
+async def volume_setter(event):
+    if len(event.text.split()) <= 1:
+        return await eor(event, "`Please specify a volume from 1 to 200!`")
+    inp = event.text.split()
+    if inp[1].startswith("@"):
+        chat = inp[1]
+        vol = int(inp[2])
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, "**ERROR:**\n{}".format(str(e)))
+    elif inp[1].startswith("-"):
+        chat = int(inp[1])
+        vol = int(inp[2])
+        try:
+            chat = int("-100" + str((await vcClient.get_entity(chat)).id))
+        except Exception as e:
+            return await eor(event, "**ERROR:**\n{}".format(str(e)))
+    elif inp[1].isdigit() and len(inp) == 2:
+        vol = int(inp[1])
+        chat = event.chat_id
+    if vol:
+        ultSongs = Player(chat)
+        await ultSongs.group_call.set_my_volume(int(vol))
+        if vol > 200:
+            vol = 200
+        elif vol < 1:
+            vol = 1
+        return await eor(event, "• Volume Changed to `{}%` •".format(vol))
