a4dc97b41b0f71923ea897b8c8147d9f1c095efb
Devesh Pal <newdev0@outlook.com>
2021-10-09 23:45:46 +0530

    Update start.py

--- assistant/start.py
+++ assistant/start.py
@@ -18,7 +18,7 @@ from strings.strings import get_string
 
 from . import *
 
-Owner_info_msg = f"""
+Owner_info_msg = udB.get("BOT_INFO_START") or f"""
 <strong>Owner</strong> - {OWNER_NAME}
 <strong>OwnerID</strong> - <code>{OWNER_ID}</code>
 
@@ -92,7 +92,7 @@ async def ultroid(event):
                 ok = "You can contact my master using this bot!!\n\nSend your Message, I will Deliver it To Master."
             await event.reply(
                 f"Hey there [{get_display_name(u)}](tg://user?id={u.id}), this is Ultroid Assistant of [{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})!\n\n{ok}",
-                buttons=[Button.inline("Info.", data="ownerinfo")],
+                buttons=[Button.inline("Info.", data="ownerinfo")] if Owner_info_msg != "False" else None,
             )
         else:
             me = f"[{ultroid_bot.me.first_name}](tg://user?id={ultroid_bot.uid})"
