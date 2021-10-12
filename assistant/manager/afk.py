2237da57e583104944f72744244ca7766567a50e
Devesh Pal <competitive.schooling.info@gmail.com>
2021-10-13 01:14:02 +0530

    k

--- assistant/manager/afk.py
+++ assistant/manager/afk.py
@@ -27,6 +27,8 @@ async def go_afk(event):
     replied = await event.get_reply_message()
     if not reason and replied.text and not replied.media:
       reason = replied.text
+    else:
+      reason = replied
   time_ = time.time()
   if AFK.get(event.chat_id):
     AFk[event.chat_id].update({event.sender_id:{"reason":reason,"time":time_}})
@@ -49,7 +51,7 @@ async def make_change(event):
     name = get_display_name(event.sender)
     cha_send = chat_[event.sender_id]
     reason = cha_send["reason"]
-    msg = f"**{name}** is No Longer AFK!\n**Was AFK for** {time_formatter((cha_send['time']-time.time())*1000)}"
+    msg = f"**{name}** is No Longer AFK!\n**Was AFK for** {time_formatter(cha_send['time']-time.time())}"
     await event.reply(msg)
     del chat_[event.sender_id]
     if not chat_:
@@ -61,8 +63,8 @@ async def make_change(event):
     if replied.sender_id in chat_.keys():
       s_der = chat_[replied.sender_id]
       res_ = s_der["reason"]
-      time_ = time_formatter((s_der["time"] - time.time())*1000)
-      msg = f"**{name}** is AFK Currently!\n**From :** {time_}" 
+      time_ = time_formatter(s_der["time"] - time.time()
+            msg = f"**{name}** is AFK Currently!\n**From :** {time_}" 
       if res_ and isinstance(res_, str):
         msg += f"\n**Reason :** {res_}"
       elif res_ and isinstance(res_, Message):
