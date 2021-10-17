a33ef8bdc85bd32a0ba25c62e7ead8fda6611006
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-17 08:00:16 +0000

    pylint: auto fixes

--- plugins/nightmode.py
+++ plugins/nightmode.py
@@ -34,7 +34,7 @@ from pyUltroid.dB.night_db import *
 from telethon.tl.functions.messages import EditChatDefaultBannedRightsRequest
 from telethon.tl.types import ChatBannedRights
 
-from . import LOGS, eor, ultroid_bot, ultroid_cmd, get_string
+from . import LOGS, eor, get_string, ultroid_bot, ultroid_cmd
 
 
 @ultroid_cmd(pattern="nmtime ?(.*)")
