0dbfba2e52e67d3a129c268ab0524aafbe726eae
Devesh Pal <newdev0@outlook.com>
2021-10-21 13:38:35 +0530

    Update games.py

--- assistant/games.py
+++ assistant/games.py
@@ -80,6 +80,7 @@ async def choose_cata(event):
             re_json=True,
         )
         qs = qsss["results"]
+        LOGS.info(qsss)
         TRIVIA_CHATS.update({event.chat_id: {}})
         for q in qs:
             opts = [PollAnswer(q["correct_answer"], b"0")]
