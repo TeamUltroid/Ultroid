52e5a0122ed1c91ccae975a977c89f72913f7bf4
Danish <danish@ultroid.tech>
2021-10-22 22:55:52 +0530

    fixed

--- assistant/manager/afk.py
+++ assistant/manager/afk.py
@@ -100,7 +100,7 @@ async def make_change(event):
             s_der = chat_[entity.id]
             name = get_display_name(entity)
             res_ = s_der["reason"]
-            time_ = time_formatter((dt.now() - s_der["time"]).microseconds / 1000)
+            time_ = time_formatter((dt.now() - s_der["time"]).seconds * 1000)
             msg = f"**{name}** is AFK Currently!\n**From :** {time_}"
             if res_ and isinstance(res_, str):
                 msg += f"\n**Reason :** {res_}"
