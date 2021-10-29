c1bb49c34ed2e844ea33519b87ee15b2997656f6
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-10-29 13:27:09 +0000

    pylint: auto fixes

--- plugins/giftools.py
+++ plugins/giftools.py
@@ -76,13 +76,14 @@ async def reverse_gif(event):
     if not (a and a.media) and "video" not in mediainfo(a.media):
         return await eor(e, "`Reply To Video only`", time=5)
     msg = await eor(e, get_string("com_1"))
-    dur = a.file.duration
+    a.file.duration
     file = await a.download_media()
     await bash(f'ffmpeg -i "{file}" -vf reverse -af areverse reversed.mp4 -y')
     await event.respond("- **Reversed Video/GIF**", file="reversed.mp4")
     await msg.delete()
     os.remove(file)
     os.remove("reversed.mp4")
+
 
 @ultroid_cmd(pattern="gif ?(.*)")
 async def gifs(ult):
