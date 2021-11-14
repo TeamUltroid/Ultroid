e0304f7a666c3d03158e50d74bf673ad0ecc8284
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-11-14 05:30:25 +0000

    pylint: auto fixes

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -107,10 +107,7 @@ async def _(event):
     if event.reply_to_msg_id:
         reply_to_id = event.reply_to_msg_id
     if (
-        any(
-            item in cmd
-            for item in KEEP_SAFE().All
-        )
+        any(item in cmd for item in KEEP_SAFE().All)
         and event.sender_id != ultroid_bot.uid
     ):
         if event.sender_id in _ignore_eval:
