aca083ccdb1fd20870cb6c5ab1f791e7f6b9ea3c
Devesh Pal <newdev0@outlook.com>
2021-11-15 23:02:05 +0530

    Update and rename clean_action.py to cleanaction.py

--- plugins/cleanaction.py
+++ plugins/cleanaction.py
@@ -18,20 +18,27 @@
 
 """
 
-from pyUltroid.dB.clean_db import add_clean, rem_clean
+from telethon.utils import get_display_name
 
 from . import eor, get_string, udB, ultroid_cmd
 
 
 @ultroid_cmd(pattern="addclean$", admins_only=True)
 async def _(e):
-    add_clean(e.chat_id)
+    key = self.get_key("CLEANCHAT") or []
+    if e.chat_id in key:
+        return await eod(e, get_string("clan_5"))
+    key.append(e.chat_id)
+    udB.set_key("CLEANCHAT", key)
     await eor(e, get_string("clan_1"), time=5)
 
 
 @ultroid_cmd(pattern="remclean$")
 async def _(e):
-    rem_clean(e.chat_id)
+    key = udB.get_key("CLEANCHAT") or []
+    if e.chat_id in key:
+        key.remove(e.chat_id)
+        udB.set_key("CLEANCHAT", key)
     await eor(e, get_string("clan_2"), time=5)
 
 
@@ -39,14 +46,13 @@ async def _(e):
 async def _(e):
     k = udB.get_key("CLEANCHAT")
     if k:
-        k = k.split(" ")
         o = ""
         for x in k:
             try:
-                title = e.chat.title
+                title = get_display_name(await e.client.get_entity(x))
             except BaseException:
                 title = get_string("clan_3")
-            o += x + " " + title + "\n"
+            o += f"{x} {title}\n"
         await eor(e, o)
     else:
         await eor(e, get_string("clan_4"), time=5)
