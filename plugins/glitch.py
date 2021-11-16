e87a9c665b2e884e5260d2eaffefa7f0c40136ba
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-11-16 19:28:14 +0530

    Typo (#211)
    Co-authored-by: Vɪɴᴀʏᴀᴋ Pᴀɴᴅᴇʏ <87496159+Harpia-Vieillot@users.noreply.github.com>

--- plugins/glitch.py
+++ plugins/glitch.py
@@ -31,7 +31,7 @@ async def _(e):
     wut = mediainfo(reply.media)
     if not wut.startswith(("pic", "sticker")):
         return await eor(e, get_string("com_4"))
-    xx = await eor(e, "`Gliching...`")
+    xx = await eor(e, "`Glitching...`")
     ok = await e.client.download_media(reply.media)
     cmd = f"glitch_me gif --line_count 200 -f 10 -d 50 '{ok}' ult.gif"
     stdout, stderr = await bash(cmd)
