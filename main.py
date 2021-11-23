dd8765f4ca8f4e39d9e0eb7771d14ee883866d25
Danish <danish@ultroid.tech>
2021-11-23 13:02:39 +0530

    Update main.py

--- main.py
+++ main.py
@@ -16,12 +16,13 @@ def _check(z):
 
 
 for z in range(5):
+    n = z + 1
     if z == 0:
         z = ""
     fine, out = _check(str(z))
     if fine:
         subprocess.Popen(
-            ["python3", "-m", "pyUltroid", out[0], out[1], out[2], out[3], out[4]],
+            ["python3", "-m", "pyUltroid", out[0], out[1], out[2], out[3], out[4], n],
             stdin=None,
             stderr=None,
             stdout=None,
