13605657133949352f368adb2a3e84089f12157c
Devesh Pal <69723581+New-dev0@users.noreply.github.com>
2021-11-26 23:06:28 +0530

    Update _userlogs.py

--- plugins/_userlogs.py
+++ plugins/_userlogs.py
@@ -41,10 +41,13 @@ async def all_messages_catcher(e):
     y = e.chat
     where_n, who_n = get_display_name(y), get_display_name(x)
     where_l = e.message_link
-    buttons = [
-        [Button.url(where_n, where_l)],
-        [Button.mention(who_n, await e.client.get_entity(x.id))],
-    ]
+    buttons = [[Button.url(where_n, where_l)]]
+    if isinstance(x, types.User):
+        buttons.append([Button.mention(who_n, await e.client.get_entity(x.id))])
+    elif getattr(x, "username"):
+        buttons.append([Button.url(who_n, f"t.me/{x.username}")])
+    else:
+        buttons.append([Button.inline(who_n, "do_nothing")])
     try:
         sent = await asst.send_message(NEEDTOLOG, e.message, buttons=buttons)
         tag_add(sent.id, e.chat_id, e.id)
@@ -159,3 +162,8 @@ async def leave_ch_at(event):
     name = (await client.get_entity(int(ch_id))).title
     await client.delete_dialog(int(ch_id))
     await event.edit(get_string("userlogs_5").format(name))
+
+
+@callback("do_nothing")
+async def _(event):
+    await event.answer()
