606db7f0b12f9dcc7db5a0e090f6085c473971d0
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-11 16:24:41 +0000

    pylint: auto fixes

--- plugins/devtools.py
+++ plugins/devtools.py
@@ -141,9 +141,8 @@ async def _(event):
             # Consider it as Code Error, and move on to be shown ahead.
             pass
     reply_to_id = event.reply_to_msg_id or event.id
-    if (
-        any(item in cmd for item in KEEP_SAFE().All)
-        and (not event.out or event.sender_id != ultroid_bot.uid)
+    if any(item in cmd for item in KEEP_SAFE().All) and (
+        not event.out or event.sender_id != ultroid_bot.uid
     ):
         if event.sender_id in _ignore_eval:
             return await xx.edit(
