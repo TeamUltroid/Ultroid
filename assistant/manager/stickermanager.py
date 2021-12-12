2d4ffaad4377065d6534929257a1ad3bfadc69c7
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-12 13:46:00 +0000

    pylint: auto fixes

--- assistant/manager/stickermanager.py
+++ assistant/manager/stickermanager.py
@@ -1,11 +1,12 @@
 from io import BytesIO
-from pyUltroid.functions.tools import resize_photo
+
 from pyUltroid.functions.misc import create_quotly
-from . import asst_cmd, udB
-from telethon.utils import get_input_document, get_display_name
+from pyUltroid.functions.tools import resize_photo
 from telethon.tl.functions.messages import UploadMediaRequest
-from telethon.tl.types import InputStickerSetItem as SetItem
-from telethon.tl.functions.stickers import CreateStickerSetRequest
+from telethon.utils import get_display_name
+
+from . import asst_cmd, udB
+
 
 @asst_cmd(
     pattern="kang",
@@ -15,10 +16,9 @@ async def kang_cmd(ult):
         return await ult.eor("`Reply to a sticker/photo..`", time=5)
     reply = await ult.get_reply_message()
     animated, dl = None, None
-    emoji = "🏵"
     if reply.sticker:
-        file = reply.sticker
-        emoji = reply.file.emoji
+        reply.sticker
+        reply.file.emoji
         if reply.file.name.endswith(".tgs"):
             animated = True
             dl = await reply.download_media()
@@ -35,16 +35,16 @@ async def kang_cmd(ult):
         return await ult.eor("`Reply to sticker or text to add it in your pack...`")
     if dl:
         upl = await ult.client.upload_file(dl)
-        file = await ult.client(UploadMediaRequest(types.InputPeerSelf(), upl))
+        await ult.client(UploadMediaRequest(types.InputPeerSelf(), upl))
     get_ = udB.get_key("STICKERS") or {}
     sender = await ult.get_sender()
     if not get_.get(ult.sender_id):
         sn = f"ult_{ult.sender_id}"
         title = f"{get_display_name(sender)}'s Kang Pack"
         if animated:
-          sn += "_anim"
-          title += " (Animated)"
+            sn += "_anim"
+            title += " (Animated)"
         sn += f"_by_{asst.me.username}"
-        
-      # ait ult.client(CreateStickerSetRequest(user_id=sender.id, title=title, short_name=sn, stickers=[SetItem(file)], animated=animated))
-        
+
+    # ait ult.client(CreateStickerSetRequest(user_id=sender.id, title=title,
+    # short_name=sn, stickers=[SetItem(file)], animated=animated))
