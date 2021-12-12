510d0d2380c8c3fc892a800a6294c726dc4d9e2f
Devesh Pal <newdev0@outlook.com>
2021-12-12 19:20:43 +0530

    Create stickermanager.py

--- assistant/manager/stickermanager.py
+++ assistant/manager/stickermanager.py
@@ -1,12 +1,11 @@
 from io import BytesIO
-
-from pyUltroid.functions.misc import create_quotly
 from pyUltroid.functions.tools import resize_photo
-from telethon.tl.functions.messages import UploadMediaRequest
-from telethon.utils import get_display_name
-
+from pyUltroid.functions.misc import create_quotly
 from . import asst_cmd, udB
-
+from telethon.utils import get_input_document, get_display_name
+from telethon.tl.functions.messages import UploadMediaRequest
+from telethon.tl.types import InputStickerSetItem as SetItem
+from telethon.tl.functions.stickers import CreateStickerSetRequest
 
 @asst_cmd(
     pattern="kang",
@@ -16,9 +15,10 @@ async def kang_cmd(ult):
         return await ult.eor("`Reply to a sticker/photo..`", time=5)
     reply = await ult.get_reply_message()
     animated, dl = None, None
+    emoji = "🏵"
     if reply.sticker:
-        reply.sticker
-        reply.file.emoji
+        file = reply.sticker
+        emoji = reply.file.emoji
         if reply.file.name.endswith(".tgs"):
             animated = True
             dl = await reply.download_media()
@@ -35,16 +35,24 @@ async def kang_cmd(ult):
         return await ult.eor("`Reply to sticker or text to add it in your pack...`")
     if dl:
         upl = await ult.client.upload_file(dl)
-        await ult.client(UploadMediaRequest(types.InputPeerSelf(), upl))
+        file = await ult.client(UploadMediaRequest(types.InputPeerSelf(), upl))
     get_ = udB.get_key("STICKERS") or {}
     sender = await ult.get_sender()
     if not get_.get(ult.sender_id):
         sn = f"ult_{ult.sender_id}"
         title = f"{get_display_name(sender)}'s Kang Pack"
+        type_ = "static"
         if animated:
-            sn += "_anim"
-            title += " (Animated)"
+          type_ = "anim"
+          sn += "_anim"
+          title += " (Animated)"
         sn += f"_by_{asst.me.username}"
-
-    # ait ult.client(CreateStickerSetRequest(user_id=sender.id, title=title,
-    # short_name=sn, stickers=[SetItem(file)], animated=animated))
+        try:
+            pack = await ult.client(CreateStickerSetRequest(user_id=sender.id, title=title, short_name=sn, stickers=[SetItem(file, emoji=emoji)], animated=animated))
+        except Exception as er:
+            return await ult.eor(str(er))
+        sn = pack.set.short_name
+        get_.update({ult.sender_id:{type_:[]}})
+        udB.set_key("STICKERS", get_)
+        return await ult.reply(f"**Kanged Successfully!\nEmoji :** {emoji}\n**Link :** [Click Here](https://t.me/addstickers/{sn})")
+    
