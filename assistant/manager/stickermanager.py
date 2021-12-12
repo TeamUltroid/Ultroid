a4ca9fd10bfeba67788318697094d76ed7aecd1c
Devesh Pal <newdev0@outlook.com>
2021-12-12 21:40:50 +0530

    Update stickermanager.py

--- assistant/manager/stickermanager.py
+++ assistant/manager/stickermanager.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2021 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
 from PIL import Image
 from pyUltroid.functions.misc import create_quotly
 from telethon import errors
