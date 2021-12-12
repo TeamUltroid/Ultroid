47aa2316ef86ba30286c59b3bf6310b9482f8db2
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-12 13:54:03 +0000

    pylint: auto fixes

--- assistant/manager/stickermanager.py
+++ assistant/manager/stickermanager.py
@@ -7,7 +7,7 @@ from telethon.tl.functions.stickers import CreateStickerSetRequest
 from telethon.tl.types import InputStickerSetItem as SetItem
 from telethon.utils import get_display_name
 
-from . import asst_cmd, udB, asst
+from . import asst, asst_cmd, udB
 
 
 @asst_cmd(
