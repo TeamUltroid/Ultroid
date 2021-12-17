c75cc82a1b0706f18f3c719b227973ab5d57a995
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-17 19:38:32 +0530

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -116,7 +116,7 @@ async def _(e):
         ],
         [
             Button.inline("bayfiles", data=f"flbayfiles//{__cache}"),
-            Button.inline("x0", data=f"flx0//{__cache}"),
+            Button.inline("x0.at", data=f"flx0.at//{__cache}"),
         ],
         [
             Button.inline("file.io", data=f"flfile.io//{__cache}"),
@@ -153,7 +153,7 @@ async def _(e):
         filename = filename.split("/")[-1]
     await e.edit(f"Uploading `{filename}` on {host}")
     link = await webuploader(chat_id, msg_id, host)
-    await e.edit(f"Uploaded [{filename}]({link}) on {host}.")
+    await e.edit(f"Uploaded [{filename}]({link.strip().replace("\n","")}) on {host}.")
 
 
 @in_pattern("repo", owner=True)
