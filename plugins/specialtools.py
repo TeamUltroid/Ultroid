6fabdb1c2e13ac5313b7a004ff794f4dacd3189b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-25 03:05:45 +0000

    pylint: auto fixes

--- plugins/specialtools.py
+++ plugins/specialtools.py
@@ -325,7 +325,7 @@ async def quott_(event):
                     )
                 else:
                     reply_ = []
-                    for msg in range(reply_.id, reply_.id+int(spli_[0])+1):
+                    for msg in range(reply_.id, reply_.id + int(spli_[0]) + 1):
                         msh = await event.client.get_messages(event.chat_id, ids=msg)
                         if msh:
                             reply_.append(msh)
