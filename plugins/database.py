d3ec21e140c5f9b89a620b9eaeb75e7dd0770ea0
Devesh Pal <newdev0@outlook.com>
2021-12-27 09:46:10 +0530

    Update database.py

--- plugins/database.py
+++ plugins/database.py
@@ -29,16 +29,18 @@ from . import Redis, eor, get_string, udB, ultroid_cmd
 
 @ultroid_cmd(pattern="setdb ?(.*)", fullsudo=True)
 async def _(ult):
+    match = ult.pattern_match.group(1)
+    if not match:
+        return await ult.eor("Provide key and value to set!")
     try:
-        delim = " " if re.search("[|]", ult.pattern_match.group(1)) is None else " | "
-        data = ult.pattern_match.group(1).split(delim, maxsplit=1)
+        delim = " " if re.search("[|]", match) is None else " | "
+        data = match.split(delim, maxsplit=1)
         if data[0] in ["--extend", "-e"]:
             data = data[1].split(maxsplit=1)
             data[1] = str(udB.get_key(data[0])) + " " + data[1]
         udB.set_key(data[0], data[1])
         redisdata = Redis(data[0])
-        await eor(
-            ult,
+        await ult.eor(
             "**DB Key Value Pair Updated\nKey :** `{}`\n**Value :** `{}`".format(
                 data[0],
                 redisdata,
@@ -50,11 +52,10 @@ async def _(ult):
 
 @ultroid_cmd(pattern="deldb ?(.*)", fullsudo=True)
 async def _(ult):
-    try:
-        key = ult.pattern_match.group(1)
-        _ = key.split(maxsplit=1)
-    except IndexError:
+    key = ult.pattern_match.group(1)
+    if not key:
         return await ult.eor("Give me a key name to delete!", time=5)
+    _ = key.split(maxsplit=1)
     try:
         if _[0] == "-m":
             for key in _[1].split():
@@ -71,8 +72,11 @@ async def _(ult):
 
 @ultroid_cmd(pattern="rendb ?(.*)", fullsudo=True)
 async def _(ult):
-    delim = " " if re.search("[|]", ult.pattern_match.group(1)) is None else " | "
-    data = ult.pattern_match.group(1).split(delim)
+    match = ult.pattern_match.group(1)
+    if not match:
+        return await ult.eor("`Provide Keys name to rename..`")
+    delim = " " if re.search("[|]", match) is None else " | "
+    data = match.split(delim)
     if Redis(data[0]):
         try:
             udB.rename(data[0], data[1])
