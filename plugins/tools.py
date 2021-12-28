40fa01081555ba4c107bd62b40dc43557cdc730b
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-12-28 14:42:39 +0000

    pylint: auto fixes

--- plugins/tools.py
+++ plugins/tools.py
@@ -53,7 +53,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import pack_bot_file_id
 
-from . import HNDLR, LOGS, async_searcher, bash, eor, get_string
+from . import HNDLR, async_searcher, bash, eor, get_string
 from . import humanbytes as hb
 from . import inline_mention, is_url_ok, mediainfo, ultroid_cmd
 
