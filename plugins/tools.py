4e72fbaaee474e1a8f70c997c747a2ea9441c370
Devesh Pal <newdev0@outlook.com>
2021-12-28 20:11:44 +0530

    Update tools.py

--- plugins/tools.py
+++ plugins/tools.py
@@ -438,10 +438,9 @@ async def magic(event):
         post=True,
         re_json=True,
     )
-    LOGS.info(data)
-    response = data.get("payload", {}).get("response", {})
+    response = data.get("response", {})
     if not response.get("status"):
         return await event.eor("**ERRROR :** `{}`".format(response["message"]))
     await event.eor(
-        f"• **Ultroid Tiny**\n• Given Url : {url}\n• Shorten Url : https://tiny.ultroid.tech/{data['payload']['id']}"
+        f"• **Ultroid Tiny**\n• Given Url : {url}\n• Shorten Url : {data['response']['tinyUrl']}"
     )
