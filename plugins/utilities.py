2918692c8817eadfb9253ea1e68fcbdf4b9eb3a3
Kaif <88398455+kaif-00z@users.noreply.github.com>
2022-01-05 11:27:08 +0530

    gist support

--- plugins/utilities.py
+++ plugins/utilities.py
@@ -18,6 +18,8 @@
 
 • `{i}paste` - `Include long text / Reply to text file.`
 
+• `{i}gistpaste` - `Include long text / Reply to text.`
+
 • `{i}info <username/userid/chatid>`
     Reply to someone's msg.
 
@@ -49,6 +51,7 @@
    Use - Paste the contents of file and send as pic.
 """
 import calendar
+from github3 import create_gist
 import html
 import io
 import os
@@ -260,6 +263,40 @@ async def _(event):
     except BaseException as e:
         LOGS.exception(e)
         await xx.edit(reply_text)
+
+@ultroid_cmd(pattern="gistpaste( (.*)|$)", manager=True, allow_all=True)
+async def _(event):
+    # creating anonymous gist , i will add document support soon
+    try:
+        input_str = event.text.split(maxsplit=1)[1]
+    except IndexError:
+        input_str = None
+    xx = await event.eor("` 《 Pasting on gist... 》 `")
+    if input_str:
+        message = input_str
+    elif event.reply_to:
+        reply = await event.get_reply_message()
+        if reply.text:
+            message = reply
+    else:
+        message = None
+    if not message:
+        return await xx.eor(
+            "`Reply to a Message or Give me Some Text !`", time=5
+        )
+    try:
+        file = {
+            'ultroid.txt' : {
+                'content': message
+            }
+        }
+        gist = create_gist("ultroid", file)
+        link = gist.html_url
+        reply_text = f"• **Pasted to Gist :** [gist]({link})"
+        await xx.eor(reply_text)
+    except Exception as ultroid:
+        LOGS.info(str(ultroid))
+
 
 
 @ultroid_cmd(
