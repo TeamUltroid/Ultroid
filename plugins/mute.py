8df879859e0c2a6daaf2aae6e883466f00252f13
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-01-18 20:43:37 +0530

    Nasha OP

--- plugins/mute.py
+++ plugins/mute.py
@@ -59,15 +59,15 @@ async def startmute(event):
         userid = event.chat_id
     else:
         return await xx.eor("`Reply to a user or add their userid.`", time=5)
-    chat = await event.get_chat()
+    chat_id = await event.get_chat()
     if "admin_rights" in vars(chat) and vars(chat)["admin_rights"] is not None:
         if chat.admin_rights.delete_messages is not True:
             return await xx.eor("`No proper admin rights...`", time=5)
     elif "creator" not in vars(chat) and not event.is_private:
         return await xx.eor("`No proper admin rights...`", time=5)
-    if is_muted(chat.id, userid):
+    if is_muted(event.chat_id, userid):
         return await xx.eor("`This user is already muted in this chat.`", time=5)
-    mute(chat.id, userid)
+    mute(event.chat_id, userid)
     await xx.eor("`Successfully muted...`", time=3)
 
 
