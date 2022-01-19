21d6c07efa1d464e0356c93108d5eb5ad27c94b4
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-01-19 22:38:11 +0530

    Create dnd.py

---
+++ plugins/dnd.py
@@ -0,0 +1,31 @@
+#S
+#O
+#M
+#E
+
+#H
+#E
+#A
+#D
+#E
+#R
+#S
+
+from . import ultroid_cmd
+#from pyUltroid.functions.dnd_db import chat_in_dnd, add_dnd, rem_dnd, get_dnd_chats
+
+
+@ultroid_cmd(pattern="dnd$", manager=True, admins_only=True, groups_only=True)
+async def _(event):
+    if chat_in_dnd(event.chat_id):
+        return await event.eor("`Chat already in do not disturb mode.`")
+    add_dnd(event.chat_id)
+    await event.eor("`Do not disturb mode activated for this chat.`")
+
+@ultroid_cmd(pattern="deldnd$", manager=True, admins_only=True, groups_only=True)
+async def _(event):
+    if not chat_in_dnd(event.chat_id):
+        return await event.eor("`Chat is not in do not disturb mode.`")
+    del_dnd(event.chat_id)
+    await event.eor("`Do not disturb mode deactivated for this chat.`")
+
