0428b4622bd8ced2b1310d79b67e50f251f1e22e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-01-19 22:54:50 +0530

    Update dnd.py

--- plugins/dnd.py
+++ plugins/dnd.py
@@ -1,19 +1,26 @@
-# S
-# O
-# M
-# E
+# Ultroid - UserBot
+# Copyright (C) 2021-2022 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
-# H
-# E
-# A
-# D
-# E
-# R
-# S
 
-from . import ultroid_cmd
+from . import ultroid_cmd, ultroid_bot, asst
+from telethon.events import ChatAction
+from pyUltroid.functions.dnd_db import chat_in_dnd, add_dnd, rem_dnd, get_dnd_chats
 
-# from pyUltroid.functions.dnd_db import chat_in_dnd, add_dnd, rem_dnd, get_dnd_chats
+
+@ultroid_bot.on(ChatAction(chats=get_dnd_chats()))
+@asst.on(ChatAction(chats=get_dnd_chats()))
+async def _(event):
+    if event.user_joined:
+        for user in event.users:
+            try:
+                await event.client.kick_participant(event.chat_id, user)
+            except Exception as ex:
+                LOGS.error("Error in DND:\n" + str(ex))
+        await event.delete()
 
 
 @ultroid_cmd(pattern="dnd$", manager=True, admins_only=True, groups_only=True)
@@ -28,5 +35,5 @@ async def _(event):
 async def _(event):
     if not chat_in_dnd(event.chat_id):
         return await event.eor("`Chat is not in do not disturb mode.`")
-    del_dnd(event.chat_id)
+    rem_dnd(event.chat_id)
     await event.eor("`Do not disturb mode deactivated for this chat.`")
