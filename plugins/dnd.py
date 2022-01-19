78e390ebbf091b68e5d0b192e9610bac62bd79ba
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-01-19 23:26:03 +0530

    Create dnd.py

--- plugins/dnd.py
+++ plugins/dnd.py
@@ -5,6 +5,18 @@
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
 
+"""
+✘ Commands Available
+
+Do Not Disturb - As it says, activating this in your group will kick new users who joins the group.
+
+• `{i}dnd`
+    To activate.
+
+• `{i}deldnd`
+    To deactivate.
+"""
+
 
 from pyUltroid.dB.dnd_db import add_dnd, chat_in_dnd, del_dnd, get_dnd_chats
 from telethon.events import ChatAction
@@ -27,14 +39,14 @@ async def _(event):
 @ultroid_cmd(pattern="dnd$", manager=True, admins_only=True, groups_only=True)
 async def _(event):
     if chat_in_dnd(event.chat_id):
-        return await event.eor("`Chat already in do not disturb mode.`")
+        return await event.eor("`Chat already in do not disturb mode.`", time=3)
     add_dnd(event.chat_id)
-    await event.eor("`Do not disturb mode activated for this chat.`")
+    await event.eor("`Do not disturb mode activated for this chat.`", time=3)
 
 
 @ultroid_cmd(pattern="deldnd$", manager=True, admins_only=True, groups_only=True)
 async def _(event):
     if not chat_in_dnd(event.chat_id):
-        return await event.eor("`Chat is not in do not disturb mode.`")
+        return await event.eor("`Chat is not in do not disturb mode.`", time=3)
     del_dnd(event.chat_id)
-    await event.eor("`Do not disturb mode deactivated for this chat.`")
+    await event.eor("`Do not disturb mode deactivated for this chat.`", time=3)
