00cde36a083b12bd7866f6f635aeade2199dd733
Devesh Pal <newdev0@outlook.com>
2022-01-20 09:48:30 +0530

    Create dnd.py

--- plugins/dnd.py
+++ plugins/dnd.py
@@ -21,20 +21,17 @@ Do Not Disturb - As it says, activating this in your group will kick new users w
 from pyUltroid.dB.dnd_db import add_dnd, chat_in_dnd, del_dnd, get_dnd_chats
 from telethon.events import ChatAction
 
-from . import LOGS, asst, ultroid_bot, ultroid_cmd
+from . import LOGS, udB, asst, ultroid_bot, ultroid_cmd
 
-
-@ultroid_bot.on(ChatAction)
-@asst.on(ChatAction)
-async def _(event):
-    if int(event.chat_id) in get_dnd_chats():
-        if event.user_joined:
-            for user in event.users:
-                try:
-                    await event.client.kick_participant(event.chat_id, user)
-                except Exception as ex:
-                    LOGS.error("Error in DND:\n" + str(ex))
-            await event.delete()
+async def dnd_func(event):
+    if event.chat_id in get_dnd_chats():
+        for user in event.users:
+            try:
+                await event.client.kick_participant(event.chat_id, user)
+            except Exception as ex:
+                LOGS.error("Error in DND:")
+                LOGS.exception(ex)
+        await event.delete()
 
 
 @ultroid_cmd(pattern="dnd$", manager=True, admins_only=True, groups_only=True)
@@ -42,6 +39,7 @@ async def _(event):
     if chat_in_dnd(event.chat_id):
         return await event.eor("`Chat already in do not disturb mode.`", time=3)
     add_dnd(event.chat_id)
+    event.client.add_handler(dnd_func, events.ChatAction(func=lambda x: x.user_joined))
     await event.eor("`Do not disturb mode activated for this chat.`", time=3)
 
 
@@ -51,3 +49,8 @@ async def _(event):
         return await event.eor("`Chat is not in do not disturb mode.`", time=3)
     del_dnd(event.chat_id)
     await event.eor("`Do not disturb mode deactivated for this chat.`", time=3)
+
+
+if udB.get_key("DND_CHATS"):
+    ultroid_bot.add_handler(dnd_func, events.ChatAction(func=lambda x: x.user_joined))
+    asst.add_handler(dnd_func, events.ChatAction(func=lambda x: x.user_joined))
