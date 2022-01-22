ae79a2152d2272ed812abaee54e8d70102bc8482
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-01-22 20:17:03 +0530

    Update fileshare.py

--- plugins/fileshare.py
+++ plugins/fileshare.py
@@ -17,16 +17,15 @@
 import os
 
 from pyUltroid.functions.tools import get_file_link
-
+from pyUltroid.dB.filestore_db import get_stored_msg, list_all_stored_msgs
 from . import asst, eor, get_string, udB, ultroid_cmd
 
 
-@ultroid_cmd(pattern="store")
+@ultroid_cmd(pattern="store$")
 async def filestoreplg(event):
     msg = await event.get_reply_message()
-    if msg is None:
-        await event.eor(get_string("fsh_3"), time=10)
-        return
+    if not msg:
+        return await event.eor(get_string("fsh_3"), time=10)
     # allow storing both messages and media.
     filehash = await get_file_link(msg)
     link_to_file = "https://t.me/{}?start={}".format(asst.me.username, filehash)
@@ -37,15 +36,31 @@ async def filestoreplg(event):
     )
 
 
+@ultroid_cmd("delstore ?(.*)")
+async def _(event):
+    match = event.pattern_match.group(1)
+    if not match:
+        return await event.eor("`Give stored film's link to delete.`", time=5)
+    match = match.split("?start=")
+    botusername = match[0].split("/")[-1]
+    if botusername != asst.me.username:
+        return await event.eor("`Message/Media of provided link was not stored by this bot.`", time=5)
+    msg_id = get_stored_msg(match[1])
+    if not msg_id:
+        return await event.eor("`Message/Media of provided link was already deleted.`", time=5)
+    del_stored(match[1])
+    msg = await asst.get_messages(udB.get_key("LOG_CHANNEL"), ids=int(msg_id))
+    await msg.delete()
+    await event.eor("__Deleted__")
+    
+
 @ultroid_cmd("liststored$")
 async def liststored(event):
-    get = udB.get_key("FILE_STORE") or {}
-    if not get:
-        await event.eor(get_string("fsh_4"), time=5)
-        return
+    files = list_all_stored_msgs()
+    if not files:
+        return await event.eor(get_string("fsh_4"), time=5)
     msg = "**Stored files:**\n"
-    for c, i in enumerate(list(get.keys())):
-        c += 1
+    for c, i in enumerate(files, start=1):
         msg += f"`{c}`. https://t.me/{asst.me.username}?start={i}\n"
     if len(msg) > 4095:
         with open("liststored.txt", "w") as f:
