f958cd892384cf58e515631ef220839e5f46f033
Aditya <xditya@ultroid.tech>
2022-01-27 08:09:10 +0530

    fix

--- plugins/autocorrect.py
+++ plugins/autocorrect.py
@@ -26,7 +26,7 @@ from telethon import events
 
 @ultroid_cmd(pattern="autocorrect", fullsudo=True)
 async def acc(e):
-    if Redis("AUTOCORRECT"):
+    if udB.get_key("AUTOCORRECT"):
         udB.set_key("AUTOCORRECT", "True")
         ultroid_bot.add_handler(
             gramme, events.NewMessage(outgoing=True, func=lambda x: x.text)
