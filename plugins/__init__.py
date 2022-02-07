d0f56c80b4a72dd6144935830e3211b60af3fa94
Devesh Pal <newdev0@outlook.com>
2022-02-07 17:20:22 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -27,7 +27,7 @@ from telethon.tl import functions, types
 from strings import get_string
 
 Redis = udB.get_key
-
+con = TgConverter
 OWNER_NAME = ultroid_bot.full_name
 OWNER_ID = ultroid_bot.uid
 
