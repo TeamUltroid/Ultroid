6cd6c08c992f4493f1413ea749de103765e0f782
Devesh Pal <newdev0@outlook.com>
2022-02-16 09:19:29 +0530

    Update termux.sh

--- resources/startup/termux.sh
+++ resources/startup/termux.sh
@@ -1,4 +1,4 @@
-echo "Updating System.."
+echo "Updating System..\n\n"
 pkg update -y
 apt update
 apt upgrade -y
@@ -8,9 +8,9 @@ python_not_installed="$(python -c 'exit()')"
 # Install Python if n0t installed..
 if [ python_not_installed ]
 then
-    echo "Installing Python..\nThis may take some long..."
+    echo "Installing Python..\nThis may take some long...\n"
     pkg install python3 -y
 fi
 
-echo "Running up Installation tool."
+echo "Running up Installation tool.\n"
 python resources/startup/_termux.py
