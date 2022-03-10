3186fcaab23c715bb6b3d1da7d1d029377b40b42
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-03-10 18:24:04 +0000

    pylint: auto fixes

--- plugins/autoban.py
+++ plugins/autoban.py
@@ -27,9 +27,10 @@
 
 
 from pyUltroid.dB import autoban_db, dnd_db
+from pyUltroid.functions.admins import get_update_linked_chat
 from telethon import events
 from telethon.tl.types import Channel
-from pyUltroid.functions.admins import get_update_linked_chat
+
 from . import LOGS, asst, get_string, inline_mention, ultroid_bot, ultroid_cmd
 
 
