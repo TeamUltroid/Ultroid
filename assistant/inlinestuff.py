b99136a670f823adcefcadad39e91cdd704efa8d
Devesh Pal <newdev0@outlook.com>
2022-03-14 22:13:16 +0530

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -9,7 +9,7 @@ import base64
 from datetime import datetime
 from random import choice
 from re import compile as re_compile
-
+import inspect
 from bs4 import BeautifulSoup as bs
 from pyUltroid.functions.misc import google_search
 from pyUltroid.functions.tools import (
@@ -22,7 +22,7 @@ from pyUltroid.functions.tools import (
 from telethon import Button
 from telethon.tl.types import DocumentAttributeAudio as Audio
 from telethon.tl.types import InputWebDocument as wb
-
+from telethon.tl.alltlobjects import LAYER, tlobjects
 from . import *
 from . import _ult_cache
 
@@ -737,3 +737,31 @@ async def omgubuntu(ult):
         switch_pm_param="start",
     )
     _OMG[match] = res
+
+
+@in_pattern("tl", owner=True)
+async def inline_tl(ult):
+    try:
+        match = ult.text.split(maxsplit=1)[1]
+    except IndexError:
+        text = f"**It is Telegram TlObjects Searcher.**\n__(Don't use if you know what it is!)__\n\n• Example Usage\n`@{asst.me.username} tl fns GetUserRequest`"
+        return await ult.answer([await ult.builder.article(title="How to Use?", description="Tl Searcher by Ultroid", url="https://t.me/TheUltroid", text=text)], switch_pm="Tl Search 🔍", switch_pm_param="start")
+    res = []
+    for key in tlobjects.values():
+        if match in key.__name__:
+            tyyp = "Function" if "tl.functions." in str(key) else "Type"
+            text = f"**{key.__name__}** (`{key.CONSTRUCTOR_ID}`)\n"
+            text += f"**Category:** `{tyyp}`\n"
+            text += "\n`from {key.__module__} import {key.__name__}`\n"
+            args = str(inspect.signature(key))[1:][:-1]
+            if args:
+                text += "Parameter:"
+                for para in args.split(","):
+                    text += " "*4 + para + "\n"
+            text += f"Layer: {LAYER}"
+            res.append(await ult.builder.article(title=key.__name__, description=tyyp, url="https://t.me/TheUltroid", text=text))
+    if not res:
+        mo = f"No Results for {match}!"
+    else:
+        mo = f"Showing {len(res)} results!"
+    await ult.answer(res, switch_pm=mo, switch_pm_param="start")
