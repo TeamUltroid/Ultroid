e908d5dbda74695e825c48b93e2beff54fa6ea79
Devesh Pal <newdev0@outlook.com>
2022-03-19 09:03:23 +0530

    Update inlinestuff.py

--- assistant/inlinestuff.py
+++ assistant/inlinestuff.py
@@ -728,6 +728,7 @@ async def omgubuntu(ult):
                 description=desc,
                 url=url,
                 text=text,
+                buttons=Button.switch_inline("Search Again", query=ult.text, same_peer=True),
                 include_media=True,
                 content=img,
                 thumb=img,
