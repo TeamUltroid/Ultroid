3776e0cfb4aa355cb5bfba2f7abe2d4aa24dd126
Devesh Pal <newdev0@outlook.com>
2022-07-18 20:17:48 +0530

    Use brush to clean your teeths

--- plugins/variables.py
+++ plugins/variables.py
@@ -74,7 +74,7 @@ async def get_var(event):
             await eor(x, "Such a var doesn't exist!", time=5)
 
     elif opt == "db":
-        val = udB.get_key(varname)
+        val = udB.get(varname)
         if val is not None:
             await x.edit(f"**Key** - `{varname}`\n**Value**: `{val}`")
         else:
