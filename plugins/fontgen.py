ba6479915d8828f152652cb81bc000dfefae132a
CyrusXD <79554993+Ashutosh1478@users.noreply.github.com>
2022-07-21 12:28:58 +0530

    Update fontgen.py

--- plugins/fontgen.py
+++ plugins/fontgen.py
@@ -4,13 +4,10 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-"""
-• `{i}font <font name> : <text>`
-    Generate different fonts for the text.
 
-• `{i}font`
-    To get list of fonts
-"""
+from . import get_help
+
+__doc__ = get_help("help_fontgen")
 
 import string
 
