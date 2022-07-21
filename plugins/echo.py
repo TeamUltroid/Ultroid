6639364eac80e380d7713467ca739bd738b34761
CyrusXD <79554993+Ashutosh1478@users.noreply.github.com>
2022-07-21 11:29:59 +0530

    Update echo.py

--- plugins/echo.py
+++ plugins/echo.py
@@ -4,18 +4,11 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-"""
-✘ Commands Available
 
-•`{i}addecho <reply to anyone>`
-   Start Auto Echo message of Replied user.
+from . import get_help
 
-•`{i}remecho <reply to anyone>`
-   Turn It off
+__doc__ = get_help("help_echo")
 
-•`{i}listecho <reply to anyone>`
-   To Get list.
-"""
 
 from pyUltroid.dB.echo_db import add_echo, check_echo, list_echo, rem_echo
 from telethon.utils import get_display_name
