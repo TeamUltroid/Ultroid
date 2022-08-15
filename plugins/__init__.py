084a5e182b0dd328df317d3b48d1d4ccf6295e4e
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2022-08-15 04:34:15 +0000

    pylint: auto fixes

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -12,6 +12,7 @@ from random import choice
 
 import requests
 from telethon import Button, events
+from telethon.tl import functions, types  # pylint:ignore
 
 from pyUltroid import *
 from pyUltroid._misc._assistant import asst_cmd, callback, in_pattern
@@ -23,10 +24,7 @@ from pyUltroid.functions.info import *
 from pyUltroid.functions.misc import *
 from pyUltroid.functions.tools import *
 from pyUltroid.version import __version__, ultroid_version
-
 from strings import get_help, get_string
-
-from telethon.tl import functions, types # pylint:ignore
 
 Redis = udB.get_key
 con = TgConverter
