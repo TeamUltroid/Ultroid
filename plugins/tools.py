56f475498fb1d5aca84fc2af47ea19d0166ec96e
Devesh Pal <newdev0@outlook.com>
2022-11-21 13:00:05 +0530

    Update tools.py

--- plugins/tools.py
+++ plugins/tools.py
@@ -38,6 +38,7 @@
 """
 import glob
 import io
+import secrets
 import os
 from asyncio.exceptions import TimeoutError as AsyncTimeout
 
@@ -421,9 +422,9 @@ async def magic(event):
     except IndexError:
         return await event.eor("`Provide url to turn into tiny...`")
     match, id_ = match.split(), None
-    data = {}
-    if len(match) > 1:
-        data["id"] = match[1]
+    data = {
+        "id": match[1] if len(match) > 1 else secrets.token_urlsafe(6)
+    }
     url = match[0]
     data["link"] = url
     data = await async_searcher(
