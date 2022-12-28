5e6fb84a39d1401ff85183bc5ed5568e706bd8ca
Devesh Pal <newdev0@outlook.com>
2022-12-28 23:07:48 +0530

    reduce db files

--- plugins/autoban.py
+++ plugins/autoban.py
@@ -10,14 +10,14 @@ from . import get_help
 __doc__ = get_help("help_autoban")
 
 from telethon import events
-
-from pyUltroid.dB import dnd_db
+from pyUltroid.dB.base import KeyManager
 
 from . import LOGS, asst, ultroid_bot, ultroid_cmd
 
+Keym = KeyManager("DND_CHATS", cast=list)
 
 async def dnd_func(event):
-    if event.chat_id in dnd_db.get_dnd_chats():
+    if event.chat_id in Keym.get():
         for user in event.users:
             try:
                 await (
@@ -39,20 +39,20 @@ async def dnd_func(event):
 async def _(event):
     match = event.pattern_match.group(1)
     if match == "on":
-        if dnd_db.chat_in_dnd(event.chat_id):
+        if Keym.contains(event.chat_id):
             return await event.eor("`Chat already in do not disturb mode.`", time=3)
-        dnd_db.add_dnd(event.chat_id)
+        Keym.add(event.chat_id)
         event.client.add_handler(
             dnd_func, events.ChatAction(func=lambda x: x.user_joined)
         )
         await event.eor("`Do not disturb mode activated for this chat.`", time=3)
     elif match == "off":
-        if not dnd_db.chat_in_dnd(event.chat_id):
+        if not Keym.contains(event.chat_id):
             return await event.eor("`Chat is not in do not disturb mode.`", time=3)
-        dnd_db.del_dnd(event.chat_id)
+        Keym.remove(event.chat_id)
         await event.eor("`Do not disturb mode deactivated for this chat.`", time=3)
 
 
-if dnd_db.get_dnd_chats():
+if Keym.get():
     ultroid_bot.add_handler(dnd_func, events.ChatAction(func=lambda x: x.user_joined))
     asst.add_handler(dnd_func, events.ChatAction(func=lambda x: x.user_joined))
