9da0b9229aa30a8f321426596b69ff9d44362ae3
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2023-02-03 18:01:03 +0000

    pylint: auto fixes

--- bot/gdrive.py
+++ bot/gdrive.py
@@ -453,12 +453,12 @@ class GDrive:
             while filesize != uploaded:
                 chunk_data = await f.read(chunksize)
                 headers = {"Content-Length": str(len(chunk_data)),
-                           "Content-Range": "bytes " + str(uploaded) + "-" + str(uploaded+len(chunk_data)-1) +"/" + str(filesize)}
+                           "Content-Range": "bytes " + str(uploaded) + "-" + str(uploaded + len(chunk_data) - 1) + "/" + str(filesize)}
                 uploaded += len(chunk_data)
                 await event.reply(str(headers) + str(uploaded))
                 resp = await self._session.put(upload_url, data=chunk_data, headers=headers)
 
-                await event.reply((await resp.text()+str(headers)) or "No text")
+                await event.reply((await resp.text() + str(headers)) or "No text")
                 diff = time.time() - start
                 percentage = round((uploaded / filesize) * 100, 2)
                 speed = round(uploaded / diff, 2)
