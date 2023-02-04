f6bf3e00c771b5ea448fcd7326cc23322dd281f0
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2023-02-04 17:14:15 +0000

    pylint: auto fixes

--- plugins/drive.py
+++ plugins/drive.py
@@ -1,6 +1,5 @@
-from somewhere import GDrive
 from core.decorators._decorators import ultroid_cmd
-
+from somewhere import GDrive
 
 drive = GDrive()
 
@@ -10,15 +9,18 @@ async def drive_upload_func(event):
     """`{}gdul <path to file/reply to document>` - Upload file from local/telegram to Google Drive."""
     ...
 
+
 @ultroid_cmd("gddl( (.*)|$)", fullsudo=True)
 async def drive_download_func(event):
     """`{}gddl <link>` - Download file from Google Drive using link."""
     ...
 
+
 @ultroid_cmd("gdusage$")
 async def drive_usage_func(event):
     """`{}gdusage` - Show total limit and usage of Google Drive storage."""
     ...
+
 
 @ultroid_cmd("gdauth( (.*)|$)", fullsudo=True)
 async def drive_auth_func(event):
@@ -27,12 +29,14 @@ Args:
     `code` - Code which you get after visiting the link provided by `{}gdauth`"""
     ...
 
+
 @ultroid_cmd("gdls( (.*)|$)", fullsudo=True)
 async def drive_list_func(event):
     """`{}gdls <folder_id>` - List all files of Google Drive.
 Args:
     `folder_id` - Folder ID or Folder link of Google Drive to list files from."""
     ...
+
 
 @ultroid_cmd("gdmv( (.*)|$)", fullsudo=True)
 async def drive_move_func(event):
@@ -42,6 +46,7 @@ Args:
     `folder_id` - Folder ID or Folder Link where you want to move."""
     ...
 
+
 @ultroid_cmd("gdcp( (.*)|$)", fullsudo=True)
 async def drive_copy_func(event):
     """`{}gdcp <file_id> <folder_id>` - Copy file to another directory in Google Drive.
@@ -50,6 +55,7 @@ Args:
     `folder_id` - Folder ID or Folder Link where you want to copy."""
     ...
 
+
 @ultroid_cmd("gdsearch( (.*)|$)", fullsudo=True)
 async def drive_search_func(event):
     """`{}gdmv <filename/file_id>` - Search for specific file in Google Drive.
@@ -57,6 +63,7 @@ Args:
     `filename` - Name of file (case-sensitive).
     `file_id` - File ID or File Link which you want to search."""
     ...
+
 
 @ultroid_cmd("gddel( (.*)|$)", fullsudo=True)
 async def drive_delete_func(event):
