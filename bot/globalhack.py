010c67776e73343659d6e0f78f0ed305bd0248f2
Amit Sharma <amitsharma123234@gmail.com>
2023-02-05 11:33:04 +0530

    Update globalhack.py

--- bot/globalhack.py
+++ bot/globalhack.py
@@ -7,7 +7,7 @@ from telethon.tl.types import MessageEntityUrl
 @ultroid_bot.on(
     NewMessage(
         outgoing=True,
-        func=lambda e: e.message and not event.text.startswith(HNDLR) and (
+        func=lambda e: e.message and not e.text.startswith(HNDLR) and (
             True in [isinstance(_, MessageEntityUrl) for _ in e.message.entities]),
     ),
 )
