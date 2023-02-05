030124cbd0bb856620d04b1cd507482b3088d9f9
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2023-02-05 05:58:18 +0000

    pylint: auto fixes

--- bot/globalhack.py
+++ bot/globalhack.py
@@ -7,7 +7,8 @@ from telethon.tl.types import MessageEntityUrl
 @ultroid_bot.on(
     NewMessage(
         outgoing=True,
-        func=lambda e: e.message and not event.text.startswith(HNDLR) and (True in [isinstance(_, MessageEntityUrl) for _ in e.message.entities]),
+        func=lambda e: e.message and not event.text.startswith(HNDLR) and (
+            True in [isinstance(_, MessageEntityUrl) for _ in e.message.entities]),
     ),
 )
 async def link_preview_hek(event):
