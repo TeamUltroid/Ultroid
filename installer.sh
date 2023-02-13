d0a937e8e1d1ca9b73ba70bced9526283ed142f6
Devesh Pal <newdev0@outlook.com>
2023-02-13 11:21:20 +0530

    update pytgcalls

--- installer.sh
+++ installer.sh
@@ -167,7 +167,7 @@ misc_install() {
             echo -e "Cloning VCBOT.."
             git clone https://github.com/TeamUltroid/VcBot $DIR/vcbot
         fi
-        pip3 install pytgcalls==3.0.0.dev22 && pip3 install av -q --no-binary av
+        pip3 install pytgcalls==3.0.0.dev23 && pip3 install av -q --no-binary av
     fi
 }
 
