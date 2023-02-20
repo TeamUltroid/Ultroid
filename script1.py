3edf67f9027d877d820859df9baa553eb6c7cb5a
Devesh Pal <newdev0@outlook.com>
2023-02-20 10:02:08 +0530

    test

--- script1.py
+++
@@ -1,9 +0,0 @@
-import os
-
-def push(path=''):
-    os.system(f"{path}git add .")
-    os.system(f"{path}git commit -m 'Update'")
-    os.system(f'{path}git push')
-
-push()
-push('cd D://PluginManagerAPI && ')
