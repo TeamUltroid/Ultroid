a528e8e0edfefa658972fcd4b197093912dad272
RaingerXD <126769455+RaingerXD@users.noreply.github.com>
2023-03-14 14:27:22 +0700

    Update session.sh

--- resources/session/session.sh
+++ resources/session/session.sh
@@ -26,7 +26,7 @@ apt-get update
 apt-get upgrade -y
 pkg upgrade -y
 pkg install python wget -y
-wget https://raw.githubusercontent.com/TeamUltroid/ultroid/main/resources/session/ssgen.py
+wget https://raw.githubusercontent.com/RaingerXD/Ultroid/main/resources/session/ssgen.py
 pip uninstall telethon -y && install telethon
 clear
 python3 ssgen.py
