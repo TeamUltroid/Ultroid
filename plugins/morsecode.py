eadaa49e5b55ab0f10e11857fbc29bab3a4c3241
Devesh Pal <newdev0@outlook.com>
2023-03-31 22:54:38 +0530

    minor fixes

--- plugins/morsecode.py
+++ plugins/morsecode.py
@@ -18,18 +18,18 @@ from . import async_searcher, get_string, ultroid_cmd
 
 
 @ultroid_cmd(pattern="mencode ?(.*)")
-async def mencode(event):
+async def mencode_cmd(event):
     msg = await event.eor(get_string("com_1"))
     text = event.pattern_match.group(1)
     if not text:
         return msg.edit("Please give a text!")
-    base_url = "https://apis.xditya.me/morse/encode?text=" + text
+    base_url = f"https://apis.xditya.me/morse/encode?text={text}" 
     encoded = await async_searcher(base_url)
     await msg.edit("**Encoded.**\n\n**Morse Code:** `{}`".format(encoded))
 
 
 @ultroid_cmd(pattern="mdecode ?(.*)")
-async def mencode(event):
+async def mdecode_cmd(event):
     msg = await event.eor(get_string("com_1"))
     text = event.pattern_match.group(1)
     if not text:
