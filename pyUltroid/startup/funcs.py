bba9fbe09544757521d5077d14880b9713f0ae0d
buddhhu <amitsharma123234@gmail.com>
2023-05-15 22:40:17 +0530

    Update

--- pyUltroid/startup/funcs.py
+++ pyUltroid/startup/funcs.py
@@ -42,7 +42,7 @@ from telethon.tl.types import (
 )
 from telethon.utils import get_peer_id
 from decouple import config, RepositoryEnv
-from.. import LOGS, ULTConfig
+from .. import LOGS, ULTConfig
 from ..fns.helper import download_file, inline_mention, updater
 
 db_url = 0
