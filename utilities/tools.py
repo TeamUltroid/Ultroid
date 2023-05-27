a84b336b52b39d8324913972b8a56758d27d2901
Devesh Pal <newdev0@outlook.com>
2023-05-27 09:38:01 +0530

    move

--- utilities/tools.py
+++ utilities/tools.py
@@ -561,30 +561,6 @@ class TgConverter:
         return out_path
 
     @staticmethod
-    def resize_photo_sticker(photo):
-        """Resize the given photo to 512x512 (for creating telegram sticker)."""
-        image = Image.open(photo)
-        if (image.width and image.height) < 512:
-            size1 = image.width
-            size2 = image.height
-            if image.width > image.height:
-                scale = 512 / size1
-                size1new = 512
-                size2new = size2 * scale
-            else:
-                scale = 512 / size2
-                size1new = size1 * scale
-                size2new = 512
-            size1new = math.floor(size1new)
-            size2new = math.floor(size2new)
-            sizenew = (size1new, size2new)
-            image = image.resize(sizenew)
-        else:
-            maxsize = (512, 512)
-            image.thumbnail(maxsize)
-        return image
-
-    @staticmethod
     async def ffmpeg_convert(input_, output, remove=False):
         if output.endswith(".webm"):
             return await TgConverter.create_webm(
@@ -722,23 +698,19 @@ def _get_value(stri):
 
 
 def safe_load(file, *args, **kwargs):
-    if isinstance(file, str):
-        read = file.split("\n")
-    else:
-        read = file.readlines()
+    read = file.split("\n") if isinstance(file, str) else file.readlines()
     out = {}
     for line in read:
         if ":" in line:  # Ignores Empty & Invalid lines
             spli = line.split(":", maxsplit=1)
             key = spli[0].strip()
             value = _get_value(spli[1])
-            out.update({key: value or []})
+            out[key] = value or []
         elif "-" in line:
             spli = line.split("-", maxsplit=1)
-            where = out[list(out.keys())[-1]]
-            if isinstance(where, list):
-                value = _get_value(spli[1])
-                if value:
+            if value := _get_value(spli[1]):
+                where = out[list(out.keys())[-1]]
+                if isinstance(where, list):
                     where.append(value)
     return out
 
