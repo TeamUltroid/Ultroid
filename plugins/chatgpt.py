34f16f3a2a9d46e0ef202d84f330684be0020a86
CyrusXD <79554993+Ashutosh1478@users.noreply.github.com>
2023-06-21 10:49:20 +0530

    Update chatgpt.py

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -1,30 +1,78 @@
+# credits - dot arc
+
+"""
+**Get Answers from Chat GPT (Open AI)**
+
+> `{i}gpt` (-i = for image) (query)
+
+**• Examples: **
+> `{i}gpt How to fetch a url in javascript`
+> `{i}gpt -i Cute Panda eating bamboo` 
+
+☠️ It needs OpenAI api key to function! ☠️
+"""
+
+import asyncio
+import os
 import openai
 
-from .. import udB, ultroid_cmd
+from .. import ultroid_cmd, check_filename, udB, LOGS, fast_download
 
 
-@ultroid_cmd("chatgpt( (.?)|$)")
-async def chatgpt(event):
-    """Chat with GPT-3"""
-    if not udB.get_key("OPENAI_KEY"):
-        return await event.eor("Set `OPENAI_KEY` in Database keys.")
-    openai.api_key = udB.get_KEY("OPENAI_KEY")
-
-    query = event.pattern_match.group(1).strip()
-    reply_message = await event.get_reply_message()
-    if not query and (reply_message and reply_message.text):
-        query = reply_message.text
-    if not query:
-        return await event.eor("Reply to a message to chat with GPT-3")
-    message = await event.eor("Chatting with GPT-3...")
-    output = openai.Completion.create(
-        model=udB.get_key("GPT_MODEL") or "davinci",
-        prompt=query,
-        temperature=0.9,
-        max_tokens=150,
-        top_p=1,
-        frequency_penalty=0,
-        presence_penalty=0.6,
-        stop=["\n", " User:", " AI:"],
+def get_gpt_answer(gen_image, question, api_key):
+    openai.api_key = api_key
+    if gen_image:
+        x = openai.Image.create(
+            prompt=question,
+            n=1,
+            size="1024x1024",
+            user="arc",
+        )
+        return x["data"][0]["url"]
+    x = openai.Completion.create(
+        model=udB.get_key("GPT_MODEL") or "text-davinci-003",
+        prompt=question,
+        temperature=0.5,
+        stop=None,
+        n=1,
+        user="arc",
+        max_tokens=768,
     )
-    await message.edit(f"__{query}__: \n__{output['choices'][0]['text']}__")
+    LOGS.debug(f'Token Used on ({question}) > {x["usage"]["total_tokens"]}')
+    return x["choices"][0].text.strip()
+
+
+@ultroid_cmd(pattern="gpt ?(.*)")
+async def openai_chat_gpt(e):
+    api_key = udB.get_key("OPENAI_API")
+    gen_image = False
+    if not api_key:
+        return await e.eor("`OPENAI_API` key missing..")
+
+    args = e.pattern_match.group(1)
+    reply = await e.get_reply_message()
+    if not args:
+        if reply and reply.text:
+            args = reply.message
+    if not args:
+        return await e.eor("`Gimme a Question to ask from ChatGPT`")
+
+    if args.startswith("-i"):
+        gen_image = True
+        args = args[2:].strip()
+        edit_text = "image"
+    else:
+        edit_text = "answer"
+
+    m = await e.eor(f"`getting {edit_text} from chatgpt..`")
+    response = await asyncio.to_thread(get_gpt_answer, gen_image, args, api_key)
+    if response:
+        if not gen_image:
+            await m.edit(
+                f"**Query :** \n~ __{args}__ \n\n**ChatGPT :** \n~ __{response}__"
+            )
+            return
+        path, _ = await fast_download(response, filename=check_filename("dall-e.png"))
+        await e.eor(f"<b>{args[:1023]}</b>", file=path, parse_mode="html")
+        os.remove(path)
+        await m.delete()
