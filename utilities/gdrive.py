e1e65f4a39286a3e3c8eeabe5ccc36485e4117ce
buddhhu <amitsharma123234@gmail.com>
2023-09-19 21:46:16 +0530

    Search files/folder in drive

--- utilities/gdrive.py
+++ utilities/gdrive.py
@@ -39,7 +39,9 @@ class GDrive:
         self.client_id: str = udB.get_key("GDRIVE_CLIENT_ID")
         self.client_secret: str = udB.get_key("GDRIVE_CLIENT_SECRET")
         self.folder_id: str = udB.get_key("GDRIVE_FOLDER_ID") or "root"
-        self.redirect_uri: str = udB.get_key("GDRIVE_REDIRECT_URI") or "http://localhost"
+        self.redirect_uri: str = (
+            udB.get_key("GDRIVE_REDIRECT_URI") or "http://localhost"
+        )
         self.scope: str = "https://www.googleapis.com/auth/drive"
         self.creds: dict = udB.get_key("GDRIVE_AUTH_TOKEN") or {}
         self.service_account: dict = udB.get_key("GDRIVE_SERVICE_KEY")
@@ -54,36 +56,53 @@ class GDrive:
         return f"https://drive.google.com/folderview?id={folder_id}"
 
     def get_oauth2_url(self) -> str:
-        return "https://accounts.google.com/o/oauth2/v2/auth?" + urlencode({
-            "client_id": self.client_id,
-            "redirect_uri": self.redirect_uri,
-            "response_type": "code",
-            "scope": self.scope,
-            "access_type": "offline",
-        })
+        return "https://accounts.google.com/o/oauth2/v2/auth?" + urlencode(
+            {
+                "client_id": self.client_id,
+                "redirect_uri": self.redirect_uri,
+                "response_type": "code",
+                "scope": self.scope,
+                "access_type": "offline",
+            }
+        )
 
     async def get_access_token(self, code: str = None) -> dict:
         if self.service_account:
-            header = {
-                "alg": "RS256",
-                "typ": "JWT"
-            }
+            header = {"alg": "RS256", "typ": "JWT"}
             payload = {
                 "iss": self.service_account["client_email"],
                 "scope": self.scope,
                 "aud": "https://oauth2.googleapis.com/token",
                 "exp": int(time.time()) + 3590,
-                "iat": int(time.time())
+                "iat": int(time.time()),
             }
-            token = jwt.encode(payload, self.service_account.get("private_key"), algorithm="RS256", headers=header)
-            resp = await self._session.post("https://oauth2.googleapis.com/token", data={"grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer", "assertion": token})
+            token = jwt.encode(
+                payload,
+                self.service_account.get("private_key"),
+                algorithm="RS256",
+                headers=header,
+            )
+            resp = await self._session.post(
+                "https://oauth2.googleapis.com/token",
+                data={
+                    "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                    "assertion": token,
+                },
+            )
             self.creds = await resp.json()
             self.creds["expires_in"] = time.time() + 3590
             udB.set_key("GDRIVE_AUTH_TOKEN", self.creds)
             return self.creds
         resp = await self._session.post(
             "https://oauth2.googleapis.com/token",
-            data={"client_id": self.client_id, "client_secret": self.client_secret, "redirect_uri": self.redirect_uri, "grant_type": "authorization_code", "code": code}, headers={"Content-Type": "application/x-www-form-urlencoded"}
+            data={
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "redirect_uri": self.redirect_uri,
+                "grant_type": "authorization_code",
+                "code": code,
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
         )
         self.creds = await resp.json()
         self.creds["expires_in"] = time.time() + 3590
@@ -92,24 +111,41 @@ class GDrive:
 
     async def _refresh_access_token(self) -> None:
         if self.service_account:
-            header = {
-                "alg": "RS256",
-                "typ": "JWT"
-            }
+            header = {"alg": "RS256", "typ": "JWT"}
             payload = {
                 "iss": self.service_account["client_email"],
                 "scope": self.scope,
                 "aud": "https://oauth2.googleapis.com/token",
                 "exp": int(time.time()) + 3600,
-                "iat": int(time.time())
+                "iat": int(time.time()),
             }
-            token = jwt.encode(payload, self.service_account.get("private_key"), algorithm="RS256", headers=header)
-            resp = await self._session.post("https://oauth2.googleapis.com/token", data={"grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer", "assertion": token})
+            token = jwt.encode(
+                payload,
+                self.service_account.get("private_key"),
+                algorithm="RS256",
+                headers=header,
+            )
+            resp = await self._session.post(
+                "https://oauth2.googleapis.com/token",
+                data={
+                    "grant_type": "urn:ietf:params:oauth:grant-type:jwt-bearer",
+                    "assertion": token,
+                },
+            )
             self.creds = await resp.json()
             self.creds["expires_in"] = time.time() + 3590
             udB.set_key("GDRIVE_AUTH_TOKEN", self.creds)
             return
-        resp = await self._session.post("https://oauth2.googleapis.com/token", data={"client_id": self.client_id, "client_secret": self.client_secret, "grant_type": "refresh_token", "refresh_token": self.creds.get("refresh_token")}, headers={"Content-Type": "application/x-www-form-urlencoded"})
+        resp = await self._session.post(
+            "https://oauth2.googleapis.com/token",
+            data={
+                "client_id": self.client_id,
+                "client_secret": self.client_secret,
+                "grant_type": "refresh_token",
+                "refresh_token": self.creds.get("refresh_token"),
+            },
+            headers={"Content-Type": "application/x-www-form-urlencoded"},
+        )
         creds = await resp.json()
         if "error" in creds:
             return creds
@@ -118,45 +154,68 @@ class GDrive:
         udB.set_key("GDRIVE_AUTH_TOKEN", self.creds)
 
     async def get_size_status(self) -> dict:
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
-        return await (await self._session.get(
-            self.base_url + "/about",
-            headers={
-                "Authorization": "Bearer " + self.creds.get("access_token"),
-                "Content-Type": "application/json",
-            },
-            params={"fields": "storageQuota"},
-        )).json()
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
+        return await (
+            await self._session.get(
+                self.base_url + "/about",
+                headers={
+                    "Authorization": "Bearer " + self.creds.get("access_token"),
+                    "Content-Type": "application/json",
+                },
+                params={"fields": "storageQuota"},
+            )
+        ).json()
 
-    async def set_permissions(self, fileid: str, role: str = "reader", type: str = "anyone"):
+    async def set_permissions(
+        self, fileid: str, role: str = "reader", type: str = "anyone"
+    ):
         # set permissions to anyone with link can view
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
-        return await (await self._session.post(
-            self.base_url + f"/files/{fileid}/permissions",
-            headers={
-                "Authorization": "Bearer " + self.creds.get("access_token"),
-                "Content-Type": "application/json",
-            },
-            json={
-                "role": role,
-                "type": type,
-            },
-        )).json()
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
+        return await (
+            await self._session.post(
+                self.base_url + f"/files/{fileid}/permissions",
+                headers={
+                    "Authorization": "Bearer " + self.creds.get("access_token"),
+                    "Content-Type": "application/json",
+                },
+                json={
+                    "role": role,
+                    "type": type,
+                },
+            )
+        ).json()
 
-    async def list_files(self) -> list:
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
+    async def list_files(self, query: str = None) -> list:
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
         files = []
         page_token = None
-        params = {"supportAllDrives": "true"}
+        params = {
+            "fields": "files(id, name, mimeType, parents, webContentLink, webViewLink, size)",
+            "supportsAllDrives": "true",
+            "includeItemsFromAllDrives": "true",
+        }
+        if query:
+            params["q"] = f"name contains '{query}'"
+            params["q"] += (
+                f" and '{self.folder_id}' in parents" if self.folder_id else ""
+            )
         while True:
-            resp = await (await self._session.get(
-                self.base_url + "/files",
-                headers={
-                   "Authorization": "Bearer " + self.creds.get("access_token"),
-                   "Content-Type": "application/json",
-                },
-                params=params,
-            )).json()
+            resp = await (
+                await self._session.get(
+                    self.base_url + "/files",
+                    headers={
+                        "Authorization": "Bearer " + self.creds.get("access_token"),
+                        "Content-Type": "application/json",
+                    },
+                    params=params,
+                )
+            ).json()
             for file in resp.get("files", []):
                 if file["mimeType"] == self.folder_mime:
                     file["url"] = self._create_folder_link(file["id"])
@@ -171,7 +230,9 @@ class GDrive:
         return files
 
     async def delete(self, fileId: str) -> dict:
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
         r = await self._session.delete(
             self.base_url + f"/files/{fileId}",
             headers={
@@ -184,8 +245,12 @@ class GDrive:
         except ContentTypeError:
             return {"status": "success"}
 
-    async def copy_file(self, fileId: str, filename: str, folder_id: str, move: bool = False):
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
+    async def copy_file(
+        self, fileId: str, filename: str, folder_id: str, move: bool = False
+    ):
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
         headers = {
             "Authorization": "Bearer " + self.creds.get("access_token"),
             "Content-Type": "application/json",
@@ -195,6 +260,7 @@ class GDrive:
             "mimeType": "application/octet-stream",
             "fields": "id, name, webContentLink",
             "supportsAllDrives": "true",
+            "includeItemsFromAllDrives": "true",
         }
         file_metadata = {
             "name": filename,
@@ -218,8 +284,12 @@ class GDrive:
             return await self.copy_file(fileId, filename, folder_id, move)
         return r
 
-    async def upload_file(self, event, path: str, filename: str = None, folder_id: str = None):
-        await self._refresh_access_token() if time.time() > self.creds.get("expires_in") else None
+    async def upload_file(
+        self, event, path: str, filename: str = None, folder_id: str = None
+    ):
+        await self._refresh_access_token() if time.time() > self.creds.get(
+            "expires_in"
+        ) else None
         last_txt = ""
         filename = filename if filename else path.split("/")[-1]
         mime_type = guess_type(path)[0] or "application/octet-stream"
@@ -228,8 +298,10 @@ class GDrive:
         # await self.get_size_status()
         chunksize = 104857600  # 100MB
         # 1. Retrieve session for resumable upload.
-        headers = {"Authorization": "Bearer " +
-                   self.creds.get("access_token"), "Content-Type": "application/json"}
+        headers = {
+            "Authorization": "Bearer " + self.creds.get("access_token"),
+            "Content-Type": "application/json",
+        }
         params = {
             "name": filename,
             "mimeType": mime_type,
@@ -249,8 +321,10 @@ class GDrive:
             # upload to root and move
             r = await self.upload_file(event, path, filename, "root")
             return await self.copy_file(r["id"], filename, folder_id, move=True)
-        upload_url = "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&upload_id=" + \
-            r.headers.get("X-GUploader-UploadID")
+        upload_url = (
+            "https://www.googleapis.com/upload/drive/v3/files?uploadType=resumable&upload_id="
+            + r.headers.get("X-GUploader-UploadID")
+        )
 
         with open(path, "rb") as f:
             uploaded = 0
@@ -258,10 +332,19 @@ class GDrive:
             resp = None
             while filesize != uploaded:
                 chunk_data = f.read(chunksize)
-                headers = {"Content-Length": str(len(chunk_data)),
-                           "Content-Range": "bytes " + str(uploaded) + "-" + str(uploaded + len(chunk_data) - 1) + "/" + str(filesize)}
+                headers = {
+                    "Content-Length": str(len(chunk_data)),
+                    "Content-Range": "bytes "
+                    + str(uploaded)
+                    + "-"
+                    + str(uploaded + len(chunk_data) - 1)
+                    + "/"
+                    + str(filesize),
+                }
                 uploaded += len(chunk_data)
-                resp = await self._session.put(upload_url, data=chunk_data, headers=headers)
+                resp = await self._session.put(
+                    upload_url, data=chunk_data, headers=headers
+                )
                 diff = time.time() - start
                 percentage = round((uploaded / filesize) * 100, 2)
                 speed = round(uploaded / diff, 2)
