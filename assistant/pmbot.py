424f48fe5a3c937057db2c2951bed1c7ef1ddeb0
TrueSaiyan <ufperth@protonmail.com>
2024-01-25 11:39:07 +0800

    Update pmbot.py
    pmbot update
    
    when botchat is enabled your assistant will also send a message to your log group. replying to the user there will also work..
    
    Your SUDOS can also reply

--- assistant/pmbot.py
+++ assistant/pmbot.py
@@ -9,6 +9,8 @@
 
 # --------------------------------------- Imports -------------------------------------------- #
 
+import asyncio
+import logging
 import os
 
 from telethon.errors.rpcerrorlist import UserNotParticipantError
@@ -25,8 +27,59 @@ from pyUltroid.fns.helper import inline_mention
 from . import *
 
 botb = KeyManager("BOTBLS", cast=list)
-FSUB = udB.get_key("PMBOT_FSUB")
+FSUB = Keys.PMBOT_FSUB
+PMBOTGROUP = Keys.LOG_CHANNEL
 CACHE = {}
+SUDOS = Keys.SUDOS
+PMUSERS = [OWNER_ID, SUDOS]
+logging.basicConfig(
+    format="%(asctime)s | %(name)s [%(levelname)s] : %(message)s",
+    level=logging.INFO,
+    datefmt="%m/%d/%Y, %H:%M:%S",
+)
+logger = logging.getLogger("DEBUGGING")
+
+# --------------------------------------- Functions -------------------------------------------- #
+
+
+async def forward_to_multiple(event, *user_ids):
+    results = []
+    tasks = []
+
+    for user_id in user_ids:
+        task = asyncio.create_task(event.forward_to(user_id))
+        tasks.append(task)
+
+    for task in tasks:
+        try:
+            result = await task
+            results.append(result)
+        except Exception as e:
+            results.append(str(e))
+
+    return results
+
+
+async def check_reply_from_bot(event):
+    if (event.is_private and event.is_reply) or (
+        event.chat_id == PMBOTGROUP and event.is_reply and event.reply_to_msg_id
+    ):
+        if event.chat_id == PMBOTGROUP:
+            replied_message = await event.client.get_messages(
+                event.chat_id, ids=event.reply_to_msg_id
+            )
+            if replied_message.from_id:
+                entity = replied_message.from_id.user_id
+            else:
+                return False
+            return entity == 6176247391
+        else:
+            # For private messages, no need to check the entity, as it's
+            # already a reply
+            return True
+    return False
+
+
 # --------------------------------------- Incoming -------------------------------------------- #
 
 
@@ -37,7 +90,6 @@ CACHE = {}
 )
 async def on_new_mssg(event):
     who = event.sender_id
-    # doesn't reply to that user anymore
     if event.text.startswith("/") or who == OWNER_ID:
         return
     if FSUB:
@@ -72,9 +124,12 @@ async def on_new_mssg(event):
         if MSG and BTTS:
             return await event.reply(MSG, buttons=BTTS)
     xx = await event.forward_to(OWNER_ID)
+    zz = await event.forward_to(PMBOTGROUP)
     if event.fwd_from:
         await xx.reply(f"From {inline_mention(event.sender)} [`{event.sender_id}`]")
+        await zz.reply(f"From {inline_mention(event.sender)} [`{event.sender_id}`]")
     add_stuff(xx.id, who)
+    add_stuff(zz.id, who)
 
 
 # --------------------------------------- Outgoing -------------------------------------------- #
@@ -82,30 +137,74 @@ async def on_new_mssg(event):
 
 @asst_cmd(
     load=AST_PLUGINS,
-    from_users=[OWNER_ID],
+    from_users=PMUSERS,
     incoming=True,
-    func=lambda e: e.is_private and e.is_reply,
+    func=check_reply_from_bot,
 )
 async def on_out_mssg(event):
     x = event.reply_to_msg_id
-    to_user = get_who(x)
+    logger.info(f"msg_id: {x}")
+    if event.chat_id == PMBOTGROUP:
+        group_to_user = get_who(x)
+    else:
+        to_user = get_who(x)
+
+    if event.reply_to_msg_id:
+        replied_message = await event.client.get_messages(
+            event.chat_id, ids=event.reply_to_msg_id
+        )
+        if (
+            replied_message
+            and replied_message.fwd_from
+            and replied_message.fwd_from.from_id
+            and replied_message.fwd_from.from_id.user_id != 6176247391
+        ):
+            return
     if event.text.startswith("/who"):
         try:
-            k = await asst.get_entity(to_user)
-            photu = await event.client.download_profile_photo(k.id)
-            await event.reply(
-                f"• **Name :** {get_display_name(k)}\n• **ID :** `{k.id}`\n• **Link :** {inline_mention(k)}",
-                file=photu,
-            )
-            if photu:
-                os.remove(photu)
-            return
+            if event.is_private and to_user:
+                k = await asst.get_entity(to_user)
+                photu = await event.client.download_profile_photo(k.id)
+                await event.reply(
+                    f"• **Name :** {get_display_name(k)}\n• **ID :** `{k.id}`\n• **Link :** {inline_mention(k)}",
+                    file=photu,
+                )
+                if photu:
+                    os.remove(photu)
+                return
+            elif event.chat_id == PMBOTGROUP and group_to_user:
+                k = await asst.get_entity(group_to_user)
+                photu = await event.client.download_profile_photo(k.id)
+                await event.reply(
+                    f"• **Name :** {get_display_name(k)}\n• **ID :** `{k.id}`\n• **Link :** {inline_mention(k)}",
+                    file=photu,
+                )
+                if photu:
+                    os.remove(photu)
+                return
+            else:
+                return await event.reply(
+                    "Unable to determine the user. Please reply to a specific message."
+                )
         except BaseException as er:
             return await event.reply(f"**ERROR : **{str(er)}")
     elif event.text.startswith("/"):
         return
-    if to_user:
-        await asst.send_message(to_user, event.message)
+
+    if event.chat_id == PMBOTGROUP:
+        if group_to_user:
+            await asst.send_message(group_to_user, event.message)
+        else:
+            return await event.reply(
+                "Unable to determine the user. Please reply to a specific message."
+            )
+    elif event.sender_id in PMUSERS:
+        if to_user:
+            await asst.send_message(to_user, event.message)
+        else:
+            return await event.reply(
+                "Unable to determine the user. Please reply to a specific message."
+            )
 
 
 # --------------------------------------- Ban/Unban -------------------------------------------- #
