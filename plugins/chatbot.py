7e6fcfbce521760dff8b220e1e43fe63beceab2c
TrueSaiyan <ufperth@protonmail.com>
2024-01-26 14:54:56 +0800

    Update chatbot.py

--- plugins/chatbot.py
+++ plugins/chatbot.py
@@ -10,9 +10,85 @@ from . import get_help
 __doc__ = get_help("help_chatbot")
 
 
-from pyUltroid.fns.tools import get_chatbot_reply
+from pyUltroid.fns.tools import get_chatbot_reply, get_oracle_reply
 
 from . import LOGS, eod, get_string, inline_mention, udB, ultroid_cmd
+
+
+@ultroid_cmd(pattern="repoai")
+async def im_oracle(event):
+    if event.reply_to:
+        message = (await event.get_reply_message()).text.strip()
+    else:
+        try:
+            message = event.text.split(" ", 1)[1]
+        except IndexError:
+            return await eod(event, get_string("tban_1"), time=10)
+    reply_ = await get_orcale_reply(
+        query=message, user_id=ultroid_bot.me.id, mongo_url=MONGO_URI
+    )
+    await event.eor(reply_)
+
+
+@ultroid_cmd(pattern="addoai")
+async def add_oracle(event):
+    await oracle_bot_fn(event, type_="add")
+
+
+@ultroid_cmd(pattern="remoai")
+async def rem_oracle(event):
+    await oracle_bot_fn(event, type_="remov")
+
+
+@ultroid_cmd(pattern="listoai")
+async def listoracle(event):
+    key = udB.get_key("ORACLE_USERS") or {}
+    users = key.get(event.chat_id, [])
+    if not users:
+        return await event.eor(get_string("chab_2"), time=5)
+    msg = "**Total List Of Oracle Enabled Users In This Chat :**\n\n"
+    for i in users:
+        try:
+            user = await event.client.get_entity(int(i))
+            user = inline_mention(user)
+        except BaseException:
+            user = f"`{i}`"
+        msg += f"• {user}\n"
+    await event.eor(msg, link_preview=False)
+
+
+async def oracle_bot_fn(event, type_):
+    if event.reply_to:
+        user_ = (await event.get_reply_message()).sender
+    else:
+        temp = event.text.split(maxsplit=1)
+        try:
+            user_ = await event.client.get_entity(await event.client.parse_id(temp[1]))
+        except BaseException as er:
+            LOGS.exception(er)
+            user_ = event.chat if event.is_private else None
+    if not user_:
+        return await eod(
+            event,
+            get_string("chab_1"),
+        )
+    key = udB.get_key("ORACLE_USERS") or {}
+    chat = event.chat_id
+    user = user_.id
+    if type_ == "add":
+        if key.get(chat):
+            if user not in key[chat]:
+                key[chat].append(user)
+        else:
+            key.update({chat: [user]})
+    elif type_ == "remov":
+        if key.get(chat):
+            if user in key[chat]:
+                key[chat].remove(user)
+            if chat in key and not key[chat]:
+                del key[chat]
+    udB.set_key("ORACLE_USERS", key)
+    await event.eor(f"**Oracle:**\n{type_}ed {inline_mention(user_)}")
 
 
 @ultroid_cmd(pattern="repai")
