6682c21b446a7cca778083cec84f3123e05dfb17
TrueSaiyan <ufperth@protonmail.com>
2024-02-08 20:15:21 +0800

    Update custom_markdown.py

--- pyUltroid/fns/custom_markdown.py
+++ pyUltroid/fns/custom_markdown.py
@@ -1,3 +1,10 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://github.com/TeamUltroid/pyUltroid/blob/main/LICENSE>.
+
 from telethon import types
 from telethon.extensions import markdown
 
