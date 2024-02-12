b1cd18e3e3db92d9779f7a051ccc8c1e6df88968
TrueSaiyan <ufperth@protonmail.com>
2024-02-13 07:26:19 +0800

    Update helper.py

--- pyUltroid/fns/helper.py
+++ pyUltroid/fns/helper.py
@@ -288,7 +288,7 @@ async def bash(cmd, run_code=0):
     err = stderr.decode().strip() or None
     out = stdout.decode().strip()
     if not run_code and err:
-        if match := re.match("\\/bin\\/sh: (.*): ?(\\w+): not found", err):
+        if match := re.match("\/bin\/sh: (.*): ?(\w+): not found", err):
             return out, f"{match.group(2).upper()}_NOT_FOUND"
     return out, err
 
