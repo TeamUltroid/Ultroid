2cbfcd819aa55dd856aef2a6b261b39743a1e681
TrueSaiyan <ufperth@protonmail.com>
2024-02-14 04:20:29 +0800

    Update usage.py

--- plugins/usage.py
+++ plugins/usage.py
@@ -4,18 +4,10 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-"""
-✘ Commands Available
 
-• `{i}usage`
-    Get overall usage.
+from . import get_help
 
-• `{i}usage heroku`
-   Get heroku stats.
-
-• `{i}usage db`
-   Get database storage usage.
-"""
+__doc__ = get_help("help_usage")
 
 import math
 import shutil
