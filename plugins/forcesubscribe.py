5da1539511787f4c24a7f2e4e8c480275e26cc0d
TrueSaiyan <ufperth@protonmail.com>
2024-02-20 13:28:36 +0800

    Update forcesubscribe.py

--- plugins/forcesubscribe.py
+++ plugins/forcesubscribe.py
@@ -4,21 +4,10 @@
 # This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
 # PLease read the GNU Affero General Public License in
 # <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
-"""
-✘ Commands Available -
 
-• `{i}fsub <chat username><id>`
-    Enable ForceSub in Used Chat !
+from . import get_help
 
-• `{i}checkfsub`
-    Check/Get Active ForceSub Setting of Used Chat.
-
-• `{i}remfsub`
-    Remove ForceSub from Used Chat !
-
-    Note - You Need to be Admin in Both Channel/Chats
-        in order to Use ForceSubscribe.
-"""
+__doc__ = get_help("help_forcesubscribe")
 
 import re
 
