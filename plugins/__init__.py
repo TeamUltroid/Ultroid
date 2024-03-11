20f0876d8cfcea556d32b84612f4138a0b3849b1
TrueSaiyan <ufperth@protonmail.com>
2024-03-11 15:59:19 +0800

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -22,6 +22,7 @@ from pyUltroid.dB import DEVLIST, ULTROID_IMAGES
 from pyUltroid.fns.helper import *
 from pyUltroid.fns.misc import *
 from pyUltroid.fns.tools import *
+from pyUltroid.fns.custom_markdown import *
 from pyUltroid.startup._database import _BaseDatabase as Database
 from pyUltroid.version import __version__, ultroid_version
 from strings import get_help, get_string
