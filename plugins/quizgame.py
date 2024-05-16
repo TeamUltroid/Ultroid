5f4d2a494547b6ae96766fc5635f983f70fba151
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2024-05-16 17:29:58 +0000

    pylint: auto fixes

--- plugins/quizgame.py
+++ plugins/quizgame.py
@@ -9,7 +9,8 @@ from core.decorators._assistant import asst_cmd, callback
 from telethon.errors import ChatSendStickersForbiddenError
 from telethon.events import Raw
 from telethon.tl.functions.messages import SendMediaRequest
-from telethon.tl.types import InputMediaPoll, Poll, PollAnswer, UpdateMessagePollVote
+from telethon.tl.types import (InputMediaPoll, Poll, PollAnswer,
+                               UpdateMessagePollVote)
 
 from .. import Button, asst, async_searcher, get_string, inline_mention
 
@@ -84,12 +85,14 @@ async def choose_cata(event):
         text = get_string("games_3")
     elif match[0] == "c":
         m = match[1:]
-        buttons = [[Button.inline(str(i), f"trziat{m}_{i}") for i in range(10, 70, 20)]]
+        buttons = [
+            [Button.inline(str(i), f"trziat{m}_{i}") for i in range(10, 70, 20)]]
         text = get_string("games_4")
     elif match[0] == "t":
         m_ = match[1:]
         buttons = [
-            [Button.inline(str(i), f"trzias{m_}_{i}") for i in [10, 30, 60, 120]]
+            [Button.inline(str(i), f"trzias{m_}_{i}")
+             for i in [10, 30, 60, 120]]
         ]
         text = get_string("games_5")
     elif match[0] == "s":
@@ -119,7 +122,9 @@ async def choose_cata(event):
             opts = [PollAnswer(unescape(q["correct_answer"]), ansi)]
             [
                 opts.append(
-                    PollAnswer(unescape(a), str(uuid.uuid1()).split("-")[0].encode())
+                    PollAnswer(
+                        unescape(a), str(
+                            uuid.uuid1()).split("-")[0].encode())
                 )
                 for a in q["incorrect_answers"]
             ]
@@ -138,7 +143,8 @@ async def choose_cata(event):
                 solution_entities=[],
             )
             m_ = await event.client(SendMediaRequest(chat, media=poll))
-            POLLS.update({m_.poll.poll.id: {"chat": m_.chat_id, "answer": ansi}})
+            POLLS.update(
+                {m_.poll.poll.id: {"chat": m_.chat_id, "answer": ansi}})
             await asyncio.sleep(int(in_))
         if not TRIVIA_CHATS[chat]:
             await event.respond(
