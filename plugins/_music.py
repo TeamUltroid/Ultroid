49cf1ea251b5728c0fedcd603124175ad235c8fc
ErRickow <remixingmagelang@gmail.com>
2024-07-11 03:53:29 +0700

    _music.py

---
+++ plugins/_music.py
@@ -0,0 +1,479 @@
+# Ultroid - UserBot
+# Copyright (C) 2021-2023 TeamUltroid
+#
+# This file is a part of < https://github.com/TeamUltroid/Ultroid/ >
+# PLease read the GNU Affero General Public License in
+# <https://www.github.com/TeamUltroid/Ultroid/blob/main/LICENSE/>.
+
+import asyncio
+import os
+from time import time
+from traceback import format_exc
+
+from pyUltroid import HNDLR, LOGS, asst, udB, vcClient
+from pyUltroid._misc import owner_and_sudos
+from pyUltroid._misc._decorators import compile_pattern
+from pyUltroid.fns.admins import admin_check
+from pyUltroid.fns.helper import bash, downloader, time_formatter
+from pyUltroid.fns.ytdl import get_videos_link
+from pyUltroid.version import __version__ as AyVer
+from pytgcalls import GroupCallFactory
+from pytgcalls.exceptions import GroupCallNotFoundError
+from telethon import events
+from telethon.errors.rpcerrorlist import (ChatSendMediaForbiddenError,
+                                          ParticipantJoinMissingError)
+from telethon.tl import functions
+
+try:
+    from yt_dlp import YoutubeDL
+except ImportError:
+    YoutubeDL = None
+    LOGS.error("'yt-dlp' not found!")
+
+try:
+    from youtubesearchpython import VideosSearch
+except ImportError:
+    VideosSearch = None
+
+
+asstUserName = asst.me.username
+LOG_CHANNEL = udB.get_key("LOG_CHANNEL")
+ACTIVE_CALLS, VC_QUEUE = [], {}
+MSGID_CACHE, VIDEO_ON = {}, {}
+CLIENTS = {}
+
+
+def VC_AUTHS():
+    _vcsudos = udB.get_key("VC_SUDOS") or []
+    return [int(a) for a in [*owner_and_sudos(), *_vcsudos]]
+
+
+class Player:
+    def __init__(self, chat, event=None, video=False):
+        self._chat = chat
+        self._current_chat = event.chat_id if event else LOG_CHANNEL
+        self._video = video
+        if CLIENTS.get(chat):
+            self.group_call = CLIENTS[chat]
+        else:
+            _client = GroupCallFactory(
+                vcClient,
+                GroupCallFactory.MTPROTO_CLIENT_TYPE.TELETHON,
+            )
+            self.group_call = _client.get_group_call()
+            CLIENTS.update({chat: self.group_call})
+
+    async def make_vc_active(self):
+        try:
+            await vcClient(
+                functions.phone.CreateGroupCallRequest(
+                    self._chat, title="🎧 Naya Music 🎶"
+                )
+            )
+        except Exception as e:
+            LOGS.exception(e)
+            return False, e
+        return True, None
+
+    async def startCall(self):
+        if VIDEO_ON:
+            for chats in VIDEO_ON:
+                await VIDEO_ON[chats].stop()
+            VIDEO_ON.clear()
+            await asyncio.sleep(3)
+        if self._video:
+            for chats in list(CLIENTS):
+                if chats != self._chat:
+                    await CLIENTS[chats].stop()
+                    del CLIENTS[chats]
+            VIDEO_ON.update({self._chat: self.group_call})
+        if self._chat not in ACTIVE_CALLS:
+            try:
+                self.group_call.on_network_status_changed(self.on_network_changed)
+                self.group_call.on_playout_ended(self.playout_ended_handler)
+                await self.group_call.join(self._chat)
+            except GroupCallNotFoundError as er:
+                LOGS.info(er)
+                dn, err = await self.make_vc_active()
+                if err:
+                    return False, err
+            except Exception as e:
+                LOGS.exception(e)
+                return False, e
+        return True, None
+
+    async def on_network_changed(self, call, is_connected):
+        chat = self._chat
+        if is_connected:
+            if chat not in ACTIVE_CALLS:
+                ACTIVE_CALLS.append(chat)
+        elif chat in ACTIVE_CALLS:
+            ACTIVE_CALLS.remove(chat)
+
+    async def playout_ended_handler(self, call, source, mtype):
+        if os.path.exists(source):
+            os.remove(source)
+        await self.play_from_queue()
+
+    async def play_from_queue(self):
+        chat_id = self._chat
+        if chat_id in VIDEO_ON:
+            await self.group_call.stop_video()
+            VIDEO_ON.pop(chat_id)
+        try:
+            song, title, link, thumb, from_user, pos, dur = await get_from_queue(
+                chat_id
+            )
+            try:
+                await self.group_call.start_audio(song)
+            except ParticipantJoinMissingError:
+                await self.vc_joiner()
+                await self.group_call.start_audio(song)
+            if MSGID_CACHE.get(chat_id):
+                await MSGID_CACHE[chat_id].delete()
+                del MSGID_CACHE[chat_id]
+            text = f"<strong>🎧 Sedang dimainkan #{pos}: <a href={link}>{title}</a>\n⏰ Durasi:</strong> <code>{dur}</code>\n👤 <strong>Diminta oleh:</strong> {from_user}"
+
+            try:
+                xx = await vcClient.send_message(
+                    self._current_chat,
+                    f"<strong>🎧 Sedang dimainkan #{pos}: <a href={link}>{title}</a>\n⏰ Durasi:</strong> <code>{dur}</code>\n👤 <strong>Diminta oleh:</strong> {from_user}",
+                    file=thumb,
+                    link_preview=False,
+                    parse_mode="html",
+                )
+
+            except ChatSendMediaForbiddenError:
+                xx = await vcClient.send_message(
+                    self._current_chat, text, link_preview=False, parse_mode="html"
+                )
+            MSGID_CACHE.update({chat_id: xx})
+            VC_QUEUE[chat_id].pop(pos)
+            if not VC_QUEUE[chat_id]:
+                VC_QUEUE.pop(chat_id)
+
+        except (IndexError, KeyError):
+            await self.group_call.stop()
+            del CLIENTS[self._chat]
+            await vcClient.send_message(
+                self._current_chat,
+                f"❏ <b>Berhasil Turun Voice Chat</b>\n└ <b>Chat ID:</b> <code>{chat_id}</code>",
+                parse_mode="html",
+            )
+        except Exception as er:
+            LOGS.exception(er)
+            await vcClient.send_message(
+                self._current_chat,
+                f"<strong>ERROR:</strong> <code>{format_exc()}</code>",
+                parse_mode="html",
+            )
+
+    async def vc_joiner(self):
+        chat_id = self._chat
+        done, err = await self.startCall()
+
+        if done:
+            await vcClient.send_message(
+                self._current_chat,
+                f"❏ <b>Berhasil Bergabung Voice Chat</b>\n└ <b>Chat ID:</b> <code>{chat_id}</code>",
+                parse_mode="html",
+            )
+
+            return True
+        await vcClient.send_message(
+            self._current_chat,
+            f"<strong>ERROR</strong> <code>{chat_id}</code> :\n<code>{err}</code>",
+            parse_mode="html",
+        )
+        return False
+
+
+# --------------------------------------------------
+
+
+def vc_asst(dec, **kwargs):
+    def ult(func):
+        kwargs["func"] = (
+            lambda e: not e.is_private and not e.via_bot_id and not e.fwd_from
+        )
+        handler = udB.get_key("VC_HNDLR") or HNDLR
+        kwargs["pattern"] = compile_pattern(dec, handler)
+        vc_auth = kwargs.get("vc_auth", True)
+        key = udB.get_key("VC_AUTH_GROUPS") or {}
+        if "vc_auth" in kwargs:
+            del kwargs["vc_auth"]
+
+        async def vc_handler(e):
+            VCAUTH = list(key.keys())
+            if not (
+                (e.out)
+                or (e.sender_id in VC_AUTHS())
+                or (vc_auth and e.chat_id in VCAUTH)
+            ):
+                return
+            elif vc_auth and key.get(e.chat_id):
+                cha, adm = key.get(e.chat_id), key[e.chat_id]["admins"]
+                if adm and not (await admin_check(e)):
+                    return
+            try:
+                await func(e)
+            except Exception:
+                LOGS.exception(Exception)
+                await asst.send_message(
+                    LOG_CHANNEL,
+                    f"VC Error - <code>{AyVer}</code>\n\n<code>{e.text}</code>\n\n<code>{format_exc()}</code>",
+                    parse_mode="html",
+                )
+
+        vcClient.add_event_handler(
+            vc_handler,
+            events.NewMessage(**kwargs),
+        )
+
+    return ult
+
+
+# --------------------------------------------------
+
+
+def add_to_queue(chat_id, song, song_name, link, thumb, from_user, duration):
+    try:
+        n = sorted(list(VC_QUEUE[chat_id].keys()))
+        play_at = n[-1] + 1
+    except BaseException:
+        play_at = 1
+    stuff = {
+        play_at: {
+            "song": song,
+            "title": song_name,
+            "link": link,
+            "thumb": thumb,
+            "from_user": from_user,
+            "duration": duration,
+        }
+    }
+    if VC_QUEUE.get(chat_id):
+        VC_QUEUE[int(chat_id)].update(stuff)
+    else:
+        VC_QUEUE.update({chat_id: stuff})
+    return VC_QUEUE[chat_id]
+
+
+def list_queue(chat):
+    if VC_QUEUE.get(chat):
+        txt, n = "", 0
+        for x in list(VC_QUEUE[chat].keys())[:18]:
+            n += 1
+            data = VC_QUEUE[chat][x]
+            txt += f'<strong>{n}. <a href={data["link"]}>{data["title"]}</a> :</strong> <i>By: {data["from_user"]}</i>\n'
+        txt += "\n\n....."
+        return txt
+
+
+async def get_from_queue(chat_id):
+    play_this = list(VC_QUEUE[int(chat_id)].keys())[0]
+    info = VC_QUEUE[int(chat_id)][play_this]
+    song = info.get("song")
+    title = info["title"]
+    link = info["link"]
+    thumb = info["thumb"]
+    from_user = info["from_user"]
+    duration = info["duration"]
+    if not song:
+        song = await get_stream_link(link)
+    return song, title, link, thumb, from_user, play_this, duration
+
+
+# --------------------------------------------------
+
+from asyncio import get_event_loop
+from functools import partial
+from json import dumps, loads
+from urllib.parse import quote_plus
+
+from requests import get
+
+
+def run_sync(func, *args, **kwargs):
+    return get_event_loop().run_in_executor(None, partial(func, *args, **kwargs))
+
+
+class YouTubeSearch:
+    def __init__(self, search_terms: str, max_results=None):
+        self.search_terms = search_terms
+        self.max_results = max_results
+        self.videos = self._search()
+
+    def _search(self):
+        encoded_search = quote_plus(self.search_terms)
+        url = f"https://www.youtube.com/results?search_query={encoded_search}"
+        response = get(url).text
+        while "ytInitialData" not in response:
+            response = get(url).text
+        results = self._parse_html(response)
+        if self.max_results is not None and len(results) > self.max_results:
+            return results[: self.max_results]
+        return results
+
+    @staticmethod
+    def _parse_html(response):
+        results = []
+        start = response.index("ytInitialData") + len("ytInitialData") + 3
+        end = response.index("};", start) + 1
+        json_str = response[start:end]
+        data = loads(json_str)
+
+        for contents in data["contents"]["twoColumnSearchResultsRenderer"][
+            "primaryContents"
+        ]["sectionListRenderer"]["contents"]:
+            for video in contents["itemSectionRenderer"]["contents"]:
+                res = {}
+                if "videoRenderer" in video.keys():
+                    video_data = video.get("videoRenderer", {})
+                    res["id"] = video_data.get("videoId", None)
+                    res["title"] = (
+                        video_data.get("title", {})
+                        .get("runs", [[{}]])[0]
+                        .get("text", None)
+                    )
+                    res["duration"] = video_data.get("lengthText", {}).get(
+                        "simpleText", 0
+                    )
+                    res["views"] = video_data.get("viewCountText", {}).get(
+                        "simpleText", 0
+                    )
+                    res["link"] = "https://www.youtube.com" + (
+                        video_data.get("navigationEndpoint", {})
+                        .get("commandMetadata", {})
+                        .get("webCommandMetadata", {})
+                        .get("url", None)
+                    )
+                    if (
+                        res["duration"] != 0
+                        and res["views"] != 0
+                        and sum(
+                            int(x) * 60**i
+                            for i, x in enumerate(
+                                reversed(str(res["duration"]).split(":"))
+                            )
+                        )
+                        < 7200
+                    ):
+                        results.append(res)
+
+            if results:
+                return results
+        return results
+
+    def to_dict(self, clear_cache=True):
+        result = self.videos
+        if clear_cache:
+            self.videos = ""
+        return result
+
+    def to_json(self, clear_cache=True):
+        result = dumps({"videos": self.videos})
+        if clear_cache:
+            self.videos = ""
+        return result
+
+
+async def download(query):
+    if query.startswith("https://") and "youtube" not in query.lower():
+        thumb, duration = None, "Unknown"
+        title = link = query
+    else:
+        search = VideosSearch(query, limit=1).result()
+        results = YouTubeSearch(query, max_results=1).to_dict()
+        videoid = results[0]["id"]
+        title = results[0]["title"]
+        duration = results[0]["duration"]
+        link = results[0]["link"]
+        thumb = f"https://img.youtube.com/vi/{videoid}/hqdefault.jpg"
+    dl = await get_stream_link(link)
+    return dl, thumb, title, link, duration
+
+
+async def get_stream_link(ytlink):
+    stream = await bash(f'yt-dlp -g -f "best[height<=?720][width<=?1280]" {ytlink}')
+    return stream[0]
+
+
+async def vid_download(query):
+    search = VideosSearch(query, limit=1).result()
+    data = search["result"][0]
+    link = data["link"]
+    video = await get_stream_link(link)
+    title = data["title"]
+    thumb = f"https://i.ytimg.com/vi/{data['id']}/hqdefault.jpg"
+    duration = data.get("duration") or "♾"
+    return video, thumb, title, link, duration
+
+
+async def dl_playlist(chat, from_user, link):
+    # untill issue get fix
+    # https://github.com/alexmercerind/youtube-search-python/issues/107
+    """
+    vids = Playlist.getVideos(link)
+    try:
+        vid1 = vids["videos"][0]
+        duration = vid1["duration"] or "♾"
+        title = vid1["title"]
+        song = await get_stream_link(vid1['link'])
+        thumb = f"https://i.ytimg.com/vi/{vid1['id']}/hqdefault.jpg"
+        return song[0], thumb, title, vid1["link"], duration
+    finally:
+        vids = vids["videos"][1:]
+        for z in vids:
+            duration = z["duration"] or "♾"
+            title = z["title"]
+            thumb = f"https://i.ytimg.com/vi/{z['id']}/hqdefault.jpg"
+            add_to_queue(chat, None, title, z["link"], thumb, from_user, duration)
+    """
+    links = await get_videos_link(link)
+    try:
+        search = VideosSearch(links[0], limit=1).result()
+        vid1 = search["result"][0]
+        duration = vid1.get("duration") or "♾"
+        title = vid1["title"]
+        song = await get_stream_link(vid1["link"])
+        thumb = f"https://i.ytimg.com/vi/{vid1['id']}/hqdefault.jpg"
+        return song, thumb, title, vid1["link"], duration
+    finally:
+        for z in links[1:]:
+            try:
+                search = VideosSearch(z, limit=1).result()
+                vid = search["result"][0]
+                duration = vid.get("duration") or "♾"
+                title = vid["title"]
+                thumb = f"https://i.ytimg.com/vi/{vid['id']}/hqdefault.jpg"
+                add_to_queue(chat, None, title, vid["link"], thumb, from_user, duration)
+            except Exception as er:
+                LOGS.exception(er)
+
+
+async def file_download(event, reply, fast_download=False):
+    thumb = "https://telegra.ph//file/fc93865e70ab86745c11d.jpg"
+    title = reply.file.title or reply.file.name or f"{str(time())}.jpg"
+    file = reply.file.name or f"{str(time())}.jpg"
+    if fast_download:
+        dl = await downloader(
+            f"/downloads/{file}",
+            reply.media.document,
+            event,
+            time(),
+            f"Downloading {title}...",
+        )
+
+        dl = dl.name
+    else:
+        dl = await reply.download_media()
+    duration = (
+        time_formatter(reply.file.duration * 1000) if reply.file.duration else "🤷‍♂️"
+    )
+    if reply.document.thumbs:
+        thumb = await reply.download_media("/downloads/", thumb=-1)
+    return dl, thumb, title, reply.message_link, duration
+
+
+# --------------------------------------------------
