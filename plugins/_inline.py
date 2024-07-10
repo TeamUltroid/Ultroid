13ddeca87440e6bc71460e917d5a50a756ee4e6c
ErRickow <remixingmagelang@gmail.com>
2024-07-11 05:17:35 +0700

    Update _inline.py

--- plugins/_inline.py
+++ plugins/_inline.py
@@ -107,7 +107,7 @@ async def inline_handler(event):
         )
     else:
         result = await event.builder.article(
-            title="Ultroid Help Menu", text=text, buttons=_main_help_menu
+            title="ErUbot Help Menu", text=text, buttons=_main_help_menu
         )
     await event.answer([result], private=True, cache_time=300, gallery=True)
 
