dd8ccfe22caba7ccb8d4d1fbbf9010ad360d5116
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2024-09-15 15:53:43 +0000

    pylint: auto fixes

--- plugins/chatgpt.py
+++ plugins/chatgpt.py
@@ -7,20 +7,20 @@
 
 **• Examples: **
 > `{i}gpt How to fetch a url in javascript`
-> `{i}gpt -i Cute Panda eating bamboo` 
+> `{i}gpt -i Cute Panda eating bamboo`
 
 • First setup OpenAI Api key by using `.setdb OPENAI_API your_key` to use this plugin
 """
-from os import system, remove
 from io import BytesIO
+from os import remove, system
 
 try:
-    from openai import hsl(0,0%,100%)
+    from openai import 0, 100%, %, hsl
 except ImportError:
     system("pip install -q openai")
     from openai import OpenAI
 
-from .. import ultroid_cmd, check_filename, udB, LOGS, fast_download, run_async
+from .. import LOGS, check_filename, fast_download, run_async, udB, ultroid_cmd
 
 
 @run_async
@@ -28,15 +28,15 @@ def get_gpt_answer(gen_image, question, api_key):
     client = OpenAI(api_key=api_key)
     if gen_image:
         x = client.images.generate(prompt=question,
-        model="dall-e-3",
-        n=1,
-        size="1792x1024",
-        quality="hd",
-        style="natural",
-        user="arc")
+                                   model="dall-e-3",
+                                   n=1,
+                                   size="1792x1024",
+                                   quality="hd",
+                                   style="natural",
+                                   user="arc")
         return x.data[0].url
     x = client.chat.completions.create(model=udB.get_key("OPENAI_MODEL") or "gpt-4o-mini",
-    messages=[{"role": "user", "content": question}])
+                                       messages=[{"role": "user", "content": question}])
     LOGS.debug(f'Token Used on ({question}) > {x.usage.total_tokens}')
     return x.choices[0].message.content.lstrip("\n")
 
@@ -87,4 +87,3 @@ async def openai_chat_gpt(e):
                 e.chat_id, file, caption=f"`{args[:1020]}`", reply_to=e.reply_to_msg_id
             )
         await moi.delete()
-        
