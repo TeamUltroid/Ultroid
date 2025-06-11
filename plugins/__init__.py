22b8bc616585138d8d5de41a8d16fbc39e8515b6
New-dev0 <newdev0@outlook.com>
2025-06-12 01:42:52 +0530

    Reduce official plugins

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -75,19 +75,6 @@ NOSPAM_CHAT = [
     -1001473548283,  # SharingUserbot
 ]
 
-KANGING_STR = [
-    "Using Witchery to kang this sticker...",
-    "Plagiarising hehe...",
-    "Inviting this sticker over to my pack...",
-    "Kanging this sticker...",
-    "Hey that's a nice sticker!\nMind if I kang?!..",
-    "Hehe me stel ur stiker...",
-    "Ay look over there (☉｡☉)!→\nWhile I kang this...",
-    "Roses are red violets are blue, kanging this sticker so my pack looks cool",
-    "Imprisoning this sticker...",
-    "Mr.Steal-Your-Sticker is stealing this sticker... ",
-]
-
 
 ATRA_COL = [
     "DarkCyan",
